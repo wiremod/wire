fb5a210e9936f630318e0cd1b18087687133532e
TomyLobo <tomylobo@nurfuerspam.de>
2009-11-08 19:59:56 +0000

    [E2] console.lua+cl_console.lua: Added a wire_expression2_concmd_whitelist console variable. By default this variable is empty, which means that everything is valid. I also fixed that annoying "wire_expression2_concmd doesn't persist" bug.

--- lua/entities/gmod_wire_expression2/core/cl_console.lua
+++ lua/entities/gmod_wire_expression2/core/cl_console.lua
@@ -1,1 +1,23 @@
-CreateClientConVar("wire_expression2_concmd", 0, true, true)
+local convars = {
+	wire_expression2_concmd = 0,
+	wire_expression2_concmd_whitelist = "",
+}
+
+local function CreateCVars()
+	for name,default in pairs(convars) do
+		current_cvar = CreateClientConVar(name, default, true, true)
+		local value = current_cvar:GetString() or default
+		RunConsoleCommand(name, value)
+	end
+end
+
+if CanRunConsoleCommand() then
+	CreateCVars()
+else
+	hook.Add("OnEntityCreated", "wire_expression2_console", function(ent)
+		if not ValidEntity(ent) then return end
+		if ent ~= LocalPlayer() then return end
+
+		CreateCVars()
+	end)
+end
--- lua/entities/gmod_wire_expression2/core/console.lua
+++ lua/entities/gmod_wire_expression2/core/console.lua
@@ -4,9 +4,21 @@
 
 E2Lib.RegisterExtension("console", true)
 
-local function validConCmd(self)
-	if not self.player:IsValid() then return false end
-	return self.player:GetInfoNum("wire_expression2_concmd") ~= 0
+local function validConCmd(self, command)
+	local ply = self.player
+	if not ply:IsValid() then return false end
+	if ply:GetInfoNum("wire_expression2_concmd") == 0 then return false end
+
+
+	local whitelist = (ply:GetInfo("wire_expression2_concmd_whitelist") or ""):Trim()
+	if whitelist == "" then return true end
+
+	command = command.." "
+	for _,whitelist_element in ipairs(string.Explode(",",whitelist)) do
+		whitelist_element = whitelist_element:Trim().." "
+		if whitelist_element == command:sub(1,whitelist_element:len()) then return true end
+	end
+	return false
 end
 
 --[[
@@ -16,20 +28,20 @@ end
 ]]
 
 e2function number concmd(string command)
-	if not validConCmd(self) then return 0 end
+	if not validConCmd(self, command) then return 0 end
 	self.player:ConCommand(command)
 	return 1
 end
 
 e2function string convar(string cvar)
-	if not validConCmd(self) then return "" end
+	if not validConCmd(self, cvar) then return "" end
 	local ret = self.player:GetInfo(cvar)
 	if not ret then return "" end
 	return ret
 end
 
 e2function number convarnum(string cvar)
-	if not validConCmd(self) then return 0 end
+	if not validConCmd(self, cvar) then return 0 end
 	local ret = self.player:GetInfoNum(cvar)
 	if not ret then return 0 end
 	return ret
