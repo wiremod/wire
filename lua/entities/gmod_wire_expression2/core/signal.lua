49009fd3b018b675881608c6b17b11e85c765835
TomyLobo <tomylobo@nurfuerspam.de>
2009-11-13 14:27:17 +0000

    [E2] signal.lua: Keying by entity index instead of context now. (This time without the line 24 error)

--- lua/entities/gmod_wire_expression2/core/signal.lua
+++ lua/entities/gmod_wire_expression2/core/signal.lua
@@ -9,7 +9,7 @@ Rewrite that made it work: TomyLobo
 local scopes = {{}, {}}
 
 -- holds the currently queued signals. Format:
--- signal_queue[group][name][receiver] = { group, name, scope, sender, senderid }
+-- signal_queue[group][name][receiverid] = { group, name, scope, sender, senderid }
 local signal_queue = {}
 
 -- holds the current signal data. Format:
@@ -19,13 +19,15 @@ local currentSignal = nil
 --[[************************************************************************]]--
 
 -- executes a chip's code
-local function triggerSignal(receiver, signaldata)
+local function triggerSignal(receiverid, signaldata)
+	local receiver = Entity(receiverid)
+	if not receiver or not receiver.Execute then return end
 	currentSignal = signaldata
-	receiver.entity:Execute()
+	receiver:Execute()
 	currentSignal = nil
 end
 
-local timerRunning=false
+local timerRunning = false
 
 local function checkSignals()
 	timerRunning = false
@@ -41,9 +43,9 @@ local function checkSignals()
 		-- loop through all queued signals in the group
 		for name,receivers in pairs(signals) do
 			-- ... and all receivers
-			for receiver,signaldata in pairs(receivers) do
+			for receiverid,signaldata in pairs(receivers) do
 				-- and trigger all signals on the queue
-				triggerSignal(receiver, signaldata)
+				triggerSignal(receiverid, signaldata)
 			end
 		end
 	end
@@ -51,16 +53,16 @@ local function checkSignals()
 end
 
 -- queues a chip's code for execution after at most
-local function postSignal(receiver, group, name, scope, sender, senderid)
+local function postSignal(receiverid, group, name, scope, sender, senderid)
 	-- don't send the signal back to the sender
-	if sender == receiver then return end
+	if senderid == receiverid then return end
 
 	-- create signal's spot on the queue, if it doesnt exist
 	if not signal_queue[group] then signal_queue[group] = {} end
 	if not signal_queue[group][name] then signal_queue[group][name] = {} end
 
 	-- add the given signal to the queue
-	signal_queue[group][name][receiver] = { group, name, scope, sender.entity, senderid }
+	signal_queue[group][name][receiverid] = { group, name, scope, sender, senderid }
 
 	-- set a timer if it isnt already set
 	if not timerRunning then
@@ -89,11 +91,12 @@ local function broadcastSignal(group, name, scope, sender, filter_player)
 	-- there was no signal registered for the selected scope/group/name combination.
 	if not contexts then return end
 
-	local senderid = sender.entity:EntIndex()
+	local senderid = sender:EntIndex()
 
-	for receiver,_ in pairs(contexts) do
-		if (not filter_player or receiver.player == filter_player) and (scope ~= 2 or receiver.player ~= sender_player) then
-			postSignal(receiver, group, name, scope, sender, senderid)
+	for receiverid,_ in pairs(contexts) do
+		local receiver_player = Entity(receiverid).player
+		if (not filter_player or receiver_player == filter_player) and (scope ~= 2 or receiver_player ~= sender_player) then
+			postSignal(receiverid, group, name, scope, sender, senderid)
 		end
 	end
 end
@@ -157,7 +160,7 @@ e2function void runOnSignal(string name, scope, activate)
 	if not signals[name] then signals[name] = {} end
 
 	-- (un-)register signal
-	signals[name][self] = activate
+	signals[name][self.entity:EntIndex()] = activate
 end
 
 --[[************************************************************************]]--
@@ -215,7 +218,7 @@ end
 
 --- Sets the signal that the chip sends when it is removed from the world.
 e2function void signalSetOnRemove(string name, scope)
-	self.data.removeSignal = { self.data.signalgroup, name, scope, self }
+	self.data.removeSignal = { self.data.signalgroup, name, scope, self.entity }
 end
 
 --- Clears the signal that the chip sends when it is removed from the world.
@@ -225,34 +228,35 @@ end
 
 --- Sends signal <name> to scope <scope>. Additional calls to this function with the same signal will overwrite the old call until the signal is issued.
 e2function void signalSend(string name, scope)
-	broadcastSignal(self.data.signalgroup, name, scope, self)
+	broadcastSignal(self.data.signalgroup, name, scope, self.entity)
 end
 
 --- Sends signal S to the given chip. Multiple calls for different chips do not overwrite each other.
 e2function void signalSendDirect(string name, entity receiver)
 	if not validEntity(receiver) then return end
-	receiver = receiver.context
+
+	local receiverid = receiver:EntIndex()
 
 	-- filter out non-E2 entities
-	if not receiver then return end
+	if not receiver.context then return end
 
 	-- dont send back to ourselves
-	if receiver == self then return end
+	if receiver.context == self then return end
 
 	local group = self.data.signalgroup
 
 	-- check whether the target entity accepts signals from the "anyone" scope.
 	if not scopes[1][group][name] then return end
-	if not scopes[1][group][name][receiver] then return end
+	if not scopes[1][group][name][receiverid] then return end
 
 	-- send the signal
-	postSignal(receiver, group, name, 1, self, self.entity:EntIndex())
+	postSignal(receiverid, group, name, 1, self.entity, self.entity:EntIndex())
 end
 
 --- sends signal S to chips owned by the given player, multiple calls for different players do not overwrite each other
 e2function void signalSendToPlayer(string name, entity player)
 	if not validEntity(player) then return end
-	broadcastSignal(self.data.signalgroup, name, 1, self, player)
+	broadcastSignal(self.data.signalgroup, name, 1, self.entity, player)
 end
 
 --[[************************************************************************]]--
