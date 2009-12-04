b4cb50dc65228d0c3f4fa489e7c8169eca6af7e7
TomyLobo <tomylobo@nurfuerspam.de>
2009-12-04 05:31:36 +0000

    [E2] compat.lua: Clarified notice, converted to e2function syntax

--- lua/entities/gmod_wire_expression2/core/compat.lua
+++ lua/entities/gmod_wire_expression2/core/compat.lua
@@ -1,33 +1,23 @@
--- Functions for backwards-compatibility. Might be removed at any time...
+-- Functions in this file are retained purely for backwards-compatibility. They should not be used in new code and might be removed at any time.
 
-registerFunction("teamName", "n:", "s", function(self, args)
-	local op1 = args[2]
-	local rv1 = op1[1](self, op1)
-	local str = team.GetName(rv1)
+e2function string number:teamName()
+	local str = team.GetName(this)
 	if not str then return "" end
 	return str
-end)
+end
 
-registerFunction("teamScore", "n:", "n", function(self, args)
-	local op1 = args[2]
-	local rv1 = op1[1](self, op1)
-	return team.GetScore(rv1)
-end)
+e2function number number:teamScore()
+	return team.GetScore(this)
+end
 
-registerFunction("teamPlayers", "n:", "n", function(self, args)
-	local op1 = args[2]
-	local rv1 = op1[1](self, op1)
-	return team.NumPlayers(rv1)
-end)
+e2function number number:teamPlayers()
+	return team.NumPlayers(this)
+end
 
-registerFunction("teamDeaths", "n:", "n", function(self, args)
-	local op1 = args[2]
-	local rv1 = op1[1](self, op1)
-	return team.TotalDeaths(rv1)
-end)
+e2function number number:teamDeaths()
+	return team.TotalDeaths(this)
+end
 
-registerFunction("teamFrags", "n:", "n", function(self, args)
-	local op1 = args[2]
-	local rv1 = op1[1](self, op1)
-	return team.TotalFrags(rv1)
-end)
+e2function number number:teamFrags()
+	return team.TotalFrags(this)
+end
