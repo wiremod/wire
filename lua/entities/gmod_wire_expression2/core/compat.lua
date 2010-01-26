f73c86a4ce7be750345a21c271677c838beb7770
TomyLobo <tomylobo@nurfuerspam.de>
2010-01-26 18:34:47 +0000

    [E2] entity.lua+compat.lua: Moved applyForce(V), applyOffsetForce(V) and applyAngForce(A) to compat.lua.

--- lua/entities/gmod_wire_expression2/core/compat.lua
+++ lua/entities/gmod_wire_expression2/core/compat.lua
@@ -25,3 +25,44 @@ end
 e2function void setColor(r, g, b)
 	self.entity:SetColor(math.Clamp(r, 0, 255), math.Clamp(g, 0, 255), math.Clamp(b, 0, 255), 255)
 end
+
+__e2setcost(30) -- temporary
+
+e2function void applyForce(vector force)
+	local phys = self.entity:GetPhysicsObject()
+	phys:ApplyForceCenter(Vector(force[1],force[2],force[3]))
+end
+
+e2function void applyOffsetForce(vector force, vector position)
+	local phys = self.entity:GetPhysicsObject()
+	phys:ApplyForceOffset(Vector(force[1],force[2],force[3]), Vector(position[1],position[2],position[3]))
+end
+
+e2function void applyAngForce(angle angForce)
+
+	local ent = self.entity
+	local phys = ent:GetPhysicsObject()
+	if not phys:IsValid() then return end
+
+	-- assign vectors
+	local pos = ent:LocalToWorld(phys:GetMassCenter())
+	local up = ent:GetUp()
+	local left = ent:GetRight() * -1
+	local forward = ent:GetForward()
+
+	local pitch = up      * (angForce[1]*0.5)
+	local yaw   = forward * (angForce[2]*0.5)
+	local roll  = left    * (angForce[3]*0.5)
+
+	-- apply pitch force
+	phys:ApplyForceOffset( forward, pos + pitch )
+	phys:ApplyForceOffset( forward * -1, pos - pitch )
+
+	-- apply yaw force
+	phys:ApplyForceOffset( left, pos + yaw )
+	phys:ApplyForceOffset( left * -1, pos - yaw )
+
+	-- apply roll force
+	phys:ApplyForceOffset( up, pos + roll )
+	phys:ApplyForceOffset( up * -1, pos - roll )
+end
