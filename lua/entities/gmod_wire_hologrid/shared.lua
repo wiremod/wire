26a730370c7344863a3b3e63fe2d975565c64f55
Divran <arviddivran@gmail.com>
2010-10-19 18:42:15 +0000

    Put the hologrid back because apparently it was used by other entities

---
+++ lua/entities/gmod_wire_hologrid/shared.lua
@@ -0,0 +1,9 @@
+ENT.Type            = "anim"
+ENT.Base            = "base_wire_entity"
+
+ENT.PrintName       = "Holographic Grid"
+ENT.Author          = "Chad 'Jinto'"
+ENT.Contact         = "cdbarrett@gmail.com"
+
+ENT.Spawnable       = false
+ENT.AdminSpawnable  = false
