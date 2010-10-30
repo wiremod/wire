0d92ddb53dd22f9b0497c32466b5fb7b4a194896
Divran <arviddivran@gmail.com>
2010-10-30 19:34:53 +0000

    [EGP] Made the box outline drawing code half as big, twice as fast, and fixed both the bug where it'd sometimes be larger than a regular box of the same size, and where its corners would, at certain line widths, not be aligned.

--- lua/entities/gmod_wire_egp/lib/objects/boxoutline.lua
+++ lua/entities/gmod_wire_egp/lib/objects/boxoutline.lua
@@ -1,43 +1,29 @@
 local Obj = EGP:NewObject( "BoxOutline" )
 Obj.size = 1
 Obj.angle = 0
+
+local function rotate( v, a )
+	local a = a * math.pi / 180
+	local x = math.cos(a) * v[1] - math.sin(a) * v[2]
+	local y = math.sin(a) * v[1] + math.cos(a) * v[2]
+	return { x, y }
+end
+
 Obj.Draw = function( self )
 	if (self.a>0 and self.w > 0 and self.h > 0) then
 		surface.SetDrawColor( self.r, self.g, self.b, self.a )
-		local s = self.size/2
-		if (self.angle == 0) then -- this is less demanding, so I'm using angle = 0 as a special case
-			local vec1 = { x = self.x - self.w / 2			, y = self.y - self.h / 2			}
-			local vec2 = { x = self.x - self.w / 2 + self.w	, y = self.y - self.h / 2 			}
-			local vec3 = { x = self.x - self.w / 2 + self.w	, y = self.y - self.h / 2 + self.h 	}
-			local vec4 = { x = self.x - self.w / 2			, y = self.y - self.h / 2 + self.h 	}
-			EGP:DrawLine( vec1.x		, vec1.y + s	, vec2.x		, vec2.y + s, self.size )
-			EGP:DrawLine( vec2.x - s	, vec2.y		, vec3.x - s	, vec3.y	, self.size )
-			EGP:DrawLine( vec3.x		, vec3.y - s	, vec4.x		, vec4.y - s, self.size )
-			EGP:DrawLine( vec4.x + s	, vec4.y		, vec1.x + s	, vec1.y	, self.size )
-		else
-			local centerx, centery = self.x, self.y
 
-			local ofsx, ofsy = -self.w / 2	, -self.h / 2
-			local  vec1,_ = LocalToWorld(Vector(ofsx,ofsy + s,0),Angle(0,0,0),Vector(centerx,centery,0),Angle(0,-self.angle,0))
-			local _vec4,_ = LocalToWorld(Vector(ofsx + s,ofsy,0),Angle(0,0,0),Vector(centerx,centery,0),Angle(0,-self.angle,0))
+		local x, y, w, h, a, s = self.x, self.y, self.w, self.h, self.angle, self.size
 
-			local ofsx, ofsy = self.w / 2	, -self.h / 2
-			local _vec1,_ = LocalToWorld(Vector(ofsx,ofsy + s,0),Angle(0,0,0),Vector(centerx,centery,0),Angle(0,-self.angle,0))
-			local  vec2,_ = LocalToWorld(Vector(ofsx - s,ofsy,0),Angle(0,0,0),Vector(centerx,centery,0),Angle(0,-self.angle,0))
+		local vec1 = rotate( { w / 2 - s / 2, 0 }, -a )
+		local vec2 = rotate( { -w / 2 + s / 2, 0 }, -a )
+		local vec3 = rotate( { 0, h / 2 - s / 2 }, -a )
+		local vec4 = rotate( { 0, -h / 2 + s / 2 }, -a )
 
-			local ofsx, ofsy = self.w / 2	, self.h / 2
-			local _vec2,_ = LocalToWorld(Vector(ofsx - s,ofsy,0),Angle(0,0,0),Vector(centerx,centery,0),Angle(0,-self.angle,0))
-			local  vec3,_ = LocalToWorld(Vector(ofsx,ofsy - s,0),Angle(0,0,0),Vector(centerx,centery,0),Angle(0,-self.angle,0))
-
-			local ofsx, ofsy = -self.w / 2	, self.h / 2
-			local _vec3,_ = LocalToWorld(Vector(ofsx,ofsy - s,0),Angle(0,0,0),Vector(centerx,centery,0),Angle(0,-self.angle,0))
-			local  vec4,_ = LocalToWorld(Vector(ofsx + s,ofsy,0),Angle(0,0,0),Vector(centerx,centery,0),Angle(0,-self.angle,0))
-
-			EGP:DrawLine( vec1.x, vec1.y, _vec1.x, _vec1.y, self.size )
-			EGP:DrawLine( vec2.x, vec2.y, _vec2.x, _vec2.y, self.size )
-			EGP:DrawLine( vec3.x, vec3.y, _vec3.x, _vec3.y, self.size )
-			EGP:DrawLine( vec4.x, vec4.y, _vec4.x, _vec4.y, self.size )
-		end
+		surface.DrawTexturedRectRotated( x + math.ceil(vec1[1]), y + math.ceil(vec1[2]), h, s, a + 90 )
+		surface.DrawTexturedRectRotated( x + math.ceil(vec2[1]), y + math.ceil(vec2[2]), h, s, a + 90 )
+		surface.DrawTexturedRectRotated( x + math.ceil(vec3[1]), y + math.ceil(vec3[2]), w, s, a )
+		surface.DrawTexturedRectRotated( x + math.ceil(vec4[1]), y + math.ceil(vec4[2]), w, s, a )
 	end
 end
 Obj.Transmit = function( self )
