c3c941fe95a78c518dfec555f217aa1cf21580dc
Jimlad <jamesepope@hotmail.com>
2010-12-03 15:14:06 +0000

    Added wire clutch

---
+++ lua/entities/gmod_wire_clutch/shared.lua
@@ -0,0 +1,11 @@
+ENT.Type            = "anim"
+ENT.Base            = "base_wire_entity"
+
+ENT.PrintName       = "Wire Clutch"
+ENT.Author          = ""
+ENT.Contact         = ""
+ENT.Purpose         = "Allows rotational friction to be varied dynamically"
+ENT.Instructions    = ""
+
+ENT.Spawnable       = false
+ENT.AdminSpawnable  = false
