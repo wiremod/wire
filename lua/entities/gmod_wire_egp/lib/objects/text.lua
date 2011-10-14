e5a84b77fc72e5f67691126307df2029c5a09e2f
Divran <arviddivran@gmail.com>
2011-10-14 19:32:03 +0000

    [title]Added angle to egp text[/title] [E2] Fixed E2 (broken in today's update). Thanks to Python1320 for finding the fix.
    [EGP] Made text objects rotateable (just use XWL:egpAngle(N,N) on it)
    Thanks to Wizard for the code. NOTE: Only works for normal text objects. Does NOT work for TextAlign objects.

--- lua/entities/gmod_wire_egp/lib/objects/text.lua
+++ lua/entities/gmod_wire_egp/lib/objects/text.lua
@@ -7,33 +7,90 @@ Obj.fontid = 1
 Obj.size = 18
 Obj.valign = 0
 Obj.halign = 0
+Obj.angle = 0
+
+local surface_SetTextPos
+local surface_DrawText
+local surface_SetTextColor
+local surface_CreateFont
+local surface_SetFont
+local surface_GetTextSize
+local cam_PushModelMatrix
+local cam_PopModelMatrix
+local mat = Matrix()
+local matAng = Angle(0, 0, 0)
+local matTrans = Vector(0, 0, 0)
+local matScale = Vector(0, 0, 0)
+
+if CLIENT then
+	surface_SetTextPos = surface.SetTextPos
+	surface_DrawText = surface.DrawText
+	surface_SetTextColor = surface.SetTextColor
+	surface_CreateFont = surface.CreateFont
+	surface_SetFont = surface.SetFont
+	surface_GetTextSize = surface.GetTextSize
+
+
+	-- Thanks to Wizard for this rotateable text code
+	cam_PushModelMatrix = cam.PushModelMatrix
+	cam_PopModelMatrix = cam.PopModelMatrix
+	mat = Matrix()
+	mat:Scale(Vector(1, 1, 1))
+	matAng = Angle(0, 0, 0)
+	matTrans = Vector(0, 0, 0)
+end
+
 Obj.Draw = function( self )
 	if (self.text and #self.text>0) then
-		surface.SetTextColor( self.r, self.g, self.b, self.a )
+		surface_SetTextColor( self.r, self.g, self.b, self.a )
 
 		if (!EGP.ValidFonts[self.fontid]) then self.fontid = 1 end
 		local font = "WireEGP_" .. self.size .. "_" .. self.fontid
 		if (!EGP.ValidFonts_Lookup[font]) then
-			surface.CreateFont( EGP.ValidFonts[self.fontid], self.size, 800, true, false, font )
-			table.insert( EGP.ValidFonts, font )
+			surface_CreateFont( EGP.ValidFonts[self.fontid], self.size, 800, true, false, font )
+			EGP.ValidFonts[#EGP.ValidFonts+1]= font
 			EGP.ValidFonts_Lookup[font] = true
 		end
-		surface.SetFont( font )
+		surface_SetFont( font )
 
-		local w,h
-		local x, y = self.x, self.y
-		if (self.halign != 0) then
-			w,h = surface.GetTextSize( self.text )
-			x = x - (w * ((self.halign%10)/2))
+		if self.angle == 0 then
+			local w,h
+			local x, y = self.x, self.y
+			if (self.halign != 0) then
+				w,h = surface_GetTextSize( self.text )
+				x = x - (w * ((self.halign%10)/2))
+			end
+			if (self.valign) then
+				if (!h) then _,h = surface_GetTextSize( self.text ) end
+				y = y - (h * ((self.valign%10)/2))
+			end
+
+			surface_SetTextPos( x, y )
+			surface_DrawText( self.text )
+		else
+			local w,h
+			local x, y = 0,0
+			if (self.halign != 0) then
+				w,h = surface_GetTextSize( self.text )
+				x = (w * ((self.halign%10)/2))
+			end
+			if (self.valign) then
+				if (!h) then _,h = surface_GetTextSize( self.text ) end
+				y = (h * ((self.valign%10)/2))
+			end
+
+			-- Thanks to Wizard for the base to this rotateable text code. I edited it a bit to properly support alignment
+			matAng.y = self.angle
+			mat:SetAngle(matAng)
+			matTrans.x = x
+			matTrans.y = y
+			matTrans:Rotate(matAng)
+			mat:SetTranslation(Vector(self.x,self.y,0)-matTrans)
+			surface_SetTextPos(0, 0)
+			cam_PushModelMatrix(mat)
+				surface_DrawText( self.text )
+			cam_PopModelMatrix()
 		end
-		if (self.valign) then
-			if (!h) then _,h = surface.GetTextSize( self.text ) end
-			y = y - (h * ((self.valign%10)/2))
-		end
-
-		surface.SetTextPos( x, y )
-
-		surface.DrawText( self.text )
 	end
 end
 Obj.Transmit = function( self, Ent, ply )
@@ -46,6 +103,7 @@ Obj.Transmit = function( self, Ent, ply )
 	EGP.umsg.Char( math.Clamp(self.halign,0,2) )
 	EGP.umsg.Short( self.parent )
 	EGP:SendColor( self )
+	EGP.umsg.Short((self.angle%360)*20)
 end
 Obj.Receive = function( self, um )
 	local tbl = {}
@@ -57,8 +115,9 @@ Obj.Receive = function( self, um )
 	tbl.halign = um:ReadChar()
 	tbl.parent = um:ReadShort()
 	EGP:ReceiveColor( tbl, self, um )
+	tbl.angle = um:ReadShort()/20
 	return tbl
 end
 Obj.DataStreamInfo = function( self )
-	return { x = self.x, y = self.y, valign = self.valign, halign = self.halign, size = self.size, r = self.r, g = self.g, b = self.b, a = self.a, text = self.text, fontid = self.fontid, parent = self.parent }
+	return { x = self.x, y = self.y, valign = self.valign, halign = self.halign, size = self.size, r = self.r, g = self.g, b = self.b, a = self.a, text = self.text, fontid = self.fontid, parent = self.parent, angle = self.angle }
 end
