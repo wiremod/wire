323c965e86470587962c7852cfb55bb2d98afde5
Nebual <ben1120@gmail.com>
2012-11-02 19:09:05 -0700

    Moved Laser Pointer Receiver + Water Sensor stools to WireToolObj format Changed Laser Pointer Receiver SENT to Gmod13's new concise onefile format

--- lua/wire/stools/detection.lua
+++ lua/wire/stools/detection.lua
@@ -63,18 +63,11 @@ do -- wire_gps
 		end
 	end
 
+	TOOL.Model = "models/beer/wiremod/gps.mdl"
 	TOOL.ClientConVar = {
-		model = "models/beer/wiremod/gps.mdl",
+		model = TOOL.Model,
 		modelsize = "",
 	}
-	
-	function TOOL:GetModel()
-		local model = string.sub(self:GetClientInfo( "model" ), 1, -5) .. self:GetClientInfo( "modelsize" ) .. string.sub(self:GetClientInfo( "model" ), -4)
-		if not self:CheckValidModel(model) then return model
-		elseif not self:CheckValidModel(self:GetClientInfo( "model" )) then return self:GetClientInfo( "model" )
-		else return "models/beer/wiremod/gps.mdl"
-		end
-	end
 
 	function TOOL.BuildCPanel(panel)
 		panel:Help("#Tool.wire_gps.desc")
@@ -87,7 +80,7 @@ do -- wire_gps
 					["nano"] = { wire_gps_modelsize = "_nano" }
 				}
 		})
-		ModelPlug_AddToCPanel(panel, "GPS", "wire_gps", "#ToolWireIndicator_Model")
+		ModelPlug_AddToCPanel(panel, "GPS", "wire_gps")
 	end
 end -- wire_gps
 
@@ -116,15 +109,93 @@ do -- wire_gyroscope
 		end
 	end
 
+	TOOL.Model = "models/bull/various/gyroscope.mdl"
 	TOOL.ClientConVar = {
-		model = "models/bull/various/gyroscope.mdl",
+		model = TOOL.Model,
 		out180 = 0,
 	}
 
 	function TOOL.BuildCPanel(panel)
 		panel:Help("#Tool.wire_gyroscope.desc")
-		ModelPlug_AddToCPanel(panel, "gyroscope", "wire_gyroscope", "#ToolWireGyroscope_Model")
+		ModelPlug_AddToCPanel(panel, "gyroscope", "wire_gyroscope")
 		panel:CheckBox("#Tool.wire_gyroscope.out180","wire_gyroscope_out180")
 	end
 end -- wire_gyroscope
 
+do
+	WireToolSetup.open( "las_reciever", "Laser Pointer Receiver", "gmod_wire_las_reciever", nil, "Laser Pointer Receiver" )
+
+	if CLIENT then
+		language.Add( "Tool.wire_las_reciever.name", "Laser Receiver Tool (Wire)" )
+		language.Add( "Tool.wire_las_reciever.desc", "Spawns a constant laser receiver prop for use with the wire system." )
+		language.Add( "Tool.wire_las_reciever.0", "Primary: Create/Update Laser Receiver" )
+		language.Add( "WireILaserRecieverTool_ilas_reciever", "Laser Receiver:" )
+		language.Add( "sboxlimit_wire_las_recievers", "You've hit laser receivers limit!" )
+	end
+	WireToolSetup.BaseLang()
+
+	if SERVER then
+		CreateConVar('sbox_maxwire_las_recievers', 20)
+		
+		function TOOL:GetConVars() end
+
+		function TOOL:MakeEnt( ply, model, Ang, trace )
+			return MakeWireLaserReciever( ply, trace.HitPos, Ang, model, self:GetConVars() )
+		end
+	end
+
+	TOOL.Model =  "models/jaanus/wiretool/wiretool_range.mdl"
+	TOOL.ClientConVar = {
+		model = TOOL.Model,
+	}
+
+	function TOOL.BuildCPanel(panel)
+		panel:Help("#Tool.wire_las_reciever.desc")
+		ModelPlug_AddToCPanel(panel, "Misc_Tools", "wire_las_reciever")
+	end
+end
+
+do
+	WireToolSetup.open( "watersensor", "Water Sensor", "gmod_wire_watersensor", nil, "Water Sensors" )
+
+	if CLIENT then
+		language.Add( "Tool.wire_watersensor.name", "Water Sensor Tool (Wire)" )
+		language.Add( "Tool.wire_watersensor.desc", "Spawns a constant Water Sensor prop for use with the wire system." )
+		language.Add( "Tool.wire_watersensor.0", "Primary: Create/Update Water Sensor" )
+		language.Add( "Tool.wire_watersensor.modelsize", "Model Size (if available)" )
+		language.Add( "WireWatersensorTool_watersensor", "Water Sensor:" )
+		language.Add( "sboxlimit_wire_watersensors", "You've hit Water Sensors limit!" )
+	end
+	WireToolSetup.BaseLang()
+
+	if SERVER then
+		CreateConVar('sbox_maxwire_watersensors', 10)
+		ModelPlug_Register("WaterSensor")
+		
+		function TOOL:GetConVars() end
+
+		function TOOL:MakeEnt( ply, model, Ang, trace )
+			return MakeWireWaterSensor( ply, trace.HitPos, Ang, model, self:GetConVars() )
+		end
+	end
+
+	TOOL.Model = "models/beer/wiremod/watersensor.mdl"
+	TOOL.ClientConVar = {
+		model = TOOL.Model,
+		modelsize = "",
+	}
+
+	function TOOL.BuildCPanel(panel)
+		panel:Help("#Tool.wire_watersensor.desc")
+		panel:Help("#Tool.wire_watersensor.modelsize")
+		panel:AddControl("ListBox", {
+			Label = "Model Size",
+			Options = {
+					["normal"] = { wire_watersensor_modelsize = "" },
+					["mini"] = { wire_watersensor_modelsize = "_mini" },
+					["nano"] = { wire_watersensor_modelsize = "_nano" }
+				}
+		})
+		ModelPlug_AddToCPanel(panel, "WaterSensor", "wire_watersensor")
+	end
+end
