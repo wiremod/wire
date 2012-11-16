3adc17104f31959d69339659059416363e6ec387
Nebual <ben1120@gmail.com>
2012-11-16 14:48:39 -0800

    Wire Keyboard: Updated stool to WireToolObj Made the checkboxes function clearer to help with #142

--- lua/entities/gmod_wire_keyboard/init.lua
+++ lua/entities/gmod_wire_keyboard/init.lua
@@ -19,9 +19,6 @@ for i=97,122 do -- a -> z
 	All_Enums[i] = _G["KEY_" .. string.upper(string.char(i))]
 end
 
-------------------------------------------------------------------------------------------
--- Initialize
-------------------------------------------------------------------------------------------
 function ENT:Initialize()
 	self:PhysicsInit( SOLID_VPHYSICS )
 	self:SetMoveType( MOVETYPE_VPHYSICS )
@@ -40,9 +37,6 @@ function ENT:Initialize()
 	WireLib.TriggerOutput( self, "InUse", 0 )
 end
 
-------------------------------------------------------------------------------------------
--- TriggerInput
-------------------------------------------------------------------------------------------
 function ENT:TriggerInput( name, value )
 	if name == "Kick the bastard out of keyboard" then
 		self.Locked = (value ~= 0)
@@ -50,9 +44,6 @@ function ENT:TriggerInput( name, value )
 	end
 end
 
-------------------------------------------------------------------------------------------
--- ReadCell
-------------------------------------------------------------------------------------------
 function ENT:ReadCell( Address )
 	if Address >= 0 and Address < 32 then
 		return self.Buffer[Address] or 0
@@ -66,9 +57,6 @@ function ENT:ReadCell( Address )
 	return 0
 end
 
-------------------------------------------------------------------------------------------
--- WriteCell
-------------------------------------------------------------------------------------------
 function ENT:WriteCell( Address, value )
 	if Address == 0 then
 		return self:Switch( -1, nil )
@@ -79,14 +67,10 @@ function ENT:WriteCell( Address, value )
 	return false
 end
 
-------------------------------------------------------------------------------------------
--- PlayerAttach
-------------------------------------------------------------------------------------------
 function ENT:PlayerAttach( ply )
-	if not ply or not ply:IsValid() then return end -- Invalid player
-	if self.ply and self.ply:IsValid() then return end -- If the keyboard is already in use, don't attach the player
+	if not IsValid(ply) or IsValid(self.ply) then return end -- If the keyboard is already in use, don't attach the player
 
-	if ply.WireKeyboard and ply.WireKeyboard:IsValid() then -- If the player is already using a different keyboard
+	if IsValid(ply.WireKeyboard) then -- If the player is already using a different keyboard
 		if ply.WireKeyboard == self then return end -- If the keyboard is this keyboard, don't re-attach the player
 		ply.WireKeyboard:PlayerDetach() -- If it's another keyboard, detach the player from that keyboard first
 	end
@@ -97,18 +81,14 @@ function ENT:PlayerAttach( ply )
 	-- Store player
 	self.ply = ply
 
-	-- Update wire outputs
 	WireLib.TriggerOutput( self, "User", ply )
 	WireLib.TriggerOutput( self, "InUse", 1 )
-
-	-- Update status text
 	self:SetOverlayText("In use by " .. ply:Nick())
 
 	-- Block keyboard input
 	umsg.Start( "wire_keyboard_blockinput", ply ) umsg.End()
 
-	-- Check for pod
-	if self.Pod and self.Pod:IsValid() then
+	if IsValid(self.Pod) then
 		ply:ChatPrint( "This pod is linked to a keyboard - press ALT to leave." )
 	else
 		ply:ChatPrint( "Keyboard turned on - press ALT to leave." )
@@ -128,7 +108,7 @@ function ENT:PlayerAttach( ply )
 end
 
 function ENT:Use( ply )
-	if self.Pod and self.Pod:IsValid() then
+	if IsValid(self.Pod) then
 		ply:ChatPrint( "This keyboard is linked to a pod. Please use the pod instead." )
 		return
 	end
@@ -139,42 +119,34 @@ end
 function ENT:OnRemove()
 	self:LinkPod( nil, true )
 	self:PlayerDetach()
+	self.BaseClass.OnRemove(self)
 end
 
-------------------------------------------------------------------------------------------
--- PlayerDetach
-------------------------------------------------------------------------------------------
 function ENT:PlayerDetach()
-	if not self.ply or not self.ply:IsValid() then return end -- If the keyboard isn't linked to a player
+	if not IsValid(self.ply) then return end -- If the keyboard isn't linked to a player
 
-	-- Clear values
 	self.ply.WireKeyboard = nil
 	self.ply = nil
 
-	-- Clear wire outputs
 	WireLib.TriggerOutput( self, "User", nil )
 	WireLib.TriggerOutput( self, "InUse", 0 )
 
-	-- Update status text
-	self:SetOverlayText("Not in use." )
+	self:SetOverlayText("Not in use")
 
 	-- Kick player out of vehicle, if in one
-	if self.Pod and self.Pod:IsValid() and self.Pod:GetDriver() and self.Pod:GetDriver():IsValid() then
+	if IsValid(self.Pod) and IsValid(self.Pod:GetDriver()) then
 		self.Pod:GetDriver():ExitVehicle()
 	end
 end
 
-------------------------------------------------------------------------------------------
--- LinkPod
-------------------------------------------------------------------------------------------
 function ENT:LinkPod( pod, silent )
-	if not pod or not pod:IsValid() then
-		if self.Pod and self.Pod:IsValid() then
+	if not IsValid(pod) then
+		if IsValid(self.Pod) then
 			self.Pod.WireKeyboard = nil
 			self.Pod = nil
 		end
 	else
-		if self.Pod and self.Pod:IsValid() and self.Pod == pod then return end
+		if IsValid(self.Pod) and self.Pod == pod then return end
 
 		pod.WireKeyboard = self
 		self.Pod = pod
@@ -182,20 +154,17 @@ function ENT:LinkPod( pod, silent )
 end
 
 hook.Add( "PlayerEnteredVehicle", "Wire_Keyboard_PlayerEnteredVehicle", function( ply, pod )
-	if pod.WireKeyboard and pod.WireKeyboard:IsValid() then
+	if IsValid(pod.WireKeyboard) then
 		pod.WireKeyboard:PlayerAttach( ply )
 	end
 end)
 
 hook.Add("PlayerLeaveVehicle", "wire_keyboard_PlayerLeaveVehicle", function( ply, pod )
-	if pod.WireKeyboard and pod.WireKeyboard:IsValid() then
+	if IsValid(pod.WireKeyboard) then
 		pod.WireKeyboard:PlayerDetach()
 	end
 end)
 
-------------------------------------------------------------------------------------------
--- Switch
-------------------------------------------------------------------------------------------
 function ENT:Switch( key, key_enum, on )
 	if not key then return false end -- Invalid key
 	if key and key_enum then All_Enums[key] = key_enum end -- Add to list of keys
@@ -261,9 +230,7 @@ function ENT:Switch( key, key_enum, on )
 end
 
 concommand.Add("wire_keyboard_press", function(ply, cmd, args)
-	if 	not ply.WireKeyboard or not ply.WireKeyboard:IsValid() or -- If the player isn't using a keyboard
-		not ply.WireKeyboard.ply or not ply.WireKeyboard.ply:IsValid() or ply.WireKeyboard.ply ~= ply then -- If the attached player does not match this player
-
+	if not IsValid(ply.WireKeyboard) or not IsValid(ply.WireKeyboard.ply) or ply.WireKeyboard.ply ~= ply then -- If the attachedplayer isn't using a keyboard or does not match this player
 		umsg.Start( "wire_keyboard_releaseinput", ply ) umsg.End() -- Release their input in case they got stuck or something
 		return
 	end
@@ -286,15 +253,38 @@ concommand.Add("wire_keyboard_press", function(ply, cmd, args)
 	keyboard:Switch( ascii, key_enum, args[1] == "p" )
 end)
 
-------------------------------------------------------------------------------------------
--- Duplication support
-------------------------------------------------------------------------------------------
+function ENT:Setup(autobuffer)
+	self.AutoBuffer = autobuffer
+end
+
+if (SERVER) then
+	function MakeWireKeyboard( pl, Pos, Ang, model, autobuffer )
+		if ( !pl:CheckLimit( "wire_keyboards" ) ) then return false end
+
+		local wire_keyboard = ents.Create( "gmod_wire_keyboard" )
+		if (!wire_keyboard:IsValid()) then return false end
+
+		wire_keyboard:SetAngles( Ang )
+		wire_keyboard:SetPos( Pos )
+		wire_keyboard:SetModel( Model(model or "models/jaanus/wiretool/wiretool_input.mdl") )
+		wire_keyboard:Spawn()
+
+		wire_keyboard:SetPlayer( pl )
+		wire_keyboard.pl = pl
+		wire_keyboard:Setup(autobuffer)
+
+		pl:AddCount( "wire_keyboards", wire_keyboard )
+
+		return wire_keyboard
+	end
+	duplicator.RegisterEntityClass("gmod_wire_keyboard", MakeWireKeyboard, "Pos", "Ang", "Model", "AutoBuffer")
+end
+
 function ENT:BuildDupeInfo()
 	local info = self.BaseClass.BuildDupeInfo(self) or {}
-	if self.Pod and self.Pod:IsValid() then
+	if IsValid(self.Pod) then
 	    info.pod = self.Pod:EntIndex()
 	end
-	info.autobuffer = self.AutoBuffer
 	return info
 end
 
