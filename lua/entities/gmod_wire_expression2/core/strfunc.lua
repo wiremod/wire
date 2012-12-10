cf19ffcccd2ec10df10f6a47e5d5c6a3912ad9e0
Divran <arviddivran@gmail.com>
2012-12-10 22:49:49 +0100

    Optimized callable strings with a cache for the last 20 calls

--- lua/entities/gmod_wire_expression2/core/strfunc.lua
+++ lua/entities/gmod_wire_expression2/core/strfunc.lua
@@ -12,21 +12,37 @@ local function checkFuncName( self, funcname )
 	end
 end
 
+registerCallback("construct", function(self) self.strfunc_cache = {} end)
+
+local insert = table.insert
+local concat = table.concat
 local function findFunc( self, funcname, typeids, typeids_str )
-	local func, func_return_type = checkFuncName( self, funcname .. "()" )
+	local func, func_return_type
+	
+	self.prf = self.prf + 1
+	
+	local str = funcname .. "(" .. typeids_str .. ")"
+	for i=1,#self.strfunc_cache do
+		local t = self.strfunc_cache[i]
+		if t[1] == str then
+			return t[2], t[3]
+		end
+	end
+	
+	self.prf = self.prf + 2
 	
 	if #typeids > 0 then
 		if not func then
-			func, func_return_type = checkFuncName( self, funcname .. "(" .. typeids_str .. ")" )
+			func, func_return_type = checkFuncName( self, str )
 		end
 		
 		if not func then
-			func, func_return_type = checkFuncName( self, funcname .. "(" .. typeids[1] .. ":" .. table.concat(typeids,"",2) .. ")" )
+			func, func_return_type = checkFuncName( self, funcname .. "(" .. typeids[1] .. ":" .. concat(typeids,"",2) .. ")" )
 		end
 		
 		if not func then
 			for i=#typeids,1,-1 do
-				func, func_return_type = checkFuncName( self, funcname .. "(" .. table.concat(typeids,"",1,i) .. "...)" )
+				func, func_return_type = checkFuncName( self, funcname .. "(" .. concat(typeids,"",1,i) .. "...)" )
 				if func then break end
 			end
 			
@@ -37,7 +53,7 @@ local function findFunc( self, funcname, typeids, typeids_str )
 		
 		if not func then
 			for i=#typeids,2,-1 do
-				func, func_return_type = checkFuncName( self, funcname .. "(" .. typeids[1] .. ":" ..  table.concat(typeids,"",2,i) .. "...)" )
+				func, func_return_type = checkFuncName( self, funcname .. "(" .. typeids[1] .. ":" ..  concat(typeids,"",2,i) .. "...)" )
 				if func then break end
 			end
 			
@@ -45,10 +61,20 @@ local function findFunc( self, funcname, typeids, typeids_str )
 				func, func_return_type = checkFuncName( self, funcname .. "(" .. typeids[1] .. ":...)" )
 			end
 		end
+	else
+		func, func_return_type = checkFuncName( self, funcname .. "()" )
+	end
+	
+	if func then
+		local t = { str, func, func_return_type }
+		insert( self.strfunc_cache, 1, t )
+		if #self.strfunc_cache == 21 then self.strfunc_cache[21] = nil end
 	end
 	
 	return func, func_return_type
 end
+
+__e2setcost(6)
 
 registerOperator( "sfun", "", "", function(self, args)
 	local op1, funcargs, typeids, typeids_str, returntype = args[2], args[3], args[4], args[5], args[6]
@@ -68,3 +94,5 @@ registerOperator( "sfun", "", "", function(self, args)
 		func( self, funcargs )
 	end
 end)
+
+__e2setcost(nil)
