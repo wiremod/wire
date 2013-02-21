7a53033e55ba169ed8c36a545a0b3ab5c840d924
Nebual <ben1120@gmail.com>
2013-02-21 01:40:44 -0800

    Keyboard: Fixed blocking of mouse buttons Fixes #231

--- lua/entities/gmod_wire_keyboard/cl_init.lua
+++ lua/entities/gmod_wire_keyboard/cl_init.lua
@@ -3,7 +3,9 @@ include("remap.lua") -- For stools/keyboard.lua's layout selector
 
 net.Receive("wire_keyboard_blockinput", function(netlen)
 	if net.ReadBit() ~= 0 then 
-		hook.Add("PlayerBindPress", "wire_keyboard_blockinput", function(ply, bind, pressed) return true end)
+		hook.Add("PlayerBindPress", "wire_keyboard_blockinput", function(ply, bind, pressed) 
+			return (bind ~= "+attack" and bind ~= "+attack2") or nil -- true for all keys except the mouse, to block keyboard actions while typing
+		end)
 	else
 		hook.Remove("PlayerBindPress", "wire_keyboard_blockinput")
 	end
