267f4a2a81c4ef6038a2ad874435a484c4b453de
Nebual <ben1120@gmail.com>
2013-03-08 12:52:50 -0800

    E2: Added getCode [S] getCodeIncludes [T] E:remoteSetCode(ST) Credit to @Divran, #233

--- lua/entities/gmod_wire_expression2/core/custom/remoteupload.lua
+++ lua/entities/gmod_wire_expression2/core/custom/remoteupload.lua
@@ -32,3 +32,37 @@ e2function void entity:remoteSetCode( string code )
 	
 	this:Setup( code, {} )
 end
+
+e2function void entity:remoteSetCode( string main, table includes )
+	if not this or not this:IsValid() or this:GetClass() ~= "gmod_wire_expression2" then return end
+	if not E2Lib.isOwner( self, this ) then return end
+
+	local luatable = {}
+	
+	for k,v in pairs( includes.s ) do
+		if includes.stypes[k] == "s" and k ~= "main" then
+			luatable[k] = v
+		else
+			error( "Non-string value given to remoteSetCode", 2 )
+		end
+	end
+	
+	this:Setup( main, luatable )
+end
+
+e2function string getCode()
+	local main, _ = self.entity:GetCode()
+	return main
+end
+
+e2function table getCodeIncludes()
+	local _, includes = self.entity:GetCode()
+	local e2table = {n={},ntypes={},s={},stypes={},size=0}
+	
+	for k,v in pairs( includes ) do
+		e2table.s[k] = v
+		e2table.stypes[k] = "s"
+	end
+	
+	return e2table
+end
