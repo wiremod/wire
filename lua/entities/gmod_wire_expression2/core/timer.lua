d3f42643b6741c09a60bedafc6aa5990919a7b9f
Nebual <ben1120@gmail.com>
2013-06-03 21:45:19 -0700

    E2 Timers: Fixed a major memory leak involving looped timers Garrys current implementation of timer.Create does not properly garbage collect if the timer is recreated via timer.Create during its execution. Fixes #284 This is a revert of my previous commit 03274b23472d417714fe741cc52bd799b92d428c

--- lua/entities/gmod_wire_expression2/core/timer.lua
+++ lua/entities/gmod_wire_expression2/core/timer.lua
@@ -24,9 +24,16 @@ end
 local function AddTimer(self, name, delay)
 	if delay < 10 then delay = 10 end
 	
-	timer.Create("e2_" .. self.data['timer'].timerid .. "_" .. name, delay/1000, 1, function()
-		Execute(self, name)
-	end)
+	if runner == name then
+		timer.Adjust("e2_" .. self.data['timer'].timerid .. "_" .. name, delay/1000, 1, function()
+			Execute(self, name)
+		end)
+		timer.Start("e2_" .. self.data['timer'].timerid .. "_" .. name)
+	elseif !self.data['timer'].timers[name] then
+		timer.Create("e2_" .. self.data['timer'].timerid .. "_" .. name, delay/1000, 1, function()
+			Execute(self, name)
+		end)
+	end
 
 	self.data['timer'].timers[name] = true
 end
