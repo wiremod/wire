26f7df8f09e8333b287e1fcfb7425638638ce4d6
TomyLobo <stabelweb@gmx.de>
2013-07-21 09:12:38 +0200

    Removed an extraneous "Vector" that broke debug mode.
    tsk tsk, @Nebual

--- lua/entities/gmod_wire_expression2/core/vector.lua
+++ lua/entities/gmod_wire_expression2/core/vector.lua
@@ -24,7 +24,7 @@ registerType("vector", "v", { 0, 0, 0 },
 	nil,
 	function(self, output) return Vector(output[1], output[2], output[3]) end,
 	function(retval)
-		if isvector(retval) "Vector" then return end
+		if isvector(retval) then return end
 		if !istable(retval) then error("Return value is neither a Vector nor a table, but a "..type(retval).."!",0) end
 		if #retval ~= 3 then error("Return value does not have exactly 3 entries!",0) end
 	end,
