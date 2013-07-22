6705a4f473e98693dda5ad13988ba75110c71f68
Matt Eisan <mattjeisan@gmail.com>
2013-07-22 02:49:40 -0400

    Update init.lua
    Fixed giving users remote controllers

--- lua/entities/gmod_wire_adv_pod/init.lua
+++ lua/entities/gmod_wire_adv_pod/init.lua
@@ -469,9 +469,8 @@ function ENT:Use( User, caller )
 		if not User:GetEyeTrace().Entity or User:GetEyeTrace().Entity ~= self then return end
 
 		if not User:GetWeapon("RemoteController"):IsValid()  then
-			if hook.Call( "PlayerGiveSWEP", User, "RemoteController" ) then 
-				User:Give( "RemoteController" ) 
-			end 
+			if not hook.Call( "PlayerGiveSWEP", User, "RemoteController" ) then return end 
+			User:Give( "RemoteController" ) 
 		end
 
 		User:GetWeapon("RemoteController").Linked = self
