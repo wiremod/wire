46754d7905c6d9c9e897d365b6529d2ccde263aa
Natrim <maly@kinet.cz>
2013-07-31 15:05:21 +0200

    format these

--- lua/wire/expression2/core/builtin/cl_console.lua
+++ lua/wire/expression2/core/builtin/cl_console.lua
@@ -1,24 +1,24 @@
 local convars = {
-	wire_expression2_concmd = 0,
-	wire_expression2_concmd_whitelist = "",
+    wire_expression2_concmd = 0,
+    wire_expression2_concmd_whitelist = "",
 }
 
 local function CreateCVars()
-	for name,default in pairs(convars) do
-		current_cvar = CreateClientConVar(name, default, true, true)
-		local value = current_cvar:GetString() or default
-		RunConsoleCommand(name, value)
-	end
+    for name, default in pairs(convars) do
+        current_cvar = CreateClientConVar(name, default, true, true)
+        local value = current_cvar:GetString() or default
+        RunConsoleCommand(name, value)
+    end
 end
 
 if CanRunConsoleCommand() then
-	CreateCVars()
+    CreateCVars()
 else
-	hook.Add("OnEntityCreated", "wire_expression2_console", function(ent)
-		if not IsValid(ent) then return end
-		if ent ~= LocalPlayer() then return end
+    hook.Add("OnEntityCreated", "wire_expression2_console", function(ent)
+        if not IsValid(ent) then return end
+        if ent ~= LocalPlayer() then return end
 
-		CreateCVars()
-		timer.Create("Remove_wire_expression2_console_Hook",0,1,function() hook.Remove("OnEntityCreated", "wire_expression2_console") end) -- Removing it immediately may hurt the hook loop
-	end)
+        CreateCVars()
+        timer.Create("Remove_wire_expression2_console_Hook", 0, 1, function() hook.Remove("OnEntityCreated", "wire_expression2_console") end) -- Removing it immediately may hurt the hook loop
+    end)
 end
