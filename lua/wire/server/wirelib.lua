f53d7fcd1ea92e20250010cd4bacc53985097de9
Divran <arviddivran@gmail.com>
2013-08-14 13:53:25 +0200

    removed extended from wirelib.lua
    This probably breaks wire adv 1, and it does break any old dupe that
    uses wirelinks created by either wire adv 1 or the wirelink tool.

--- lua/wire/server/wirelib.lua
+++ lua/wire/server/wirelib.lua
@@ -151,7 +151,6 @@ function WireLib.CreateSpecialOutputs(ent, names, types, descs)
 	descs = descs or {}
 	local ent_ports = {}
 	ent.Outputs = ent_ports
-	if ent.extended then table.insert(names, "link") table.insert(types, "WIRELINK") end
 	for n,v in pairs(names) do
 		local name, desc, tp = ParsePortName(v, types[n] or "NORMAL", descs and descs[n])
 
@@ -177,7 +176,6 @@ function WireLib.CreateSpecialOutputs(ent, names, types, descs)
 	end
 
 	WireLib._SetOutputs(ent)
-	if ent.extended then table.remove(names) table.remove(types) end
 
 	return ent_ports
 end
@@ -244,7 +242,6 @@ function WireLib.AdjustSpecialOutputs(ent, names, types, descs)
 	types = types or {}
 	descs = descs or {}
 	local ent_ports = ent.Outputs or {}
-	if ent.extended then table.insert(names, "link") table.insert(types, "WIRELINK") end
 	for n,v in ipairs(names) do
 		local name, desc, tp = ParsePortName(v, types[n] or "NORMAL", descs and descs[n])
 
@@ -298,7 +295,6 @@ function WireLib.AdjustSpecialOutputs(ent, names, types, descs)
 	end
 
 	WireLib._SetOutputs(ent)
-	if ent.extended then table.remove(names) table.remove(types) end
 
 	return ent_ports
 end
@@ -627,12 +623,7 @@ function Wire_Link_Node(idx, ent, pos)
 end
 
 
-function Wire_Link_End(idx, ent, pos, oname, pl)
-	if oname == "link" and ent.extended == nil then
-		ent.extended = true
-		RefreshSpecialOutputs(ent)
-	end
-	
+function Wire_Link_End(idx, ent, pos, oname, pl)	
 	if !CurLink[idx] then return end
 
 	if !IsValid(CurLink[idx].Dst) then return end
@@ -823,16 +814,11 @@ function WireLib.BuildDupeInfo( Ent )
 			end
 		end
 	end
-	if Ent.extended then info.extended = true end
 
 	return info
 end
 
 function WireLib.ApplyDupeInfo( ply, ent, info, GetEntByID )
-	if info.extended and ent.extended == nil then
-		ent.extended = true
-		RefreshSpecialOutputs(ent)
-	end
 	local idx = 0
 	if IsValid(ply) then idx = ply:UniqueID() end -- Map Save loading does not have a ply
 	if (info.Wires) then
