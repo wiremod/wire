dec103ac1daa07c353f7fcede0019f7167980009
Jack37 <hybridx@azet.sk>
2013-08-16 20:41:13 +0200

    Fixed RT offset for small tv model

--- lua/wire/wiremonitors.lua
+++ lua/wire/wiremonitors.lua
@@ -159,22 +159,22 @@ end
 
 WireGPU_FromBox_Helper("Workspace 002", "models/props_lab/workspace002.mdl", Vector(-20, 49, -34), Vector(16.2, 84, -30.5), Angle(0, 133.34, 59.683))
 
--- Offset front, offset up, offset right, resolution/scale                                OF   OU     OR   SCALE   LOWX      HIGHX    LOWY     HIGHY   ROTATE90
-WireGPU_AddMonitor("Small TV",          "models/props_lab/monitor01b.mdl",                6.4, 0.45 , 1.0, 0.018 , -5.535  , 3.5    , -4.1   , 5.091 )
-WireGPU_AddMonitor("Monitor Small",     "models/kobilica/wiremonitorsmall.mdl",           0.3, 5.0  , 0  , 0.0175, -4.4    , 4.5    , 0.6    , 9.5   )
-WireGPU_AddMonitor("LCD Monitor (4:3)", "models/props/cs_office/computer_monitor.mdl",    3.3, 16.7 , 0  , 0.031 , -10.5   , 10.5   , 8.6    , 24.7  )
-WireGPU_AddMonitor("Monitor Big",       "models/kobilica/wiremonitorbig.mdl",             0.2, 13   , 0  , 0.045 , -11.5   , 11.6   , 1.6    , 24.5  )
-WireGPU_AddMonitor("Plasma TV (4:3)",   "models/blacknecro/tv_plasma_4_3.mdl",            0.1, -0.5 , 0  , 0.082 , -27.87  , 27.87  , -20.93 , 20.93 )
-WireGPU_AddMonitor("Plasma TV (16:10)", "models/props/cs_office/tv_plasma.mdl",           6.1, 18.93, 0  , 0.065 , -28.5   , 28.5   , 2      , 36    )
-WireGPU_AddMonitor("Billboard",         "models/props/cs_assault/billboard.mdl",          1  , 0    , 0  , 0.23  , -110.512, 110.512, -57.647, 57.647)
+-- Offset front, offset up, offset right, resolution/scale                                OF    OU     OR   SCALE   LOWX      HIGHX    LOWY     HIGHY   ROTATE90
+WireGPU_AddMonitor("Small TV",          "models/props_lab/monitor01b.mdl",                6.53, 0.45 , 1.0, 0.0185, -5.535  , 3.5    , -4.1   , 5.091 )
+WireGPU_AddMonitor("Monitor Small",     "models/kobilica/wiremonitorsmall.mdl",           0.3 , 5.0  , 0  , 0.0175, -4.4    , 4.5    , 0.6    , 9.5   )
+WireGPU_AddMonitor("LCD Monitor (4:3)", "models/props/cs_office/computer_monitor.mdl",    3.3 , 16.7 , 0  , 0.031 , -10.5   , 10.5   , 8.6    , 24.7  )
+WireGPU_AddMonitor("Monitor Big",       "models/kobilica/wiremonitorbig.mdl",             0.2 , 13   , 0  , 0.045 , -11.5   , 11.6   , 1.6    , 24.5  )
+WireGPU_AddMonitor("Plasma TV (4:3)",   "models/blacknecro/tv_plasma_4_3.mdl",            0.1 , -0.5 , 0  , 0.082 , -27.87  , 27.87  , -20.93 , 20.93 )
+WireGPU_AddMonitor("Plasma TV (16:10)", "models/props/cs_office/tv_plasma.mdl",           6.1 , 18.93, 0  , 0.065 , -28.5   , 28.5   , 2      , 36    )
+WireGPU_AddMonitor("Billboard",         "models/props/cs_assault/billboard.mdl",          1   , 0    , 0  , 0.23  , -110.512, 110.512, -57.647, 57.647)
 
-WireGPU_AddMonitor("Cube 1x1x1",        "models/hunter/blocks/cube1x1x1.mdl",             24 , 0    , 0  , 0.09  , -48     , 48     , -48    , 48    )
-WireGPU_AddMonitor("Panel 1x1",         "models/hunter/plates/plate1x1.mdl",              0  , 1.7  , 0  , 0.09  , -48     , 48     , -48    , 48    , true)
-WireGPU_AddMonitor("Panel 2x2",         "models/hunter/plates/plate2x2.mdl",              0  , 1.7  , 0  , 0.182 , -48     , 48     , -48    , 48    , true)
-WireGPU_AddMonitor("Panel 0.5x0.5",     "models/hunter/plates/plate05x05.mdl",            0  , 1.7  , 0  , 0.045 , -48     , 48     , -48    , 48    , true)
+WireGPU_AddMonitor("Cube 1x1x1",        "models/hunter/blocks/cube1x1x1.mdl",             24  , 0    , 0  , 0.09  , -48     , 48     , -48    , 48    )
+WireGPU_AddMonitor("Panel 1x1",         "models/hunter/plates/plate1x1.mdl",              0   , 1.7  , 0  , 0.09  , -48     , 48     , -48    , 48    , true)
+WireGPU_AddMonitor("Panel 2x2",         "models/hunter/plates/plate2x2.mdl",              0   , 1.7  , 0  , 0.182 , -48     , 48     , -48    , 48    , true)
+WireGPU_AddMonitor("Panel 0.5x0.5",     "models/hunter/plates/plate05x05.mdl",            0   , 1.7  , 0  , 0.045 , -48     , 48     , -48    , 48    , true)
 
-WireGPU_AddMonitor("Tray",              "models/props/cs_militia/reload_bullet_tray.mdl", 0  , 0.8  , 0  , 0.009 , 0       , 100    , 0      , 60    , true)
--- Offset front, offset up, offset right, resolution/scale                                OF   OU     OR   SCALE   LOWX      HIGHX    LOWY     HIGHY   ROTATE90
+WireGPU_AddMonitor("Tray",              "models/props/cs_militia/reload_bullet_tray.mdl", 0   , 0.8  , 0  , 0.009 , 0       , 100    , 0      , 60    , true)
+-- Offset front, offset up, offset right, resolution/scale                                OF    OU     OR   SCALE   LOWX      HIGHX    LOWY     HIGHY   ROTATE90
 --WireGPU_AddMonitor("LED Board (1:1)",   "models/blacknecro/ledboard60.mdl",               6.1, 18.5 , 11 , 0.065 , -60     , 60     , -60    , 60    ) -- broken
 
 WireGPU_FromBox("TF2 Red billboard", "models/props_mining/billboard001.mdl", Vector(0,-168,0), Vector(3,168,192), false)
