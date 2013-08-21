1bf3a91ef1942c0bf092a2199021d09fa65af64d
Jack37 <hybridx@azet.sk>
2013-08-21 18:54:11 +0200

    Added new SEnt: Wired Motor Wired Motor by _Kilburn, promoted from Unofficial wire extras. Motor with controller to controll its speed using Mul input. The final speed is motor torque multiplied by the number in Mul input. https://github.com/wiremod/wire-extras/issues/14

---
+++ lua/entities/gmod_wire_motor.lua
@@ -0,0 +1,121 @@
+AddCSLuaFile()
+DEFINE_BASECLASS( "base_wire_entity" )
+ENT.PrintName = "Motor"
+ENT.WireDebugName = "Motor"
+
+if CLIENT then
+	ENT.RenderGroup = RENDERGROUP_BOTH
+	return
+end
+
+function ENT:Initialize()
+	self.BaseClass.Initialize(self)
+	self.Inputs = Wire_CreateInputs( self, { "Mul" } )
+end
+
+function ENT:SetConstraint( c )
+	self.constraint = c
+	self.Mul = 0
+	self:ShowOutput()
+end
+
+function ENT:SetAxis( a )
+	self.axis = a
+end
+
+function ENT:TriggerInput(iname, value)
+	if iname == "Mul" then
+		self.Mul = value
+		self:ShowOutput()
+		local Motor = self.constraint
+		if not IsValid( Motor ) then
+			return false
+		end
+		Motor:Fire( "Scale", value, 0 )
+		Motor:Fire( "Activate", "" , 0 )
+	end
+end
+
+function ENT:ShowOutput()
+	if self.constraint and IsValid( self.constraint ) then
+		self:SetOverlayText( "Torque: "..math.floor( self.constraint:GetTable().torque * self.Mul ) )
+	end
+end
+
+//need for the const to find the controller after being duplicator pasted
+WireMotorTracking = {}
+	
+function MakeWireMotorController( pl, Pos, Ang, MyEntId, model, const, axis )
+	local controller = WireLib.MakeWireEnt(pl, {Class = "gmod_wire_motor", Pos=Pos, Angle=Ang, Model=model})
+	if not IsValid(controller) then return end
+			
+	if not const then
+		WireMotorTracking[ MyEntId ] = controller
+	else
+		controller.MyId = controller:EntIndex()
+		const.MyCrtl = controller:EntIndex()
+		controller:SetConstraint( const ) 
+		controller:DeleteOnRemove( const )
+	end
+	
+	if axis then
+		controller:SetAxis( axis )
+		controller:DeleteOnRemove( axis )
+	end
+	
+	return controller
+end
+duplicator.RegisterEntityClass("gmod_wire_motor", MakeWireMotorController, "Pos", "Ang", "MyId", "model")
+	
+function MakeWireMotor( pl, Ent1, Ent2, Bone1, Bone2, LPos1, LPos2, friction, torque, nocollide, forcelimit, MyCrtl )
+	if not constraint.CanConstrain( Ent1, Bone1 ) then return false end
+	if not constraint.CanConstrain( Ent2, Bone2 ) then return false end
+		
+	local Phys1 = Ent1:GetPhysicsObjectNum( Bone1 )
+	local Phys2 = Ent2:GetPhysicsObjectNum( Bone2 )
+	local WPos1 = Phys1:LocalToWorld( LPos1 )
+	local WPos2 = Phys2:LocalToWorld( LPos2 )
+	
+	if Phys1 == Phys2 then return false end
+
+	local const, axis = constraint.Motor( Ent1, Ent2, Bone1, Bone2, LPos1, LPos2, friction, torque, 0, nocollide, 0, pl, forcelimit )
+	
+	if not const then return nil, axis end
+		
+	local ctable = {
+		Type 		= "WireMotor",
+		pl			= pl,
+		Ent1		= Ent1,
+		Ent2		= Ent2,
+		Bone1		= Bone1,
+		Bone2		= Bone2,
+		LPos1		= LPos1,
+		LPos2		= LPos2,
+		friction	= friction,
+		torque  	= torque,
+		nocollide	= nocollide,
+		forcelimit  = forcelimit
+	}
+	const:SetTable( ctable )
+	
+	if MyCrtl then
+		local controller = WireMotorTracking[ MyCrtl ]
+		
+		const.MyCrtl = controller:EntIndex()
+		controller.MyId = controller:EntIndex()
+		
+		controller:SetConstraint( const )
+		controller:DeleteOnRemove( const )
+		if axis then
+			controller:SetAxis( axis )
+			controller:DeleteOnRemove( axis )
+		end
+		
+		Ent1:DeleteOnRemove( controller )
+		Ent2:DeleteOnRemove( controller )
+		const:DeleteOnRemove( controller )
+	end
+	
+	return const, axis
+end
+duplicator.RegisterConstraint( "WireMotor", MakeWireMotor, "pl", "Ent1", "Ent2", "Bone1", "Bone2", "LPos1", "LPos2", "friction", "torque", "nocollide", "forcelimit", "MyCrtl" )
---
+++ lua/wire/stools/motor.lua
@@ -0,0 +1,165 @@
+WireToolSetup.setCategory( "Physics" )
+WireToolSetup.open( "motor", "Motor", "gmod_wire_motor", nil, "Motors" )
+
+if CLIENT then
+    language.Add( "Tool.wire_motor.name", "Motor Tool (Wire)" )
+    language.Add( "Tool.wire_motor.desc", "Makes a controllable motor" )
+    language.Add( "Tool.wire_motor.0", "Left click on object" )
+    language.Add( "Tool.wire_motor.1", "Left click somewhere else" )
+    language.Add( "Tool.wire_motor.2", "Left click to place the controller" )
+    language.Add( "WireMotorTool_torque", "Torque:" )
+    language.Add( "WireMotorTool_friction", "Hinge Friction:" )
+	language.Add( "WireMotorTool_nocollide", "No Collide" )
+	language.Add( "WireMotorTool_forcelimit", "Force Limit:" )
+end
+WireToolSetup.BaseLang()
+WireToolSetup.SetupMax( 10 )
+
+if SERVER then
+	function TOOL:MakeEnt(ply, model, Ang, trace)
+		return MakeWireMotorController(ply, trace.HitPos, Ang, nil, model, self.constraint, self.axis)
+	end
+end
+
+function TOOL:LeftClick( trace )
+	if IsValid( trace.Entity ) and trace.Entity:IsPlayer() then return end
+
+	-- If there's no physics object then we can't constraint it!
+	if SERVER and not util.IsValidPhysicsObject( trace.Entity, trace.PhysicsBone ) then return false end
+	
+	local iNum = self:NumObjects()
+	local Phys = trace.Entity:GetPhysicsObjectNum( trace.PhysicsBone )
+	
+	-- Don't allow us to choose the world as the first object
+	if iNum == 0 and not IsValid( trace.Entity ) then return end
+	
+	-- Don't allow us to choose the same object
+	if iNum == 1 and trace.Entity == self:GetEnt(1) then return end
+	
+	self:SetObject( iNum + 1, trace.Entity, trace.HitPos, Phys, trace.PhysicsBone, trace.HitNormal )
+	
+	if iNum > 1 then
+		if CLIENT then
+			self:ClearObjects()
+			return true
+		end
+	
+		local ply = self:GetOwner()
+		local Ent1, Ent2, Ent3 = self:GetEnt(1), self:GetEnt(2), trace.Entity
+		local const, axis = self.constraint, self.axis
+		
+		if not const or not IsValid( const ) then
+			WireLib.AddNotify(self:GetOwner(), "Wire Motor Invalid!", NOTIFY_GENERIC, 7)
+			self:ClearObjects()
+			self:SetStage(0)
+			return
+		end
+		
+		local controller = self:LeftClick_Make(trace, ply)
+		self:LeftClick_PostMake(controller, ply, trace)
+		
+		if controller then
+			controller:DeleteOnRemove( const )
+			if axis then controller:DeleteOnRemove( axis ) end
+		end
+		
+		self:ClearObjects()
+		self:SetStage(0)
+	elseif iNum == 1 then
+		if CLIENT then
+			self:ClearObjects()
+			self:ReleaseGhostEntity()
+			return true
+		end
+		
+		-- Get client's CVars
+		local torque = self:GetClientNumber( "torque" )
+		local friction = self:GetClientNumber( "friction" )
+		local nocollide = self:GetClientNumber( "nocollide" )
+		local forcelimit = self:GetClientNumber( "forcelimit" )
+		
+		local Ent1,  Ent2  = self:GetEnt(1),	  self:GetEnt(2)
+		local Bone1, Bone2 = self:GetBone(1),	  self:GetBone(2)
+		local WPos1, WPos2 = self:GetPos(1),	  self:GetPos(2)
+		local LPos1, LPos2 = self:GetLocalPos(1), self:GetLocalPos(2)
+		local Norm1, Norm2 = self:GetNormal(1),	  self:GetNormal(2)
+		local Phys1, Phys2 = self:GetPhys(1), 	  self:GetPhys(2)
+		
+		-- Note: To keep stuff ragdoll friendly try to treat things as physics objects rather than entities
+		local Ang1, Ang2 = Norm1:Angle(), (Norm2 * -1):Angle()
+		local TargetAngle = Phys1:AlignAngles( Ang1, Ang2 )
+		
+		Phys1:SetAngles( TargetAngle )
+		
+		-- Move the object so that the hitpos on our object is at the second hitpos
+		local TargetPos = WPos2 + (Phys1:GetPos() - self:GetPos(1))
+
+		-- Offset slightly so it can rotate
+		TargetPos = TargetPos + (2*Norm2)
+
+		-- Set the position
+		Phys1:SetPos( TargetPos )
+
+		-- Wake up the physics object so that the entity updates
+		Phys1:Wake()
+
+		-- Set the hinge Axis perpendicular to the trace hit surface
+		LPos1 = Phys1:WorldToLocal( WPos2 + Norm2 * 64 )
+
+		local constraint, axis = MakeWireMotor( self:GetOwner(), Ent1, Ent2, Bone1, Bone2, LPos1, LPos2, friction, torque, nocollide, forcelimit )
+		self.constraint, self.axis = constraint, axis
+		
+		undo.Create("gmod_wire_motor")
+			if axis then undo.AddEntity( axis ) end
+			if constraint then undo.AddEntity( constraint ) end
+			undo.SetPlayer( self:GetOwner() )
+		undo.Finish()
+		
+		if axis then self:GetOwner():AddCleanup( "constraints", axis ) end
+		if constraint then self:GetOwner():AddCleanup( "constraints", constraint ) end
+
+		self:SetStage(2)
+		self:ReleaseGhostEntity()
+	else
+		self:StartGhostEntity( trace.Entity )
+		self:SetStage( iNum+1 )
+	end
+	
+	return true
+end
+
+function TOOL:RightClick( trace )
+	return false
+end
+
+function TOOL:Reload( trace )
+	if not IsValid( trace.Entity ) or trace.Entity:IsPlayer() then return false end
+	if CLIENT then return true end
+	
+	return constraint.RemoveConstraints( trace.Entity, "WireMotor" )
+end
+
+function TOOL:Think()
+	if self:NumObjects() == 1 then self:UpdateGhostEntity() end
+end
+
+TOOL.ClientConVar = {
+	torque = 500,
+	friction = 1,
+	nocollide = 1,
+	forcelimit = 0,
+	model = "models/jaanus/wiretool/wiretool_siren.mdl"
+}
+
+function TOOL.BuildCPanel(panel)
+	local models = {
+		["models/jaanus/wiretool/wiretool_siren.mdl"] = true,
+		["models/jaanus/wiretool/wiretool_controlchip.mdl"] = true 
+	}
+	
+	WireDermaExts.ModelSelect( panel, "wire_motor_model", models, 1 )
+	panel:NumSlider( "#WireMotorTool_torque", "wire_motor_torque", 0, 10000, 5 )
+	panel:NumSlider( "#WireMotorTool_forcelimit", "wire_motor_forcelimit", 0, 50000, 10 )
+	panel:NumSlider( "#WireMotorTool_friction", "wire_motor_friction", 0, 100, 1 )
+	panel:CheckBox( "#WireMotorTool_nocollide", "wire_motor_nocollide" )
+end
