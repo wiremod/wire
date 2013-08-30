a431f7765fec650d5b8b00f6041c490f563d9c54
Divran <arviddivran@gmail.com>
2013-08-30 16:49:41 +0200

    Made calling functions by strings more expensive
    Made calling functions by strings more expensive to match normal function calls.
    Also fixed calling functions with varargs (...)

--- lua/entities/gmod_wire_expression2/core/strfunc.lua
+++ lua/entities/gmod_wire_expression2/core/strfunc.lua
@@ -17,9 +17,9 @@ registerCallback("construct", function(self) self.strfunc_cache = {} end)
 local insert = table.insert
 local concat = table.concat
 local function findFunc( self, funcname, typeids, typeids_str )
-	local func, func_return_type
+	local func, func_return_type, vararg
 	
-	self.prf = self.prf + 1
+	self.prf = self.prf + 40
 	
 	local str = funcname .. "(" .. typeids_str .. ")"
 	for i=1,#self.strfunc_cache do
@@ -29,7 +29,7 @@ local function findFunc( self, funcname, typeids, typeids_str )
 		end
 	end
 	
-	self.prf = self.prf + 2
+	self.prf = self.prf + 40
 	
 	if #typeids > 0 then
 		if not func then
@@ -43,22 +43,24 @@ local function findFunc( self, funcname, typeids, typeids_str )
 		if not func then
 			for i=#typeids,1,-1 do
 				func, func_return_type = checkFuncName( self, funcname .. "(" .. concat(typeids,"",1,i) .. "...)" )
-				if func then break end
+				if func then vararg = true break end
 			end
 			
 			if not func then
 				func, func_return_type = checkFuncName( self, funcname .. "(...)" )
+				if func then vararg = true end
 			end
 		end
 		
 		if not func then
 			for i=#typeids,2,-1 do
 				func, func_return_type = checkFuncName( self, funcname .. "(" .. typeids[1] .. ":" ..  concat(typeids,"",2,i) .. "...)" )
-				if func then break end
+				if func then vararg = true break end
 			end
 			
 			if not func then
 				func, func_return_type = checkFuncName( self, funcname .. "(" .. typeids[1] .. ":...)" )
+				if func then vararg = true end
 			end
 		end
 	else
@@ -71,21 +73,29 @@ local function findFunc( self, funcname, typeids, typeids_str )
 		if #self.strfunc_cache == 21 then self.strfunc_cache[21] = nil end
 	end
 	
-	return func, func_return_type
+	return func, func_return_type, vararg
 end
 
-__e2setcost(6)
+__e2setcost(20)
 
 registerOperator( "sfun", "", "", function(self, args)
 	local op1, funcargs, typeids, typeids_str, returntype = args[2], args[3], args[4], args[5], args[6]
 	local funcname = op1[1](self,op1)
 
-	local func, func_return_type = findFunc( self, funcname, typeids, typeids_str )
+	local func, func_return_type, vararg = findFunc( self, funcname, typeids, typeids_str )
 	
 	if not func then error( "No function with the specified arguments exists", 0 ) end
 	
 	if returntype ~= "" and func_return_type ~= returntype then
 		error( "Mismatching return types. Got " .. nicename(wire_expression_types2[returntype][1]) .. ", expected " .. nicename(wire_expression_types2[func_return_type][1] ), 0 )
+	end
+	
+	self.prf = self.prf + 40
+	
+	if vararg then
+		funcargs[1] = function(self,arg) return arg[1] end
+		funcargs[#funcargs+1] = typeids
+		funcargs.TraceName = "FUN"
 	end
 	
 	if returntype ~= "" then
