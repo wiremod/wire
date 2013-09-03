2029463661fb390da9d1ce418796f2cf2169edaa
Nebual <ben1120@gmail.com>
2013-09-03 00:42:06 -0700

    E2: Added V:rotateAroundAxis(V axis,N degrees)

--- lua/entities/gmod_wire_expression2/core/vector.lua
+++ lua/entities/gmod_wire_expression2/core/vector.lua
@@ -281,6 +281,18 @@ e2function matrix vector:outerProduct( vector other )
 	}
 end
 
+__e2setcost(15)
+e2function vector vector:rotateAroundAxis(vector axis, degrees)
+	local ca, sa = math.cos(degrees*deg2rad), math.sin(degrees*deg2rad)
+	local x,y,z = axis[1], axis[2], axis[3]
+	local length = (x*x+y*y+z*z)^0.5
+	x,y,z = x/length, y/length, z/length
+
+	return {(ca + (x^2)*(1-ca)) * this[1] + (x*y*(1-ca) - z*sa) * this[2] + (x*z*(1-ca) + y*sa) * this[3],
+			(y*x*(1-ca) + z*sa) * this[1] + (ca + (y^2)*(1-ca)) * this[2] + (y*z*(1-ca) - x*sa) * this[3],
+			(z*x*(1-ca) - y*sa) * this[1] + (z*y*(1-ca) + x*sa) * this[2] + (ca + (z^2)*(1-ca)) * this[3]}
+end
+
 __e2setcost(5)
 
 e2function vector vector:rotate( angle ang )
