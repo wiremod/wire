62480e91842a9d35ee371157e6a17112c7569ca0
Nebual <ben1120@gmail.com>
2013-09-11 17:22:44 -0700

    E2: Added Angle + and - number, Added Angle/Angle Closes #452

--- lua/entities/gmod_wire_expression2/core/angle.lua
+++ lua/entities/gmod_wire_expression2/core/angle.lua
@@ -117,8 +117,24 @@ e2function angle operator_neg(angle rv1)
 	return { -rv1[1], -rv1[2], -rv1[3] }
 end
 
+e2function angle operator+(rv1, angle rv2)
+	return { rv1 + rv2[1], rv1 + rv2[2], rv1 + rv2[3] }
+end
+
+e2function angle operator+(angle rv1, rv2)
+	return { rv1[1] + rv2, rv1[2] + rv2, rv1[3] + rv2 }
+end
+
 e2function angle operator+(angle rv1, angle rv2)
 	return { rv1[1] + rv2[1], rv1[2] + rv2[2], rv1[3] + rv2[3] }
+end
+
+e2function angle operator-(rv1, angle rv2)
+	return { rv1 - rv2[1], rv1 - rv2[2], rv1 - rv2[3] }
+end
+
+e2function angle operator-(angle rv1, rv2)
+	return { rv1[1] - rv2, rv1[2] - rv2, rv1[3] - rv2 }
 end
 
 e2function angle operator-(angle rv1, angle rv2)
@@ -143,6 +159,10 @@ end
 
 e2function angle operator/(angle rv1, rv2)
     return { rv1[1] / rv2, rv1[2] / rv2, rv1[3] / rv2 }
+end
+
+e2function angle operator/(angle rv1, angle rv2)
+	return { rv1[1] / rv2[1], rv1[2] / rv2[2], rv1[3] / rv2[3] }
 end
 
 e2function number angle:operator[](index)
