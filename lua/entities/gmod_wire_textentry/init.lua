1aac69d66d1c4d30094d20f0cf3bf1b6733fd092
mitterdoo <connorashcroft@live.com>
2014-06-12 22:18:41 -0700

    Added Text Entry tool
    the text entry tool allows you to place keyboards players can use to
    type in input to be sent as an output for a certain amount of time.

---
+++ lua/entities/gmod_wire_textentry/init.lua
@@ -0,0 +1,131 @@
+AddCSLuaFile("shared.lua")
+AddCSLuaFile("cl_init.lua")
+include("shared.lua")
+DEFINE_BASECLASS("base_wire_entity")
+
+function ENT:SpawnFunction(ply,tr)
+	if not tr.Hit then return end
+	local pos=tr.HitPos+tr.HitNormal*16
+	local ent=ents.Create(ClassName)
+	ent:SetPos(pos)
+	ent:Spawn()
+	ent:Activate()
+	return ent
+end
+function ENT:Overlay()
+	local txt="- Text Entry -"
+	local function app(...)
+		local args={...}
+		txt=txt.."\n"..table.concat(args,"\n")
+	end
+	app("Hold Length: "..self:GetDelay())
+	if self.BlockInput then
+		app("Blocking Input")
+	elseif self.Ply then
+		app("In Use")
+	end
+	self:SetOverlayText(txt)
+end
+function ENT:Initialize()
+    self:SetModel( self.Model )
+    self:PhysicsInit(SOLID_VPHYSICS)
+    self:SetMoveType(MOVETYPE_VPHYSICS)
+    self:SetSolid(SOLID_VPHYSICS)
+    self:SetUseType(SIMPLE_USE)
+    local phys = self:GetPhysicsObject()
+    if (phys:IsValid()) then
+        phys:Wake()
+    end
+	
+	
+	self.Inputs=WireLib.CreateInputs(self,{"Block Input"})
+	self.Outputs=WireLib.CreateOutputs(self,{"In Use","Memory [STRING]","User [ENTITY]"})
+	self.BlockInput=false
+	self:Overlay()
+end
+function ENT:Output(name,value)
+	WireLib.TriggerOutput(self,name,value)
+end
+function ENT:TriggerInput(name,value)
+	if name=="Block Input" then
+		self.BlockInput = value~=0
+		self:Overlay()
+	end
+end
+util.AddNetworkString("textentry_action")
+util.AddNetworkString("textentry_show")
+net.Receive("textentry_action",function(_,ply)
+	local intruder=false
+	local self=net.ReadEntity()
+	if !IsValid(self) then intruder=true end
+	if ply!=self.Ply then intruder=true end
+	local act=net.ReadString()
+	if not intruder then
+		if act=="cancel" then
+			self.Ply=nil
+			self:Output("In Use",0)
+			self:Output("User",nil)
+			return
+		elseif act=="input" and !self.BlockInput then
+			self:Output("Memory",net.ReadString())
+			self.Ply=nil
+			self:Output("In Use",0)
+			self:Output("User",nil)
+			timer.Simple(self:GetDelay(),function()
+				if IsValid(self) then
+					self:Output("Memory","")
+				end
+			end)
+		elseif act=="input" and self.BlockInput then
+			self.Ply=nil
+			self:Output("In Use",0)
+			self:Output("User",nil)
+			ply:SendLua([[
+			notification.AddLegacy("That keyboard is not accepting input right now!",NOTIFY_ERROR,5)
+			]])
+		else
+			intruder=true
+		end
+	end
+	if intruder then
+		ply:SendLua([[notification.AddLegacy("you shouldn't be doing that. server notified.",NOTIFY_ERROR,5)]])
+		print("SOMEBODY IS SENDING NET MESSAGES MANUALLY")
+		print("> name:","",ply:Nick())
+		print("> steamid:",ply:SteamID())
+	end
+end)
+function ENT:Use(ply)
+	if self.DeltaTime then
+		if CurTime()<self.DeltaTime then
+			return
+		else
+			self.DeltaTime=CurTime()+1
+		end
+	else
+		self.DeltaTime=CurTime()+1
+	end
+	if self.BlockInput then
+		ply:SendLua([[
+		notification.AddLegacy("That keyboard is not accepting input right now!",NOTIFY_ERROR,5)
+		]])
+		return
+	end
+	assert(IsValid(ply),"ply does not exist!")
+	if IsValid(self.Ply) then
+		ply:SendLua([[
+			notification.AddLegacy("That keyboard is already in use!",NOTIFY_ERROR,5)
+		]])
+		return
+	end
+	self.Ply=ply
+	self:Output("User",ply)
+	self:Output("In Use",1)
+	net.Start("textentry_show")
+		net.WriteEntity(self)
+	net.Send(ply)
+end
+function ENT:Think()
+	if self:GetDelay()<0.1 then
+		self:SetDelay(0.1)
+	end
+end
---
+++ lua/wire/stools/textentry.lua
@@ -0,0 +1,47 @@
+WireToolSetup.setCategory("I/O")
+WireToolSetup.open("textentry","Text Entry","gmod_wire_textentry",nil,"Text Entries")
+if CLIENT then
+	local lang=function(x,y)
+		language.Add("Tool.textentry."..x,y)
+	end
+	lang("name","Wire Text Entry")
+	lang("desc","Input strings on a keyboard to be used with the wire system.")
+	lang("0","Primary: Create/Update a Text Entry keyboard.")
+	language.Add( "sboxlimit_wire_textentrys", "You've hit the Text Entries limit!" )
+	language.Add("Undone_gmod_wire_textentry","Undone Wire Text Entry")
+end
+TOOL.ClientConVar["model"] = "models/beer/wiremod/keyboard.mdl"
+TOOL.ClientConVar["delay"] = "0.1"
+if SERVER then
+	CreateConVar("sbox_maxwire_textentrys",20)
+end
+function TOOL:LeftClick(trace)
+	if CLIENT then return true end
+	if !trace.HitPos then return end
+	if IsValid(trace.Entity) then
+		local ent=trace.Entity
+		if ent:GetClass()=="gmod_wire_textentry" then
+			//update
+			local dlay=self:GetClientNumber("delay")
+			if dlay<0.1 then
+				dlay=0.1
+			end
+			ent:SetDelay(dlay)
+			return
+		end
+	end
+	local ang=trace.HitNormal:Angle()
+	ang:RotateAroundAxis(ang:Right(),-90)
+	local ent=WireLib.MakeWireEnt(self:GetOwner(),{Class="gmod_wire_textentry",Pos=trace.HitPos,Angle=ang,Model=self:GetModel()})
+	local const=WireLib.Weld(ent,trace.Entity,trace.PhysicsBone,true,false,false)
+	undo.Create("Wire Text Entry")
+		undo.AddEntity(ent)
+		undo.AddEntity(const)
+		undo.SetPlayer(self:GetOwner())
+	undo.Finish()
+end
+function TOOL.BuildCPanel(panel)
+	panel:AddControl("Header",{Description="Input strings on a keyboard to be used with the wire system."})
+	panel:NumSlider("Hold Length","textentry_delay",0.1,100,1)
+	panel:ControlHelp("Sets how long the string output is set to the inputted text in seconds.")
+end
