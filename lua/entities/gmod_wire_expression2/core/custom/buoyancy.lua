f21a3ef39c7a65e65f3ae1975b2c5f5d684ddd0a
Deyan Dobromirov <dvd_video@abv.bg>
2014-10-16 13:43:15 +0300

    Delete buoyancy.lua

--- lua/entities/gmod_wire_expression2/core/custom/buoyancy.lua
+++
@@ -1,9 +0,0 @@
-e2function void entity:setBuoyancy( number perbuoy )
-  if(IsValid(this)) then
-    local phys = this:GetPhysicsObject()
-    if(IsValid(phys)) then
-      phys:Wake()
-      phys:SetBuoyancyRatio( math.Clamp(perbuoy,0,100)/100)
-    end
-  end
-end
