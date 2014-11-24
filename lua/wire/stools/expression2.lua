e46c3850c4c6d1dcf20bd8c2ae6d2323c5f44c94
AbigailBuccaneer <abigail.buccaneer@gmail.com>
2014-11-24 19:52:30 +0000

    Fix #443. Use CanTool for protection in E2 tool.
    This means we always defer to prop protection addons for prop protection
    decisions (as we should).

--- lua/wire/stools/expression2.lua
+++ lua/wire/stools/expression2.lua
@@ -17,7 +17,6 @@ TOOL.ClientConVar = {
 	scriptmodel = "",
 	select = "",
 	autoindent = 1,
-	friendwrite = 0,
 }
 
 cleanup.Register("wire_expressions")
@@ -34,11 +33,7 @@ if SERVER then
 		local pos = trace.HitPos
 		local ang = self:GetAngle(trace)
 
-		if trace.Entity:IsValid()
-				and trace.Entity:GetClass() == "gmod_wire_expression2"
-				and (trace.Entity.player == player or trace.Entity.player:GetInfoNum("wire_expression2_friendwrite", 0) ~= 0)
-				and E2Lib.isFriend(trace.Entity.player, player)
-		then
+		if trace.Entity:IsValid() and trace.Entity:GetClass() == "gmod_wire_expression2" then
 			self:Upload(trace.Entity)
 			return true
 		end
@@ -79,11 +74,7 @@ if SERVER then
 
 		local player = self:GetOwner()
 
-		if trace.Entity:IsValid()
-				and trace.Entity:GetClass() == "gmod_wire_expression2"
-				and (trace.Entity.player == player or trace.Entity.player:GetInfoNum("wire_expression2_friendwrite", 0) ~= 0)
-				and E2Lib.isFriend(trace.Entity.player, player)
-		then
+		if trace.Entity:IsValid() and trace.Entity:GetClass() == "gmod_wire_expression2" then
 			trace.Entity:Reset()
 			return true
 		else
@@ -97,10 +88,7 @@ if SERVER then
 
 		local player = self:GetOwner()
 
-		if trace.Entity:IsValid()
-				and trace.Entity:GetClass() == "gmod_wire_expression2"
-				and E2Lib.isFriend(trace.Entity.player, player)
-		then
+		if trace.Entity:IsValid() and trace.Entity:GetClass() == "gmod_wire_expression2" then
 			self:Download(player, trace.Entity)
 			return true
 		end
@@ -154,7 +142,7 @@ if SERVER then
 			error("Invalid player entity (wtf??). This should never happen. " .. tostring(ply), 0)
 		end
 
-		if not E2Lib.isFriend(targetEnt.player, ply) then
+		if not hook.Run( "CanTool", ply, WireLib.dummytrace(targetEnt), "wire_expression") ) then return end then
 			WireLib.AddNotify(ply, "You're not allowed to download from this Expression (ent index: " .. targetEnt:EntIndex() .. ").", NOTIFY_ERROR, 7, NOTIFYSOUND_DRIP3)
 			return
 		end
@@ -267,7 +255,7 @@ if SERVER then
 			return
 		end
 
-		if toent.player != ply and (not E2Lib.isFriend(toent.player, ply) or toent.player:GetInfoNum("wire_expression2_friendwrite", 0) != 1) then
+		if not hook.Run( "CanTool", ply, WireLib.dummytrace( toent ), "wire_expression2" ) then
 			WireLib.AddNotify(ply, "You are not allowed to upload to the target Expression chip. Upload aborted.", NOTIFY_ERROR, 7, NOTIFYSOUND_DRIP3)
 			return
 		end
@@ -328,7 +316,7 @@ if SERVER then
 		if not E2 or not E2:IsValid() or E2:GetClass() ~= "gmod_wire_expression2" then return end
 		if canhas(player) then return end
 		if E2.error then return end
-		if E2.player == player or E2Lib.isFriend(E2.player, player) then
+		if hook.Run( "CanTool", player, WireLib.dummytrace( E2 ), "wire_expression2" ) then
 			E2:PCallHook("destruct")
 			E2:Error("Execution halted (Triggered by: " .. player:Nick() .. ")", "Execution halted")
 			if E2.player ~= player then
@@ -346,7 +334,7 @@ if SERVER then
 		E2 = Entity(E2)
 		if canhas(player) then return end
 		if not E2 or not E2:IsValid() or E2:GetClass() ~= "gmod_wire_expression2" then return end
-		if E2.player == player or E2Lib.isFriend(E2.player, player) then
+		if hook.Run( "CanTool", player, WireLib.dummytrace( E2 ), "wire_expression2" ) then
 			WireLib.Expression2Download(player, E2)
 			WireLib.AddNotify(player, "Downloading code...", NOTIFY_GENERIC, 5, math.random(1, 4))
 			player:PrintMessage(HUD_PRINTCONSOLE, "Downloading code...")
@@ -365,7 +353,7 @@ if SERVER then
 		E2 = Entity(E2)
 		if not E2 or not E2:IsValid() or E2:GetClass() ~= "gmod_wire_expression2" then return end
 		if canhas(player) then return end
-		if E2.player == player or (E2Lib.isFriend(E2.player, player) and E2.player:GetInfoNum("wire_expression2_friendwrite", 0) == 1) then
+		if hook.Run( "CanTool", player, WireLib.dummytrace( E2 ), "wire_expression2" ) then
 			if E2.context.data.last or E2.first then return end
 
 			E2:Reset()
