b9a8d5aea47c87ff5876f374a56efdebbb7f012f
Divran <arviddivran@gmail.com>
2014-12-05 18:46:16 +0100

    Added extinguish input to the wire igniter

--- lua/entities/gmod_wire_igniter.lua
+++ lua/entities/gmod_wire_igniter.lua
@@ -14,7 +14,7 @@ function ENT:Initialize()
 	self:PhysicsInit( SOLID_VPHYSICS )
 	self:SetMoveType( MOVETYPE_VPHYSICS )
 	self:SetSolid( SOLID_VPHYSICS )
-	self.Inputs = Wire_CreateInputs(self, { "A", "Length" })
+	self.Inputs = Wire_CreateInputs(self, { "A", "Length", "Extinguish" })
 	self.IgniteLength = 10
 	self:Setup(false, 2048)
 end
@@ -24,31 +24,45 @@ function ENT:Setup(trgply,Range)
 	if Range then self:SetBeamLength(Range) end
 end
 
+function ENT:DoTrace()
+	local vStart = self:GetPos()
+	local vForward = self:GetUp()
+
+	local trace = {}
+		trace.start = vStart
+		trace.endpos = vStart + (vForward * self:GetBeamLength())
+		trace.filter = { self }
+	local trace = util.TraceLine( trace )
+
+	local cvarTargetPlayers = GetConVarNumber('sbox_wire_igniters_allowtrgply') > 0
+
+	if not IsValid(trace.Entity) then return false end
+	if not gamemode.Call("CanProperty", self:GetPlayer(), "ignite", trace.Entity) then return false end
+	if (trace.Entity:IsPlayer() and (not self.TargetPlayers or not cvarTargetPlayers)) then return false end
+	if (trace.Entity:IsWorld()) then return false end
+	
+	return trace.Entity
+end
+
 function ENT:TriggerInput(iname, value)
 	if (iname == "A") then
 		if (value ~= 0) then
-			local vStart = self:GetPos()
-			local vForward = self:GetUp()
-
-			local trace = {}
-				trace.start = vStart
-				trace.endpos = vStart + (vForward * self:GetBeamLength())
-				trace.filter = { self }
-			local trace = util.TraceLine( trace )
-
-			local svarTargetPlayers = GetConVarNumber('sbox_wire_igniters_allowtrgply') > 0
-
-			if not IsValid(trace.Entity) then return false end
-			if not gamemode.Call("CanProperty", self:GetPlayer(), "ignite", trace.Entity) then return false end
-			if (trace.Entity:IsPlayer() && (!self.TargetPlayers || !svarTargetPlayers)) then return false end
-			if (trace.Entity:IsWorld()) then return false end
+			local target = self:DoTrace()
 			
-			trace.Entity:Extinguish()
-			trace.Entity:Ignite( self.IgniteLength, 0 )
+			if target == false then return false end
+			
+			target:Extinguish()
+			target:Ignite( self.IgniteLength, 0 )
 		end
-	else
-		if(iname == "Length") then
-			self.IgniteLength = math.min(value,GetConVarNumber("sbox_wire_igniters_maxlen"))
+	elseif (iname == "Length") then
+		self.IgniteLength = math.min(value,GetConVarNumber("sbox_wire_igniters_maxlen"))
+	elseif (iname == "Extinguish") then
+		if value ~= 0 then
+			local target = self:DoTrace()
+			
+			if target == false then return false end
+			
+			target:Extinguish()			
 		end
 	end
 end
