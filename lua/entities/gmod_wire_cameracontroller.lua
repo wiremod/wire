e613c981c12bf78e362218b8d0bf6f16800fb4e1
Divran <arviddivran@gmail.com>
2014-12-30 13:51:27 +0100

    Fixed cam controller's trace not having StartPos

--- lua/entities/gmod_wire_cameracontroller.lua
+++ lua/entities/gmod_wire_cameracontroller.lua
@@ -532,6 +532,7 @@ function ENT:UpdateOutputs()
 		end
 		
 		local trace = util.TraceLine({start=curpos,endpos=curpos+curang:Forward()*999999999,filter=self.Entities})
+		trace.StartPos = curpos
 		local hitPos = trace.HitPos or Vector(0,0,0)
 		
 		if self.OldDupe then
