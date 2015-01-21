91e0c515445246f3a595853da64780c4b2ad10b3
Divran <arviddivran@gmail.com>
2015-01-21 19:34:01 +0100

    EGP: Fixed text rotating in the wrong direction
    And added rotation support to textlayout object
    
    Fixes #792

--- lua/entities/gmod_wire_egp/lib/objects/textlayout.lua
+++ lua/entities/gmod_wire_egp/lib/objects/textlayout.lua
@@ -7,6 +7,18 @@ Obj.fontid = 1
 Obj.size = 18
 Obj.valign = 0
 Obj.halign = 0
+Obj.angle = 0
+
+if CLIENT then
+	-- Thanks to Wizard for this rotateable text code
+	cam_PushModelMatrix = cam.PushModelMatrix
+	cam_PopModelMatrix = cam.PopModelMatrix
+	mat = Matrix()
+	mat:Scale(Vector(1, 1, 1))
+	matAng = Angle(0, 0, 0)
+	matTrans = Vector(0, 0, 0)
+end
+
 Obj.Draw = function( self )
 	if (self.text and #self.text>0) then
 		surface.SetTextColor( self.r, self.g, self.b, self.a )
@@ -34,7 +46,20 @@ Obj.Draw = function( self )
 		--self.oldfontid = self.fontid
 
 		if (!self.layouter) then self.layouter = MakeTextScreenLayouter() end
-		self.layouter:DrawText(self.text, self.x, self.y, self.w, self.h, self.halign, self.valign)
+
+		if self.angle == 0 then
+			self.layouter:DrawText(self.text, self.x, self.y, self.w, self.h, self.halign, self.valign)
+		else
+			matAng.y = -self.angle
+			mat:SetAngles(matAng)
+			matTrans.x = x
+			matTrans.y = y
+			matTrans:Rotate(matAng)
+			mat:SetTranslation(Vector(self.x,self.y,0)-matTrans)
+			cam_PushModelMatrix(mat)
+				self.layouter:DrawText(self.text, 0, 0, self.w, self.h, self.halign, self.valign)
+			cam_PopModelMatrix()
+		end
 
 		--[[
 		if (!self.layouter) then
@@ -74,6 +99,7 @@ Obj.Transmit = function( self, Ent, ply )
 	net.WriteUInt(math.Clamp(self.halign,0,2), 2)
 	net.WriteInt( self.parent, 16 )
 	EGP:SendColor( self )
+	net.WriteInt((self.angle%360)*20, 16)
 end
 Obj.Receive = function( self )
 	local tbl = {}
@@ -84,8 +110,9 @@ Obj.Receive = function( self )
 	tbl.halign = net.ReadUInt(2)
 	tbl.parent = net.ReadInt(16)
 	EGP:ReceiveColor( tbl, self )
+	tbl.angle = net.ReadInt(16)/20
 	return tbl
 end
 Obj.DataStreamInfo = function( self )
-	return { x = self.x, y = self.y, w = self.w, h = self.h, valign = self.valign, halign = self.halign, size = self.size, r = self.r, g = self.g, b = self.b, a = self.a, text = self.text, fontid = self.fontid, parent = self.parent }
+	return { x = self.x, y = self.y, w = self.w, h = self.h, valign = self.valign, halign = self.halign, size = self.size, r = self.r, g = self.g, b = self.b, a = self.a, text = self.text, fontid = self.fontid, parent = self.parent, angle = self.angle }
 end
