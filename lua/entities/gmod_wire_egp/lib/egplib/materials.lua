7dbacbfe6bc031c18b03f7309eb92e913bbcb7f4
Abigail <abigail@bold.claims>
2015-02-07 12:05:46 +0000

    Add Roboto to the allowed EGP fonts. Fix #813.

--- lua/entities/gmod_wire_egp/lib/egplib/materials.lua
+++ lua/entities/gmod_wire_egp/lib/egplib/materials.lua
@@ -15,6 +15,7 @@ EGP.ValidFonts[6] = "Courier New"
 EGP.ValidFonts[7] = "Times New Roman"
 EGP.ValidFonts[8] = "ChatFont"
 EGP.ValidFonts[9] = "Marlett"
+EGP.ValidFonts[10] = "Roboto"
 if (CLIENT) then
 	local new = {}
 	for k,v in ipairs( EGP.ValidFonts ) do
