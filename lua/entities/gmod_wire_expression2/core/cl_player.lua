fc1af4ec5f6032ac9aa42287e62840391b8b3957
TomyLobo <stabelweb@gmx.de>
2015-04-09 12:32:54 +0200

    Removed client-side parts of isTyping

--- lua/entities/gmod_wire_expression2/core/cl_player.lua
+++ lua/entities/gmod_wire_expression2/core/cl_player.lua
@@ -17,7 +17,3 @@ hook.Add("OnEntityCreated", "wire_expression2_extension_player", function(ent)
 
 	SendFriendStatus()
 end)
-
--- isTyping
-hook.Add("StartChat","E2_IsTyping_Start",function() RunConsoleCommand("E2_StartChat") end)
-hook.Add("FinishChat","E2_IsTyping_Finish",function() RunConsoleCommand("E2_FinishChat") end)
