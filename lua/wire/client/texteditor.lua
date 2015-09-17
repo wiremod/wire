b9d1f5ef73695371ea668239e2a938bf91c02161
Starkkz <matiasahermosilla@gmail.com>
2015-09-17 19:43:47 -0300

    Updated texteditor.lua (Now it highlights the @rawinputs directive)

--- lua/wire/client/texteditor.lua
+++ lua/wire/client/texteditor.lua
@@ -3109,6 +3109,7 @@ do -- E2 Syntax highlighting
 		["@inputs"] = 1, -- directive yellow, types orange, rest normal
 		["@outputs"] = 1,
 		["@persist"] = 1,
+		["@rawinputs"] = 1,
 		["@trigger"] = 2, -- like 1, except that all/none are yellow
 		["@autoupdate"] = 0,
 	}
