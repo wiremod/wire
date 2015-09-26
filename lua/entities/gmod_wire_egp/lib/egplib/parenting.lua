51c664feb59d27eab8a8336e7fd366787a601a35
Rubat <robotboy655@gmail.com>
2015-09-26 18:43:22 +0300

    Added newline to ErrorNoHalt call for EGP error
    http://i.imgur.com/1Y2AWCa.png

--- lua/entities/gmod_wire_egp/lib/egplib/parenting.lua
+++ lua/entities/gmod_wire_egp/lib/egplib/parenting.lua
@@ -169,7 +169,7 @@ function EGP:GetGlobalPos( Ent, index )
 		end
 	end
 	-- Shouldn't ever get down here
-	ErrorNoHalt("[EGP] Parenting error. Tried to get position of nonexistant object (index = " .. index .. ")")
+	ErrorNoHalt("[EGP] Parenting error. Tried to get position of nonexistant object (index = " .. index .. ")\n")
 	return false, {x=0,y=0,angle=0}
 end
 
