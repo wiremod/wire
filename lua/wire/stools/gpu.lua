5a86621bc705848592387f3428ba9d9765f7cf3f
immibis <immibis@gmail.com>
2015-10-24 18:01:48 +1300

    Removed GPU and SPU from Chips/Gates category. They're still present under Visuals->Screens and Other->Sound respectively, as well as Advanced.

--- lua/wire/stools/gpu.lua
+++ lua/wire/stools/gpu.lua
@@ -1,4 +1,4 @@
-WireToolSetup.setCategory( "Chips, Gates", "Visuals/Screens", "Advanced" )
+WireToolSetup.setCategory( "Visuals/Screens", "Advanced" )
 WireToolSetup.open( "gpu", "GPU", "gmod_wire_gpu", nil, "GPUs" )
 
 if CLIENT then
