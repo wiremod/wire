b273c9e2777fa592f1a1b3bc9c28fc883bab5c24
Garrett Brown <grbrown93@sbcglobal.net>
2015-11-09 18:10:42 -0600

    Disallow applyforce damage

--- lua/entities/gmod_wire_expression2/core/custom/prop.lua
+++ lua/entities/gmod_wire_expression2/core/custom/prop.lua
@@ -210,7 +210,7 @@ end
 
 e2function void entity:propBreak()
 	if not PropCore.ValidAction(self, this, "break") then return end
-	this.IsE2Prop = 2
+	this.IsE2Prop = true
 	this:Fire("break",1,0)
 end
 
