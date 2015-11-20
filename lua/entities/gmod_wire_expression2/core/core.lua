73c1f9ef6c42ccf89f4be1e16df017cd1f89dba6
bigdogmat <bigdogmat123@gmail.com>
2015-11-20 15:02:37 -0500

    Added E2 inputClkName()

--- lua/entities/gmod_wire_expression2/core/core.lua
+++ lua/entities/gmod_wire_expression2/core/core.lua
@@ -291,6 +291,10 @@ e2function number inputClk()
 	return self.triggerinput and 1 or 0
 end
 
+e2function string inputClkName()
+	return self.triggerinput or ""
+end
+
 -- This MUST be the first destruct hook!
 registerCallback("destruct", function(self)
 	local entity = self.entity
