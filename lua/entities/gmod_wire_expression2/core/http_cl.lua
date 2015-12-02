ca2d026e689db557505cf2d276fa4bea51eef06d
Swad <SwadicalRag@users.noreply.github.com>
2015-12-02 13:53:28 +1100

    Add the main HTTP request function
    Correct the comment describing a function call
    
    Add the function to launch a clientside HTTP request
    
    Make the code lua state specific by wrapping it in if statements
    
    Define lib.netMsgID
    
    Pool the net message ID
    
    Hook onto incoming netmessages

--- lua/entities/gmod_wire_expression2/core/http_cl.lua
+++ lua/entities/gmod_wire_expression2/core/http_cl.lua
@@ -17,9 +17,39 @@
 E2Lib.clHTTP = {}
 local lib = E2Lib.clHTTP
 
+lib.netMsgID = "wire_expression2_cl_http" -- open to change
 lib.requests = {}
 
 -- string function(void)
 function lib:newUID()
 	return ("0x%08xf"):format(math.random(1,0xD4A51000))-- return 0x(hex) between 0 and 10^12
 end
+
+if SERVER then
+	util.AddNetworkString(lib.netMsgID)
+
+	-- void function(entity client, string url, function callback)
+	function lib:request(client,url,callback)
+		-- When the client sends us the data from the request, we need to know which
+		-- request it was for, so a UID is used.
+		local uid = self:newUID()
+
+		self.requests[uid] = callback
+		self:launchNewRequest(client,url,uid)
+	end
+
+	-- void function(entity client, string url, string uid)
+	-- Internal: Do not call.
+	function lib:launchNewRequest(client,url,uid)
+		net.Start(self.netMsgID)
+			net.WriteString(url)
+			net.WriteString(uid)
+		net.Send(client)
+	end
+else
+
+end
+
+net.Receive(lib.netMsgID,function(_,ply)
+	lib:handleIncomingRequest(ply)
+end)
