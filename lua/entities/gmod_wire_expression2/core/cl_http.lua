36db3092c4066df0def9a47e70838b9a1534343e
Swad <SwadicalRag@users.noreply.github.com>
2015-12-02 13:27:42 +1100

    Create cl_http.lua

---
+++ lua/entities/gmod_wire_expression2/core/cl_http.lua
@@ -0,0 +1,13 @@
+--[[
+
+	Clientside HTTP library
+	 - swadical 🍌
+
+]]
+
+--	Clientside HTTP is separate from serverside HTTP:
+--		Serverside HTTP allows greater speeds than clientside HTTP and it is
+--		up to the server owner to decide which extension should be enabled
+--		through a convar (found in core/http.lua)
+
+E2Lib.clHTTP = {}
