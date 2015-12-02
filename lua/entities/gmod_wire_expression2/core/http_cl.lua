457e68129c4b6902ea26725d0b1c64c0125fbd00
Swad <SwadicalRag@users.noreply.github.com>
2015-12-02 14:02:01 +1100

    Handle incoming requests clientside
    Add clientside request decoder
    
    Handle HTTP requests clientside
    
    lib.requests is no longer needed clientside
    
    Handle HTTP request failures too.
    
    Implement lib:returnData()
    
    Describe lib:returnData
    
    Implement lib:writeHTTPBody()
    
    Coroutine-ify the clientside senders
    
    Minor changes for coroutine compatibility
    
    Brainfart: send a UID not the netmessage name
    
    Implement lib:writeMetadata
    
    Implement lib:returnFailure
    
    Compress the request body before sending it

--- lua/entities/gmod_wire_expression2/core/http_cl.lua
+++ lua/entities/gmod_wire_expression2/core/http_cl.lua
@@ -18,23 +18,42 @@ E2Lib.clHTTP = {}
 local lib = E2Lib.clHTTP
 
 lib.netMsgID = "wire_expression2_cl_http" -- open to change
-lib.requests = {}
 
 -- string function(void)
 function lib:newUID()
 	return ("0x%08xf"):format(math.random(1,0xD4A51000))-- return 0x(hex) between 0 and 10^12
 end
 
+lib.rawRequest = http.Fetch
+lib.segmentSize = 4 * 1024-- 4 kilobytes
+lib.segmentBits = math.min(math.ceil(math.log(lib.segmentSize,2)),32)
+lib.sendInterval = 0.5-- seconds
+
+-- void function(string id, number interval, function func)
+-- Internal: Do not call.
+function lib.timerCoroutine(id,interval,func)
+	timer.Create(id,interval,0,function()
+		if func() then
+			timer.Remove(id)
+		end
+	end)
+end
+
 if SERVER then
 	util.AddNetworkString(lib.netMsgID)
 
-	-- void function(entity client, string url, function callback)
-	function lib:request(client,url,callback)
+	lib.requests = {}
+
+	-- void function(entity client, string url, function callback_success, function callback_failure)
+	function lib:request(client,url,callback_success,callback_failure)
 		-- When the client sends us the data from the request, we need to know which
 		-- request it was for, so a UID is used.
 		local uid = self:newUID()
 
-		self.requests[uid] = callback
+		self.requests[uid] = {
+			success = callback_success,
+			failure = callback_failure
+		}
 		self:launchNewRequest(client,url,uid)
 	end
 
@@ -47,7 +66,88 @@ if SERVER then
 		net.Send(client)
 	end
 else
+	-- string, string function(void)
+	-- Internal: Do not call.
+	function lib:decodeRequest()
+		return net.ReadString(),ent.ReadString()
+	end
 
+	-- void function(void)
+	-- Internal: Do not call.
+	function lib:handleIncomingRequest()
+		local url,uid = self:decodeRequest()
+
+		self:performRequest(url,uid)
+	end
+
+	-- void function(string url, string uid)
+	-- Internal: Do not call.
+	function lib:performRequest(url,uid)
+		self.rawRequest(url,function(body,length,headers,code)
+			self.timerCoroutine("wire_e2_cl_http_"..uid,self.sendInterval,function()
+				self:returnData(uid,body,length,headers,code)
+			end)
+		end,function(code)
+			self:returnFailure(uid,code)
+		end)
+	end
+
+	-- void function(string uid, string body, number length, table headers, number code)
+	-- Internal: Do not call.
+	function lib:returnData(uid,body,length,headers,code)
+		local body_compressed = util.Compress(body)
+
+		self:writeHTTPBody(uid,body_compressed,#body_compressed)
+		self:writeMetadata(uid,length,headers,code)
+
+		return true
+	end
+
+	-- void function(string uid, string body_compressed, number length)
+	-- Internal: Do not call.
+	function lib:writeHTTPBody(uid,body,length)
+		local segments = math.ceil(length/self.segmentSize)
+
+		-- reliable netmessages are always sent in order
+		for i=1,segments do
+			local segment = body:sub(self.segmentSize * (i-1),self.segmentSize * i - 1)
+			net.Start(self.netMsgID)
+				net.WriteString(uid)						-- this is who we are
+				net.WriteBool(false)						-- No; the request did not error
+				net.WriteBool(true)							-- yes we are writing a body
+				net.WriteInt(#segment,self.segmentBits + 1)	-- the body is this much long
+				net.WriteData(segment,#segment)				-- and here is the body
+			net.SendToServer()
+
+			coroutine.yield()
+		end
+	end
+
+	-- void function(string uid, number length, table headers, number code)
+	-- Internal: Do not call.
+	function lib:writeMetadata(uid,length,headers,code)
+		net.Start(self.netMsgID)
+			net.WriteString(uid)						-- this is who we are
+			net.WriteBool(false)						-- No; the request did not error
+			net.WriteBool(false)						-- we are ready to finalise the message
+			-- CAPITAL SIN: net.WriteTable is horribly
+			-- unoptimised; but there is no other
+			-- elegant solution to write in a variable
+			-- table
+			net.WriteTable(headers)						-- these are the headers
+			net.WriteInt(code,12)						-- and this is the code
+		net.SendToServer()
+	end
+
+	-- void function(string uid, number code)
+	-- Internal: Do not call.
+	function lib:returnFailure(uid,code)
+		net.Start(self.netMsgID)
+			net.WriteString(uid)	-- this is who we are
+			net.WriteBool(true)		-- unfortunately, the request caused an error
+			net.WriteInt(code,12)	-- and this is the code
+		net.SendToServer()
+	end
 end
 
 net.Receive(lib.netMsgID,function(_,ply)
