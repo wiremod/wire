ecbad252b9578ce036e612ca96ebae31c584bf32
bigdogmat <bigdogmat123@gmail.com>
2015-12-09 16:46:19 -0500

    Use unused variable

--- lua/entities/gmod_wire_expression2/core/player.lua
+++ lua/entities/gmod_wire_expression2/core/player.lua
@@ -384,7 +384,7 @@ concommand.Add("wire_expression2_friend_status", function(ply, command, args)
 	for index in args[1]:gmatch("[^,]+") do
 		local n = tonumber(index)
 		if not n then return end
-		table.insert(friends, Entity(index))
+		table.insert(friends, Entity(n))
 	end
 
 	steamfriends[ply:EntIndex()] = friends
