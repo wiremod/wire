ee669b99c5afbdbe8f532d57f58e9bae9d3513b8
Abigail <abigail@bold.claims>
2016-02-06 11:44:04 +0000

    Improve code style in E2 core/functions.lua
    Fix C-isms and change the structure of the code. Add useful readable
    comments, and remove giant header block comments.

--- lua/entities/gmod_wire_expression2/core/functions.lua
+++ lua/entities/gmod_wire_expression2/core/functions.lua
@@ -1,69 +1,57 @@
-/*==============================================================
-	E2 Function System
-		By Rusketh
-			Function Creator
-==============================================================*/
-
-local function Function(A,S,Scopes)
-
-	local Func = function(self,args)
-
-		local Variables = {}
-		for K,Data in pairs (A) do
-			local Name, Type, OP = Data[1], Data[2], args[K + 1]
-			local RV = OP[1](self, OP)
-			Variables[#Variables + 1] = {Name,RV}
-		end
-
-		local OldScopes = self:SaveScopes()
-		self:InitScope() -- Create a new Scope Enviroment
-		self:PushScope()
-
-		for I = 1, #Variables do
-			local Var = Variables[I]
-			self.Scope[Var[1]] = Var[2]
-			self.Scope["$" .. Var[1]] = Var[2]
-			self.Scope.vclk[Var[1]] = true
-		end
-
-		self.func_rv = nil
-		local ok, msg = pcall(S[1],self,S)
-
-		self:PopScope()
-		self:LoadScopes(OldScopes)
-
-		if !ok and msg:find( "C stack overflow" ) then error( "tick quota exceeded", -1 ) end -- a "C stack overflow" error will probably just confuse E2 users more than a "tick quota" error.
-
-		if !ok and msg == "return" then return self.func_rv end
-
-		if !ok then error(msg,0) end
-
-	end
-
-	return Func
-end
-
-
-
-/*==============================================================
-	E2 Function System
-		By Rusketh
-			General Operators
-==============================================================*/
+-- Implements operators for E2 user-defined functions
 
 __e2setcost(20)
-
 registerOperator("function", "", "", function(self, args)
 
-	local Stmt, args = args[2], args[3]
-	local Sig, Return, Args = args[3], args[4], args[6]
+	local statement, args = args[2], args[3]
+	local signature, returntype, parameters = args[3], args[4], args[6]
 
-	self.funcs[Sig] = Function(Args,Stmt)
+	self.funcs[signature] = function(self, arguments)
+		-- First, evaluate each argument in the current (calling) scope
+		local variables = {}
+		for i, data in ipairs(parameters) do
+			local name, expression = data[1], arguments[i + 1]
+			local value = expression[1](self, expression)
+			variables[name] = value
+		end
 
+		-- Then create a new scope for the body of the function, which consists only of the global
+		-- variables (as a function can only be written in global scope) and bind each parameter name
+		-- to the evaluated argument. (We don't have to call self:SetLocalVariableType here because the
+		-- statement was already compiled in a scope with those set.)
+		local OldScopes = self:SaveScopes()
+		self:InitScope()
+		self:PushScope()
+		for name, value in pairs(variables) do
+			self.Scope[name] = value
+			self.Scope["$" .. name] = value
+			self.Scope.vclk[name] = true
+		end
+
+		-- Then evaluate the statement to get the return value
+		self.func_rv = nil
+		local ok, message = pcall(statement[1], self, statement)
+
+		self:LoadScopes(OldScopes) -- Restore the old scope before we throw any errors
+
+		if ok then
+			return
+		end
+
+		if message == "return" then
+			-- return instructions implement early-out with error("return")
+			return self.func_rv
+		elseif message:find("C stack overflow") then
+			-- a 'C stack overflow' message typically means infinite recursion. To keep things simple for
+			-- users we just phrase this as a regular tick quota error.
+			message = "tick quota exceeded"
+		end
+
+		error(message, 0)
+	end
 end)
 
 __e2setcost(2)
-
 registerOperator("return", "", "", function(self, args)
 	if args[2] then
 		local op = args[2]
@@ -71,5 +59,5 @@ registerOperator("return", "", "", function(self, args)
 		self.func_rv = rv
 	end
 
-	error("return",0)
+	error("return", 0)
 end)
