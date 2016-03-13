c55e31d2f874563b8ccdb8dac36e5e8e68c05179
TomyLobo <tomylobo@nurfuerspam.de>
2016-03-13 21:36:57 +0100

    Rename some variables

--- lua/entities/gmod_wire_expression2/core/functions.lua
+++ lua/entities/gmod_wire_expression2/core/functions.lua
@@ -4,45 +4,45 @@
 			Function Creator
 ==============================================================*/
 
-local function Function(A, S)
+local function Function(parameters, statement)
 
 	local Func = function(self, args)
 
-		local Variables = {}
-		for K, Data in pairs(A) do
-			local Name, OP = Data[1], args[K + 1]
-			local RV = OP[1](self, OP)
-			Variables[#Variables + 1] = { Name, RV }
+		local variables = {}
+		for i, data in pairs(parameters) do
+			local name, expression = data[1], args[i + 1]
+			local value = expression[1](self, expression)
+			variables[#variables + 1] = { name, value }
 		end
 
 		local OldScopes = self:SaveScopes()
 		self:InitScope() -- Create a new Scope Enviroment
 		self:PushScope()
 
-		for I = 1, #Variables do
-			local Var = Variables[I]
+		for I = 1, #variables do
+			local Var = variables[I]
 			self.Scope[Var[1]] = Var[2]
 			self.Scope["$" .. Var[1]] = Var[2]
 			self.Scope.vclk[Var[1]] = true
 		end
 
 		self.func_rv = nil
-		local ok, msg = pcall(S[1], self, S)
+		local ok, message = pcall(statement[1], self, statement)
 
 		self:PopScope()
 		self:LoadScopes(OldScopes)
 
-		if !ok and msg:find("C stack overflow") then
+		if !ok and message:find("C stack overflow") then
 			-- a "C stack overflow" error will probably just confuse E2 users more than a "tick quota" error.
 			error("tick quota exceeded", -1)
 		end
 
-		if !ok and msg == "return" then
+		if !ok and message == "return" then
 			return self.func_rv
 		end
 
 		if !ok then
-			error(msg, 0)
+			error(message, 0)
 		end
 
 	end
@@ -61,10 +61,10 @@ end
 __e2setcost(20)
 registerOperator("function", "", "", function(self, args)
 
-	local Stmt, args = args[2], args[3]
-	local Sig, Return, Args = args[3], args[4], args[6]
+	local statement, args = args[2], args[3]
+	local signature, returntype, parameters = args[3], args[4], args[6]
 
-	self.funcs[Sig] = Function(Args, Stmt)
+	self.funcs[signature] = Function(parameters, statement)
 
 end)
 
