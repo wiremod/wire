7fca9ccce8b0947282c2a663b4680f77b16ee5c9
Axel Michaud <axelbmichaud@gmail.com>
2016-03-16 13:11:18 -0400

    Add quaternion

--- lua/entities/gmod_wire_expression2/core/quaternion.lua
+++ lua/entities/gmod_wire_expression2/core/quaternion.lua
@@ -110,7 +110,7 @@ end
 __e2setcost(1)
 
 --- Creates a zero quaternion
-e2function quaternion quat()
+e2function  quaternionquat()
 	return { 0, 0, 0, 0 }
 end
 
@@ -232,7 +232,7 @@ __e2setcost(2)
 
 registerOperator("ass", "q", "q", function(self, args)
 	local lhs, op2, scope = args[2], args[3], args[4]
-	local      rhs = op2[1](self, op2)
+	local      rhs = op2[1](self, op2) or { 0, 0, 0, 0 }
 
 	self.Scopes[scope][lhs] = rhs
 	self.Scopes[scope].vclk[lhs] = true
