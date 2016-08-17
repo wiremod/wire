1ae32494df47d59cec070a74040459a494a67f73
Scott Rogers <Scottyprogrammer@aol.com>
2016-08-17 16:14:14 -0700

    Added a new rope function with rigid inclued

--- lua/entities/gmod_wire_expression2/core/custom/constraintcore.lua
+++ lua/entities/gmod_wire_expression2/core/custom/constraintcore.lua
@@ -151,6 +151,22 @@ e2function void rope(index, entity ent1, vector v1, entity ent2, vector v2)
 end
 
 --- Creates a rope between <ent1> and <ent2> at vector positions local to each ent, with <addlength> additional length, <width> width, and <mat> material.
+e2function void rope(index, entity ent1, vector v1, entity ent2, vector v2, addlength, width, string mat )
+	if !checkEnts(self, ent1, ent2) then return end
+	if !ent1.data then ent1.data = {} end
+	if !ent1.data.Ropes then ent1.data.Ropes = {} end
+	local vec1, vec2 = Vector(v1[1], v1[2], v1[3]), Vector(v2[1], v2[2], v2[3])
+	local length = (ent1:LocalToWorld(vec1) - ent2:LocalToWorld(vec2)):Length()
+	
+	if IsValid(ent1.data.Ropes[index]) then 
+		ent1.data.Ropes[index]:Remove() 
+	end
+	
+	ent1.data.Ropes[index] = constraint.Rope( ent1, ent2, 0, 0, vec1, vec2, length, addlength, 0, width, mat, false )
+	addundo(self, ent1.data.Ropes[index], "rope")
+end
+
+--- Creates a rope between <ent1> and <ent2> at vector positions local to each ent, with <addlength> additional length, <width> width, and <mat> material.
 e2function void rope(index, entity ent1, vector v1, entity ent2, vector v2, addlength, width, string mat, rigid )
 	if !checkEnts(self, ent1, ent2) then return end
 	if !ent1.data then ent1.data = {} end
