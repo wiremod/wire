b1f1e4ab9e518e7393772085aeafe09af7b4c7ff
Divran <arviddivran@gmail.com>
2016-10-02 00:42:26 +0200

    Removed inf constant

--- lua/entities/gmod_wire_expression2/core/number.lua
+++ lua/entities/gmod_wire_expression2/core/number.lua
@@ -47,7 +47,6 @@ registerType("normal", "n", 0,
 E2Lib.registerConstant("PI", pi)
 E2Lib.registerConstant("E", exp(1))
 E2Lib.registerConstant("PHI", (1+sqrt(5))/2)
-E2Lib.registerConstant("INF", inf)
 
 --[[************************************************************************]]--
 
