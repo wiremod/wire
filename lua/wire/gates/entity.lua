add2f8b6ed2e14bc6e1a0d0efd84a9e76ec79002
TheMerkyShadow <Williamh1103@aol.com>
2017-05-01 16:44:11 -0400

    Update entity.lua

--- lua/wire/gates/entity.lua
+++ lua/wire/gates/entity.lua
@@ -4,6 +4,21 @@
 
 GateActions("Entity")
 
+local function ApplyForceValidAction(self)
+	local convat = GetConvar("sbox_E2_ApplyForce"):GetInt()
+        local ply = self:GetOwner() or nil
+	if !IsValid(ply) then return false end
+        if convar == 0 then
+	     return true
+        elseif convar == 1 and ply:IsAdmin() then 
+	     return true
+        elseif convar == 2 and ply:IsSuperAdmin() then 
+	     return true
+	else
+             return false
+	end
+end
+
 local check = WireLib.checkForce
 
 local function isAllowed( gate, ent )
@@ -17,11 +32,11 @@ GateActions["entity_applyf"] = {
 	inputtypes = { "ENTITY" , "VECTOR" },
 	timed = true,
 	output = function(gate, ent, vec )
-		if not ApplyForceValidAction() then return end
 		if not IsValid( ent ) then return end
 		local phys = ent:GetPhysicsObject()
 		if not IsValid( phys ) then return end
-		if not isAllowed( gate, ent ) then return end
+		if not isAllowed( gate, ent ) then return end	
+		if not ApplyForceValidAction(gate) then return end
 		if !isvector(vec) then vec = Vector (0, 0, 0) end
 		if not check(vec) then return end
 		if vec.x == 0 and vec.y == 0 and vec.z == 0 then return end
@@ -39,11 +54,11 @@ GateActions["entity_applyof"] = {
 	inputtypes = { "ENTITY" , "VECTOR" , "VECTOR" },
 	timed = true,
 	output = function(gate, ent, vec, offset )
-		if not ApplyForceValidAction() then return end
 		if not IsValid( ent ) then return end
 		local phys = ent:GetPhysicsObject()
 		if not IsValid( phys ) then return end
 		if not isAllowed( gate, ent ) then return end
+		if not ApplyForceValidAction(gate) then return end	
 		if !isvector(vec) then vec = Vector (0, 0, 0) end
 		if !isvector(offset) then offset = Vector (0, 0, 0) end
 		if not check(vec) or not check( offset ) then return end
@@ -64,11 +79,11 @@ GateActions["entity_applyaf"] = {
 	inputtypes = { "ENTITY" , "ANGLE" },
 	timed = true,
 	output = function(gate, ent, angForce )
-		if not ApplyForceValidAction() then return end
 		if not IsValid( ent ) then return end
 		local phys = ent:GetPhysicsObject()
 		if not IsValid( phys ) then return end
 		if not isAllowed( gate, ent ) then return end
+		if not ApplyForceValidAction(gate) then return end	
 		if not check( angForce ) then return end
 		if angForce.p == 0 and angForce.y == 0 and angForce.r == 0 then return end
 
@@ -112,11 +127,11 @@ GateActions["entity_applytorq"] = {
 	inputtypes = { "ENTITY" , "VECTOR" },
 	timed = true,
 	output = function(gate, ent, vec )
-		if not ApplyForceValidAction() then return end
 		if not IsValid( ent ) then return end
 		local phys = ent:GetPhysicsObject()
 		if not IsValid( phys ) then return end
 		if not isAllowed( gate, ent ) then return end
+		if not ApplyForceValidAction(gate) then return end	
 		if !isvector(vec) then vec = Vector (0, 0, 0) end
 		if !isvector(offset) then offset = Vector (0, 0, 0) end
 		if not check(vec) or not check( offset ) then return end
