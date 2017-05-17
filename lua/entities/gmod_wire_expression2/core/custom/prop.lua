01abedcc6e7aac95db57703388af86f1a73b4b53
CaptainPRICE <captainprice@programmer.net>
2017-05-17 03:11:58 +0200

    Fixed "entity=propSpawn(string, vector, angle, number)" E2 function to return non-nil value.

--- lua/entities/gmod_wire_expression2/core/custom/prop.lua
+++ lua/entities/gmod_wire_expression2/core/custom/prop.lua
@@ -178,8 +178,7 @@ e2function entity propSpawn(entity template, angle rot, number frozen)
 end
 
 e2function entity propSpawn(string model, vector pos, angle rot, number frozen)
-	if not PropCore.ValidAction(self, nil, "spawn") then return nil end
-	return PropCore.CreateProp(self,model,Vector(pos[1],pos[2],pos[3]),Angle(rot[1],rot[2],rot[3]),frozen)
+	return PropCore.ValidAction(self, nil, "spawn") and PropCore.CreateProp(self,model,Vector(pos[1],pos[2],pos[3]),Angle(rot[1],rot[2],rot[3]),frozen) or NULL
 end
 
 e2function entity propSpawn(entity template, vector pos, angle rot, number frozen)
