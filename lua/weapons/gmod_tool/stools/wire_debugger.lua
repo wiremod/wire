93d1b7a25177179a5599c5fdc3020a51a7fbd567
Divran <arviddivran@gmail.com>
2017-07-08 17:32:46 +0200

    Moved debugger window down
    Moved the debugger window down when you are holding a tool, to compensate for the new way that tool keybinds are displayed. We can't know exactly how tall every tool is anymore, so the 250 value is just arbitrarily pulled outta nowhere

--- lua/weapons/gmod_tool/stools/wire_debugger.lua
+++ lua/weapons/gmod_tool/stools/wire_debugger.lua
@@ -363,7 +363,7 @@ if (CLIENT) then
 
 		-- TODO: account for larger usage info boxes.
 		--draw the box
-		draw.RoundedBox(8, 2, 2+178*MoveBox, BoxWidth, Line_Count*14+16, Color(50, 50, 50, 128))
+		draw.RoundedBox(8, 2, 2+250*MoveBox, BoxWidth, Line_Count*14+16, Color(50, 50, 50, 128))
 
 		--step through all of the entries and their text to print them
 		for EntryIndex, Entry in ipairs(Entries) do
@@ -371,7 +371,7 @@ if (CLIENT) then
 				draw.Text({
 					text = string.Trim(Line.LineText) or "",
 					font = "Default",
-					pos = { Line.OffsetPos[1]+10, 178*MoveBox+10+Line.OffsetPos[2] },
+					pos = { Line.OffsetPos[1]+10, 250*MoveBox+10+Line.OffsetPos[2] },
 					color = Entry.TextColor
 					})
 
