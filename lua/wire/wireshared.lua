15ba7ee35bda52536236b94ecd9fdd82ebfed382
Just-Boog <Just-Boog@users.noreply.github.com>
2017-07-18 01:13:25 -0400

    Fixed my nan fix
    Don't ask

--- lua/wire/wireshared.lua
+++ lua/wire/wireshared.lua
@@ -1014,18 +1014,27 @@ end)
 
 -- Instead of checking if values are inrange and breaking code, I've clamped them.
 -- Massive numbers applied through E2, which are allowed by the code right above, may change the direction of the applyForce
-function WireLib.clampForce( args )
+function WireLib.clampForce( v )
 	-- args whether it be a vector or angle can be indexed like a table, so we just check each value, modify it, then return args
-	-- Check if there's more than 3 values, if there is, something is wrong
-	if #args ~= 3 then return { 0,0,0 } end
-
-	for k,v in pairs( args ) do
-		-- check if the value is nan, nan never equals itself
-		if v ~= v then
-			args[k] = 0
-		else
-			args[k] = math.Clamp( v, min_force, max_force )
-		end
+	-- check if each value is nan then make it 0 if it is, else clamp it
+	if v[1] ~= v[1] then
+		v[1] = 0
+	else
+		v[1] = math.Clamp( v[1], min_force, max_force )
 	end
-	return args
+
+	if v[2] ~= v[2] then
+		v[2] = 0
+	else
+		v[2] = math.Clamp( v[2], min_force, max_force )
+	end
+
+	if v[3] ~= v[3] then
+		v[3] = 0
+	else
+		v[3] = math.Clamp( v[3], min_force, max_force )
+	end
+
+	return v
+
 end
