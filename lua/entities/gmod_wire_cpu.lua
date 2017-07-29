4a0f398e8679f3c2c5f8ddf31182da7b5a694ec9
mms92 <martin-manche@hotmail.fr>
2017-07-29 21:23:47 +0200

    var initialisation, anonymous function and other
    initializing max-frequency variable at 1,400,000 instead of nil
    function "update from convar" created anonymously
    removed useless table creation
    removed useless do-end

--- lua/entities/gmod_wire_cpu.lua
+++ lua/entities/gmod_wire_cpu.lua
@@ -4,18 +4,16 @@ ENT.PrintName       = "Wire ZCPU"
 ENT.Author          = "Black Phoenix"
 ENT.WireDebugName	= "ZCPU"
 
-CreateConVar("wire_cpu_max_frequency", "1400000", {FCVAR_REPLICATED})
+CreateConVar("wire_cpu_max_frequency", "1400000", FCVAR_REPLICATED)
 
 if CLIENT then return end -- No more client
 
-local cpu_max_frequency = nil
+local cpu_max_frequency = 1400000
 
-do
-	local function updateCPUMaxFrequency()
-		cpu_max_frequency = GetConVar("wire_cpu_max_frequency"):GetInt()
-	end
-	cvars.AddChangeCallback("wire_cpu_max_frequency",updateCPUMaxFrequency)
-	updateCPUMaxFrequency()
+cvars.AddChangeCallback("wire_cpu_max_frequency",function()
+	cpu_max_frequency = GetConVar("wire_cpu_max_frequency"):GetInt()
+end)
+
 end
 
 function ENT:Initialize()
