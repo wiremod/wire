c368a7812f33c7f3d95192992a05be5745a19c31
AlexALX <alexalx@bigmir.net>
2018-02-24 20:52:03 +0200

    Add "DiskEntity" to CD Holder for easy identify attached disk.

--- lua/entities/gmod_wire_cd_lock.lua
+++ lua/entities/gmod_wire_cd_lock.lua
@@ -20,7 +20,7 @@ function ENT:Initialize()
 	self.DisableLinking = 0
 
 	self.Inputs = Wire_CreateInputs(self, { "Disable" })
-	self.Outputs = Wire_CreateOutputs(self, { "Locked" })
+	self.Outputs = Wire_CreateOutputs(self, { "Locked", "Ent [ENTITY]" })
 
 	self:NextThink(CurTime() + 0.25)
 end
@@ -38,6 +38,7 @@ function ENT:TriggerInput(iname, value)
 			//self.NoCollideConst = nil
 
 			Wire_TriggerOutput(self, "Locked", 0)
+			Wire_TriggerOutput(self, "Ent", nil)
 			self:NextThink(CurTime() + NEW_DISK_WAIT_TIME)
 		end
 	end
@@ -54,6 +55,7 @@ function ENT:Think()
 		self.NoCollideConst = nil
 
 		Wire_TriggerOutput(self, "Locked", 0)
+		Wire_TriggerOutput(self, "Ent", nil)
 
 		self:NextThink(CurTime() + NEW_DISK_WAIT_TIME) //Give time before next grabbing a disk.
 		return true
@@ -86,6 +88,7 @@ function ENT:AttachDisk(disk)
 	self.NoCollideConst = constraint.NoCollide(self, disk, 0, 0)
 	if (not self.NoCollideConst) then
 		Wire_TriggerOutput(self, "Locked", 0)
+		Wire_TriggerOutput(self, "Ent", nil)
 		return
 	end
 
@@ -95,6 +98,7 @@ function ENT:AttachDisk(disk)
 	    self.NoCollideConst:Remove()
 	    self.NoCollideConst = nil
 	    Wire_TriggerOutput(self, "Locked", 0)
+		Wire_TriggerOutput(self, "Ent", nil)
 	    return
 	end
 
@@ -106,6 +110,7 @@ function ENT:AttachDisk(disk)
 	disk.Lock = self
 	self.Disk = disk
 	Wire_TriggerOutput(self, "Locked", 1)
+	Wire_TriggerOutput(self, "Ent", disk)
 end
 
 duplicator.RegisterEntityClass("gmod_wire_cd_lock", WireLib.MakeWireEnt, "Data")
