458bcae9e566e9068c277ec559179693997fa69b
Garrett Brown <grbrown93@sbcglobal.net>
2019-11-09 04:51:27 -0500

    Fix missing params

--- lua/wire/stools/gpulib_switcher.lua
+++ lua/wire/stools/gpulib_switcher.lua
@@ -1,5 +1,5 @@
 WireToolSetup.setCategory( "Visuals" )
-WireToolSetup.open( "gpulib_switcher", "GPULib Switcher", "gmod_wire_gpulib_controller" )
+WireToolSetup.open( "gpulib_switcher", "GPULib Switcher", "gmod_wire_gpulib_controller", nil, "GPULib Switchers" )
 
 TOOL.ClientConVar[ "model" ] = "models/jaanus/wiretool/wiretool_siren.mdl"
 
