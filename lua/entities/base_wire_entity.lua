92dc0c5ed93110841b04b83a80a3a13f5544164f
storm37000 <6331913+storm37000@users.noreply.github.com>
2020-12-07 18:17:17 -0500

    better removal of invalid players

--- lua/entities/base_wire_entity.lua
+++ lua/entities/base_wire_entity.lua
@@ -195,7 +195,8 @@ if CLIENT then
 
 	-- Custom better version of this base_gmodentity function
 	function ENT:BeingLookedAtByLocalPlayer()
-		local trbool = BaseClass.BeingLookedAtByLocalPlayer(self)
+		local trace = LocalPlayer():GetEyeTrace()
+		local trbool = (trace.Entity == self and trace.HitPos:DistToSqr(LocalPlayer():GetShootPos()) > 40000) --this is 200^2
 
 		if self.playerWasLookingAtMe ~= trbool then
 			net.Start( "wire_overlay_request" )
@@ -304,23 +305,41 @@ end
 -- It allows us to optionally send values rather than entire strings, which saves networking
 -- It also allows us to only update overlays when someone is looking at the entity.
 
+local function cleanInvalidPlayers()
+	for k, v in ipairs( self.playersRequestingOverlayNumeric ) do
+		if !IsValid(v) then table.remove( self.playersRequestingOverlayNumeric, k ) end
+	end
+end
+
 function ENT:SetOverlayText( txt )
 	if not self.OverlayData then
 		self.OverlayData = {}
 	end
-	if txt and #txt > 1024 then
-		txt = string.sub(txt,1,1024)
+	if txt and #txt > 5000 then
+		txt = string.sub(txt,1,5000)
 	end
-	self.OverlayData.__dirty = self.OverlayData.txt ~= txt
 	self.OverlayData.txt = txt
+	if CLIENT then return end
+	if #self.playersRequestingOverlayNumeric == 0 then return end
+	cleanInvalidPlayers()
+	net.Start( "wire_overlay_txt", true )
+		net.WriteEntity( self )
+		net.WriteString( self.OverlayData.txt )
+	net.Send(self.playersRequestingOverlayNumeric)
 end
 
 function ENT:SetOverlayData( data )
-	if data.txt and #data.txt > 1024 then
-		data.txt = string.sub(data.txt,1,1024)
+	if data.txt and #data.txt > 5000 then
+		data.txt = string.sub(data.txt,1,5000)
 	end
 	self.OverlayData = data
-	self.OverlayData.__dirty = true
+	if CLIENT then return end
+	if #self.playersRequestingOverlayNumeric == 0 then return end
+	cleanInvalidPlayers()
+	net.Start( "wire_overlay_data", true )
+		net.WriteEntity( self )
+		net.WriteTable( self.OverlayData )
+	net.Send(self.playersRequestingOverlayNumeric)
 end
 
 function ENT:GetOverlayData()
@@ -341,40 +360,23 @@ util.AddNetworkString( "wire_overlay_request" )
 -- Other functions
 --------------------------------------------------------------------------------
 
-local function sendWireOverlays()
-	local found = {}
-	for _, ply in ipairs(player.GetAll()) do
-		local sendEnt = ply.wireSendOverlay
-		if sendEnt and sendEnt:IsValid() then
-			found[sendEnt.OverlayData] = true
-			if sendEnt.OverlayData.__dirty then
-				net.Start( "wire_overlay_data", true )
-					net.WriteEntity( sendEnt )
-					net.WriteTable( sendEnt.OverlayData )
-				net.Send(ply)
-			end
-		else
-			ply.wireSendOverlay = nil
-		end
-	end
-	if next(found) then
-		for OverlayData in pairs(found) do
-			OverlayData.__dirty = false
-		end
-	else
-		hook.Remove("Think","Wire_SendOverlayData")
-	end
-end
+ENT.playersRequestingOverlayNumeric = {}
 
 net.Receive( "wire_overlay_request", function( len, ply )
 	local ent = net.ReadEntity()
 	if not IsValid(ent) then return end
 	if net.ReadBool() then
-		ply.wireSendOverlay = ent
-		ent.OverlayData.__dirty = true
-		hook.Add("Think","Wire_SendOverlayData",sendWireOverlays)
+		if not table.HasValue(ent.playersRequestingOverlayNumeric,ply) then
+			table.insert(ent.playersRequestingOverlayNumeric,ply)
+		end
+		if ent.OverlayData then
+			net.Start( "wire_overlay_data" )
+				net.WriteEntity( ent )
+				net.WriteTable( ent.OverlayData )
+			net.Send(ply)
+		end
 	else
-		ply.wireSendOverlay = nil
+		table.RemoveByValue(ent.playersRequestingOverlayNumeric,ply)
 	end
 end )
 
