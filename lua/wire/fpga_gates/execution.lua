ea201544455a3620524f6c5e38e6194771b53799
Lasse Lysdal <lysdal1234@gmail.com>
2021-01-31 17:51:35 +0100

    Added in missing execution last gates

--- lua/wire/fpga_gates/execution.lua
+++ lua/wire/fpga_gates/execution.lua
@@ -16,21 +16,82 @@ FPGAGateActions("Execution")
 -- }
 
 FPGAGateActions["execution-last-normal"] = {
-  	name = "Last Normal",
-    inputs = {"A"},
-    inputtypes = {"NORMAL"},
-    outputs = {"Out"},
-    outputtypes = {"NORMAL"},
-    neverActive = true,
-    output = function(gate, value)
-      gate.memory = value
-      return gate.value
-    end,
-    reset = function(gate)
-      gate.value = 0
-      gate.memory = 0
-    end,
-    postCycle = function(gate)
-      gate.value = gate.memory
-  	end,
-  }
+  name = "Last Normal",
+  inputs = {"A"},
+  inputtypes = {"NORMAL"},
+  outputs = {"Out"},
+  outputtypes = {"NORMAL"},
+  neverActive = true,
+  output = function(gate, value)
+    gate.memory = value
+    return gate.value
+  end,
+  reset = function(gate)
+    gate.value = 0
+    gate.memory = 0
+  end,
+  postCycle = function(gate)
+    gate.value = gate.memory
+  end,
+}
+
+FPGAGateActions["execution-last-vector"] = {
+  name = "Last Vector",
+  inputs = {"A"},
+  inputtypes = {"VECTOR"},
+  outputs = {"Out"},
+  outputtypes = {"VECTOR"},
+  neverActive = true,
+  output = function(gate, value)
+    gate.memory = value
+    return gate.value
+  end,
+  reset = function(gate)
+    gate.value = Vector(0, 0, 0)
+    gate.memory = Vector(0, 0, 0)
+  end,
+  postCycle = function(gate)
+    gate.value = gate.memory
+  end,
+}
+
+FPGAGateActions["execution-last-angle"] = {
+  name = "Last Angle",
+  inputs = {"A"},
+  inputtypes = {"ANGLE"},
+  outputs = {"Out"},
+  outputtypes = {"ANGLE"},
+  neverActive = true,
+  output = function(gate, value)
+    gate.memory = value
+    return gate.value
+  end,
+  reset = function(gate)
+    gate.value = Angle(0, 0, 0)
+    gate.memory = Angle(0, 0, 0)
+  end,
+  postCycle = function(gate)
+    gate.value = gate.memory
+  end,
+}
+
+FPGAGateActions["execution-last-string"] = {
+  name = "Last String",
+  inputs = {"A"},
+  inputtypes = {"STRING"},
+  outputs = {"Out"},
+  outputtypes = {"STRING"},
+  neverActive = true,
+  output = function(gate, value)
+    gate.memory = value
+    return gate.value
+  end,
+  reset = function(gate)
+    gate.value = ""
+    gate.memory = ""
+  end,
+  postCycle = function(gate)
+    gate.value = gate.memory
+  end,
+}
+
