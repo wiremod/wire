162fb14d197c38d218822c4af80244618b1513b8
Lasse Lysdal <lysdal1234@gmail.com>
2021-02-05 14:53:29 +0100

    Removed unnecessary tool_loader

--- lua/autorun/wire_fpga_load.lua
+++ lua/autorun/wire_fpga_load.lua
@@ -1,9 +1,8 @@
-if SERVER then MsgC(Color(0, 150, 255), "Loading Wire FPGA...\n") end
+if SERVER then MsgC(Color(50, 180, 50), "Loading Wire FPGA...\n") end
 
 if SERVER then
   AddCSLuaFile("wire/client/node_editor/nodeeditor.lua")
   AddCSLuaFile("wire/client/node_editor/wire_fpga_editor.lua")
-  AddCSLuaFile("wire/tool_loader.lua")
   AddCSLuaFile("wire/fpgagates.lua")
 end
 
@@ -12,10 +11,9 @@ include("wire/fpgagates.lua")
 if CLIENT then
   AddCSLuaFile("wire/client/node_editor/nodeeditor.lua")
   AddCSLuaFile("wire/client/node_editor/wire_fpga_editor.lua")
-  AddCSLuaFile("wire/tool_loader.lua")
 
   include("wire/client/node_editor/nodeeditor.lua")
   include("wire/client/node_editor/wire_fpga_editor.lua")
 end
 
-if SERVER then MsgC(Color(0, 150, 255), "Wire FPGA loaded!\n") end
+if SERVER then MsgC(Color(50, 180, 50), "Wire FPGA loaded!\n") end
