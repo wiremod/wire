255a0b4ca829772275560c194c144c8f12271e6c
Lasse Lysdal <lysdal1234@gmail.com>
2021-02-11 19:29:34 +0100

    Fixed lua syntax

--- lua/wire/cpu_gates/logic.lua
+++ lua/wire/cpu_gates/logic.lua
@@ -28,7 +28,7 @@ CPUGateActions["logic-and"] = {
   name = "AND",
   inputs = {"A", "B"},
   output = function(gate, A, B)
-    if A != 0 and B != 0 then 
+    if A ~= 0 and B ~= 0 then 
       return 1
     else
       return 0
@@ -40,7 +40,7 @@ CPUGateActions["logic-or"] = {
   name = "OR",
   inputs = {"A", "B"},
 	output = function(gate, A, B)
-		if A != 0 or B != 0 then 
+		if A ~= 0 or B ~= 0 then 
       return 1
     else
       return 0
@@ -52,7 +52,7 @@ CPUGateActions["logic-nand"] = {
   name = "NAND",
   inputs = {"A", "B"},
 	output = function(gate, A, B)
-		if A != 0 and B != 0 then 
+		if A ~= 0 and B ~= 0 then 
       return 0
     else
       return 1
@@ -64,7 +64,7 @@ CPUGateActions["logic-nor"] = {
   name = "NOR",
   inputs = {"A", "B"},
 	output = function(gate, A, B)
-		if A != 0 or B != 0 then 
+		if A ~= 0 or B ~= 0 then 
       return 0
     else
       return 1
@@ -76,7 +76,7 @@ CPUGateActions["logic-xor"] = {
   name = "XOR",
   inputs = {"A", "B"},
 	output = function(gate, A, B)
-		if (A != 0 and B == 0) or (A == 0 and B != 0) then 
+		if (A ~= 0 and B == 0) or (A == 0 and B ~= 0) then 
       return 1
     else
       return 0
@@ -88,7 +88,7 @@ CPUGateActions["logic-xnor"] = {
   name = "XNOR",
   inputs = {"A", "B"},
 	output = function(gate, A, B)
-		if (A != 0 and B == 0) or (A == 0 and B != 0) then 
+		if (A ~= 0 and B == 0) or (A == 0 and B ~= 0) then 
       return 0
     else
       return 1
