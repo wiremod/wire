ffda2bd2587543f7083740713a3083b9922dd991
thegrb93 <grbrown93@sbcglobal.net>
2021-05-14 21:36:35 -0400

    Fix turret shoot positions (#2172)

--- lua/entities/gmod_wire_turret.lua
+++ lua/entities/gmod_wire_turret.lua
@@ -23,6 +23,7 @@ function ENT:Initialize()
 	self.Firing       = false
 	self.spreadvector = Vector()
 	self.effectdata   = EffectData()
+	self.attachmentPos = phys:WorldToLocal(self:GetAttachment(1).Pos)
 
 	self.Inputs = WireLib.CreateSpecialInputs(self,
 		{ "Fire", "Force", "Damage", "NumBullets", "Spread", "Delay", "Sound", "Tracer" },
@@ -42,12 +43,9 @@ function ENT:FireShot()
 		self:EmitSound( self.sound )
 	end
 
-	-- Get the muzzle attachment (this is pretty much always 1)
-	local Attachment = self:GetAttachment( 1 )
-
-	-- Get the shot angles and stuff.
-	local shootOrigin = Attachment.Pos + self:GetVelocity() * engine.TickInterval()
-	local shootAngles = self:GetAngles()
+	local phys = self:GetPhysicsObject()
+	local shootOrigin = phys:LocalToWorld(self.attachmentPos)
+	local shootAngles = phys:GetAngles()
 
 	-- Shoot a bullet
 	local bullet      = {}
