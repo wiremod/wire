55e7ccfc167aef95898be5cc70ba7fd7492811ac
Garrett Brown <grbrown93@sbcglobal.net>
2021-06-17 04:11:05 -0400

    Limit regex cpu consumption. Fixes: #2063 Related (https://github.com/Facepunch/garrysmod-requests/issues/1878)

--- lua/entities/gmod_wire_expression2/core/string.lua
+++ lua/entities/gmod_wire_expression2/core/string.lua
@@ -337,6 +337,10 @@ end)
 
 /******************************************************************************/
 
+local function checkregex(data, pattern)
+	local n = 0 for i in string.gmatch(string.gsub(pattern, "%%.", ""), "[%+%-%*]") do n = n + 1 end
+	return n==0 or (n==1 and #data<15000) or (n==2 and #data<900)
+end
 
 local sub = string.sub
 local gsub = string.gsub
@@ -344,7 +348,12 @@ local find = string.find
 
 --- Returns the 1st occurrence of the string <pattern>, returns 0 if not found. Prints malformed string errors to the chat area.
 e2function number string:findRE(string pattern)
-	local OK, Ret = pcall(string.find, this, pattern)
+	local OK, Ret
+	if checkregex(this, pattern) then
+		OK, Ret = pcall(string.find, this, pattern)
+	else
+		OK, Ret = false, "regex is too complex!"
+	end
 	if not OK then
 		self.player:ChatPrint(Ret)
 		return 0
@@ -355,7 +364,12 @@ end
 
 ---  Returns the 1st occurrence of the string <pattern> starting at <start> and going to the end of the string, returns 0 if not found. Prints malformed string errors to the chat area.
 e2function number string:findRE(string pattern, start)
-	local OK, Ret = pcall(find, this, pattern, start)
+	local OK, Ret
+	if checkregex(this, pattern) then
+		OK, Ret = pcall(find, this, pattern, start)
+	else
+		OK, Ret = false, "regex is too complex!"
+	end
 	if not OK then
 		self.player:ChatPrint(Ret)
 		return 0
@@ -382,12 +396,17 @@ end
 
 ---  Finds and replaces every occurrence of <pattern> with <new> using regular expressions. Prints malformed string errors to the chat area.
 e2function string string:replaceRE(string pattern, string new)
-	local OK, NewStr = pcall(gsub, this, pattern, new)
+	local OK, Ret
+	if checkregex(this, pattern) then
+		OK, Ret = pcall(gsub, this, pattern, new)
+	else
+		OK, Ret = false, "regex is too complex!"
+	end
 	if not OK then
-		self.player:ChatPrint(NewStr)
+		self.player:ChatPrint(Ret)
 		return ""
 	else
-		return NewStr or ""
+		return Ret or ""
 	end
 end
 
@@ -402,7 +421,13 @@ e2function array string:explode(string delim)
 end
 
 e2function array string:explodeRE( string delim )
-	local ret = string_Explode( delim, this, true )
+	local ret
+	if checkregex(this, delim) then
+		ret = string_Explode( delim, this, true )
+	else
+		ret = {}
+		self.player:ChatPrint("regex is too complex!")
+	end
 	self.prf = self.prf + #ret * 0.3 + #this * 0.1
 	return ret
 end
@@ -437,7 +462,12 @@ local table_remove = table.remove
 
 --- runs [[string.match]](<this>, <pattern>) and returns the sub-captures as an array. Prints malformed pattern errors to the chat area.
 e2function array string:match(string pattern)
-	local args = {pcall(string_match, this, pattern)}
+	local args
+	if checkregex(this, pattern) then
+		args = {pcall(string_match, this, pattern)}
+	else
+		args = {false, "regex is too complex!"}
+	end
 	if not args[1] then
 		self.player:ChatPrint(args[2] or "Unknown error in str:match")
 		return {}
@@ -449,7 +479,12 @@ end
 
 --- runs [[string.match]](<this>, <pattern>, <position>) and returns the sub-captures as an array. Prints malformed pattern errors to the chat area.
 e2function array string:match(string pattern, position)
-	local args = {pcall(string_match, this, pattern, position)}
+	local args
+	if checkregex(this, pattern) then
+		args = {pcall(string_match, this, pattern, position)}
+	else
+		args = {false, "regex is too complex!"}
+	end
 	if not args[1] then
 		self.player:ChatPrint(args[2] or "Unknown error in str:match")
 		return {}
@@ -485,7 +520,12 @@ end
 --- runs [[string.gmatch]](<this>, <pattern>) and returns the captures in an array in a table. Prints malformed pattern errors to the chat area.
 -- (By Divran)
 e2function table string:gmatch(string pattern)
-	local OK, ret = pcall( gmatch, self, this, pattern )
+	local OK, ret
+	if checkregex(this, pattern) then
+		OK, ret = pcall( gmatch, self, this, pattern )
+	else
+		OK, ret = false, "regex is too complex!"
+	end
 	if (!OK) then
 		self.player:ChatPrint( ret or "Unknown error in str:gmatch" )
 		return newE2Table()
@@ -498,7 +538,12 @@ end
 -- (By Divran)
 e2function table string:gmatch(string pattern, position)
 	this = this:Right( -position-1 )
-	local OK, ret = pcall( gmatch, self, this, pattern )
+	local OK, ret
+	if checkregex(this, pattern) then
+		OK, ret = pcall( gmatch, self, this, pattern )
+	else
+		OK, ret = false, "regex is too complex!"
+	end
 	if (!OK) then
 		self.player:ChatPrint( ret or "Unknown error in str:gmatch" )
 		return newE2Table()
@@ -509,7 +554,12 @@ end
 
 --- runs [[string.match]](<this>, <pattern>) and returns the first match or an empty string if the match failed. Prints malformed pattern errors to the chat area.
 e2function string string:matchFirst(string pattern)
-	local OK, Ret = pcall(string_match, this, pattern)
+	local OK, Ret
+	if checkregex(this, pattern) then
+		OK, Ret = pcall(string_match, this, pattern)
+	else
+		OK, Ret = false, "regex is too complex!"
+	end
 	if not OK then
 		self.player:ChatPrint(Ret)
 		return ""
@@ -520,7 +570,12 @@ end
 
 --- runs [[string.match]](<this>, <pattern>, <position>) and returns the first match or an empty string if the match failed. Prints malformed pattern errors to the chat area.
 e2function string string:matchFirst(string pattern, position)
-	local OK, Ret = pcall(string_match, this, pattern, position)
+	local OK, Ret
+	if checkregex(this, pattern) then
+		OK, Ret = pcall(string_match, this, pattern, position)
+	else
+		OK, Ret = false, "regex is too complex!"
+	end
 	if not OK then
 		self.player:ChatPrint(Ret)
 		return ""
