1ef0f63dd1528f3d161f52f1aac97e78dfa7a259
thegrb93 <grbrown93@sbcglobal.net>
2021-06-24 02:25:28 -0400

    Add level setting, also play regardless of distance/location (#2183)

--- lua/entities/gmod_wire_soundemitter.lua
+++ lua/entities/gmod_wire_soundemitter.lua
@@ -24,13 +24,14 @@ function ENT:Initialize()
 	self:SetSolid( SOLID_VPHYSICS )
 
 	self.Inputs = WireLib.CreateInputs(self, { "A", "Toggle", "Volume", "Play", "Stop",
-		"PitchRelative", "Sample", "SampleName [STRING]" })
+		"PitchRelative", "Sample", "SampleName [STRING]", "Level" })
 	self.Outputs = WireLib.CreateOutputs(self, { "Duration", "Property Sound", "Properties [ARRAY]", "Memory" })
 
 	self.Samples = table.Copy(DefaultSamples)
 
 	self.Active = false
 	self.Volume = 100
+	self.Level = 80
 	self.Pitch = 100
 	self.sound = self.Samples[1]
 	-- self.sound is a string, self.SoundObj is a CSoundPatch
@@ -54,6 +55,7 @@ end
 	1: volume
 	2: pitchrelative
 	3: duration
+	6: level
 ]]
 function ENT:ReadCell(address)
 	address = math.floor(address)
@@ -65,6 +67,8 @@ function ENT:ReadCell(address)
 		return self.Pitch / 100
 	elseif address == 3 then
 		return self.Outputs.Duration.Value
+	elseif address == 6 then
+		return self.Level
 	end
 end
 
@@ -77,6 +81,7 @@ local cellsOut = {
 	[3] = "Sample",
 	[4] = "Play",
 	[5] = "Stop",
+	[6] = "Level",
 }
 
 function ENT:WriteCell(address, value)
@@ -114,6 +119,8 @@ function ENT:TriggerInput(iname, value)
 		self:StopSounds()
 	elseif iname == "Volume" then
 		self.Volume = math.Clamp(math.floor(value*100), 0.0, 100.0)
+	elseif iname == "Level" then
+		self.Level = math.Clamp(value, 55.0, 165.0)
 	elseif iname == "PitchRelative" then
 		self.Pitch = math.Clamp(math.floor(value*100), 0, 255)
 	elseif iname == "Sample" then
@@ -127,7 +134,9 @@ end
 function ENT:UpdateSound()
 	if self.NeedsRefresh or self.sound ~= self.ActiveSample then
 		self.NeedsRefresh = nil
-		self.SoundObj = CreateSound(self, self.sound)
+		local filter = RecipientFilter()
+		filter:AddAllPlayers()
+		self.SoundObj = CreateSound(self, self.sound, filter)
 		self.ActiveSample = self.sound
 
 		self.SoundProperties = sound.GetProperties(self.sound)
@@ -144,6 +153,7 @@ function ENT:UpdateSound()
 	end
 	self.SoundObj:ChangePitch(self.Pitch, 0)
 	self.SoundObj:ChangeVolume(self.Volume / 100.0, 0)
+	self.SoundObj:SetSoundLevel(self.Level)
 end
 
 function ENT:SetSound(soundName)
