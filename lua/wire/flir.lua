35e19a48d14075814f7916a190bb1cd7b8da0ca6
kerbalcadet <kerbalcadet@gmail.com>
2021-08-16 03:34:02 -0500

    Reworked FLIR visual behaviour

--- lua/wire/flir.lua
+++ lua/wire/flir.lua
@@ -12,6 +12,7 @@
 if not FLIR then FLIR = { enabled = false } end
 
 if CLIENT then
+
 	FLIR.living = CreateMaterial("flir_living", "UnlitGeneric", {
 		["$basetexture"] = "color/white",
 		["$model"] = 1,
@@ -20,41 +21,100 @@ if CLIENT then
 	FLIR.normal = CreateMaterial("flir_normal", "VertexLitGeneric", {
 		["$basetexture"] = "color/white",
 		["$model"] = 1,
-		["$halflambert"] = 1 -- causes the diffuse lighting to 'wrap around' more
+		["$halflambert"] = 1, -- causes the diffuse lighting to 'wrap around' more
+		["$color2"] = "[10.0 10.0 10.0]"
 	})
 
-	FLIR.colmod = {
-		[ "$pp_colour_addr" ] = 0.4,
-		[ "$pp_colour_addg" ] = -.5,
-		[ "$pp_colour_addb" ] = -.5,
-		[ "$pp_colour_brightness" ] = .1,
-		[ "$pp_colour_contrast" ] = 1.2,
-		[ "$pp_colour_colour" ] = 0,
-		[ "$pp_colour_mulr" ] = 0,
-		[ "$pp_colour_mulg" ] = 0,
-		[ "$pp_colour_mulb" ] = 0
+	FLIR.entcol = {
+		["$pp_colour_colour" ] = 0,
+		["$pp_colour_brightness"] = -0.3,
+		["$pp_colour_contrast"] = 1.0
+	}
+
+	FLIR.mapcol = {
+		[ "$pp_colour_brightness" ] = 0.8,
+		[ "$pp_colour_contrast" ] = 0.5,
+		[ "$pp_colour_colour"] = 0.0,
+	}
+
+	FLIR.skycol = {
+		[ "$pp_colour_brightness" ] = -0.3
 	}
 
 	local materialOverrides = {
 			PlayerDraw = { FLIR.living, FLIR.normal },
-			DrawOpaqueRenderables = { FLIR.normal, nil },
-			DrawTranslucentRenderables = { FLIR.normal, nil },
 			DrawSkybox = { FLIR.normal, nil }
 	}
 
+	local function SetFLIRMat(ent)
+		if not IsValid(ent) then return end
+
+		if ent:GetMoveType() == MOVETYPE_VPHYSICS or IsValid(ent:GetParent()) or ent:IsPlayer() or ent:IsNPC() or ent:IsRagdoll() then
+				ent.FLIRMat = ent:GetMaterial()
+				ent:SetMaterial("!flir_normal")	
+		end
+
+	end
+
+	hook.Add("OnEntityCreated", "flir", function(ent)
+		if FLIR.enabled then
+			SetFLIRMat(ent)
+		end
+	end)
+
+	hook.Add("CreateClientsideRagdoll", "flir", function(ent, rag)
+		if FLIR.enabled then
+			SetFLIRMat(rag)
+		end
+	end)
+
 	function FLIR.start()
 		if FLIR.enabled then return else FLIR.enabled = true end
+
+		bright = false
+		hook.Add("PreRender", "flirbright", function()			--rendermode 2 avoids fullbright issues with particles freaking out
+			render.SetLightingMode(2)								    --but still brightness dark areas as they should
+			bright = true
+		end)
+
+		hook.Add("PostRender", "flirbright", function()
+			if bright then
+				render.SetLightingMode(0)
+				bright = false
+			end
+		end)
+
+		hook.Add("PreDrawHUD", "flirbright", function()
+			if bright then
+				render.SetLightingMode(0)
+				bright = false
+			end
+		end)
+
+
 
 		for hookName, materials in pairs(materialOverrides) do
 			hook.Add("Pre" .. hookName, "flir", function() render.MaterialOverride(materials[1]) end)
 			hook.Add("Post" .. hookName, "flir", function() render.MaterialOverride(materials[2]) end)
 		end
 
-		hook.Add("RenderScreenspaceEffects", "flir", function()
-			DrawColorModify(FLIR.colmod)
-			DrawBloom(0,100,5,5,3,0.1,0,0,0)
-			DrawSharpen(1,0.5)
+		hook.Add("PostDraw2DSkyBox", "flir", function() --overrides 2d skybox to be gray, as it normally becomes white
+			DrawColorModify(FLIR.skycol)
 		end)
+
+		hook.Add("PreDrawOpaqueRenderables", "flir", function()
+			DrawColorModify(FLIR.mapcol)		--rendermode 2 saturates a lot
+		end)
+
+		hook.Add("RenderScreenspaceEffects", "flir", function()
+			DrawColorModify(FLIR.entcol)
+			DrawBloom(0.5,1.0,2,2,2,1, 1, 1, 1)
+			DrawBokehDOF(1, 0.1, 0.1)
+		end)
+
+		for k, v in pairs(ents.GetAll()) do
+			SetFLIRMat(v)
+		end
 	end
 
 	function FLIR.stop()
@@ -64,7 +124,19 @@ if CLIENT then
 			hook.Remove("Post" .. hookName, "flir")
 		end
 		hook.Remove("RenderScreenspaceEffects", "flir")
+		hook.Remove("PostDraw2DSkyBox", "flir")
+		hook.Remove("PreDrawOpaqueRenderables", "flir")
+		hook.Remove("PreRender", "flirbright")
+		hook.Remove("PostRender", "flirbright")
 		render.MaterialOverride(nil)
+
+		
+		for k, v in pairs(ents.GetAll()) do
+			if v.FLIRMat then
+				v:SetMaterial(v.FLIRMat)
+				v.FLIRMat = nil
+			end
+		end
 	end
 
 	function FLIR.enable(enabled)
