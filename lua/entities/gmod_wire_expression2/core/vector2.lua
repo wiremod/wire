ceefe801f79ee35125efd42881dfbed76497b64e
March Cain <marchcain1@gmail.com>
2021-09-11 16:26:18 -0700

    Addition and Subtraction Operators to Vector2 and Vector4 (#2228)
    * Vec2+Num & Vec2-Num operators
    
    * Vec4+Num & Vec4 -Num operators

--- lua/entities/gmod_wire_expression2/core/vector2.lua
+++ lua/entities/gmod_wire_expression2/core/vector2.lua
@@ -113,6 +113,18 @@ registerOperator("sub", "xv2xv2", "xv2", function(self, args)
 	return { rv1[1] - rv2[1], rv1[2] - rv2[2] }
 end)
 
+registerOperator("add", "xv2n", "xv2", function(self, args)
+	local op1, op2 = args[2], args[3]
+	local rv1, rv2 = op1[1](self, op1), op2[1](self, op2)
+	return { rv1[1] + rv2, rv1[2] + rv2 }
+end)
+
+registerOperator("sub", "xv2n", "xv2", function(self, args)
+	local op1, op2 = args[2], args[3]
+	local rv1, rv2 = op1[1](self, op1), op2[1](self, op2)
+	return { rv1[1] - rv2, rv1[2] - rv2 }
+end)
+
 registerOperator("mul", "nxv2", "xv2", function(self, args)
 	local op1, op2 = args[2], args[3]
 	local rv1, rv2 = op1[1](self, op1), op2[1](self, op2)
@@ -649,6 +661,18 @@ registerOperator("sub", "xv4xv4", "xv4", function(self, args)
 	local op1, op2 = args[2], args[3]
 	local rv1, rv2 = op1[1](self, op1), op2[1](self, op2)
 	return { rv1[1] - rv2[1], rv1[2] - rv2[2], rv1[3] - rv2[3], rv1[4] - rv2[4] }
+end)
+
+registerOperator("add", "xv4n", "xv4", function(self, args)
+	local op1, op2 = args[2], args[3]
+	local rv1, rv2 = op1[1](self, op1), op2[1](self, op2)
+	return { rv1[1] + rv2, rv1[2] + rv2, rv1[3] + rv2, rv1[4] + rv2 }
+end)
+
+registerOperator("sub", "xv4n", "xv4", function(self, args)
+	local op1, op2 = args[2], args[3]
+	local rv1, rv2 = op1[1](self, op1), op2[1](self, op2)
+	return { rv1[1] - rv2, rv1[2] - rv2, rv1[3] - rv2, rv1[4] - rv2 }
 end)
 
 registerOperator("mul", "nxv4", "xv4", function(self, args)
