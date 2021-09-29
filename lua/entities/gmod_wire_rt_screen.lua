902c3d3f07b93916cbb334fe0cf473e5d9a07d59
stepa2 <sssstepa452@yandex.ru>
2021-09-29 20:31:32 +0300

    Merge my wire addons into wire (#2231)
    * Allow calling entity:setPos and entity:setAng on entities without collisions
    Also allow calling entity:propManipulate, but position and rotation is only updated
    
    * Update prop.lua
    
    Removed tailing whitespace, fixed luacheck warning
    
    * Implemented do-while statement for E2 (#2191)
    
    * Removed debug messages I fogot to remove, added parsing tests, disabled bugged optimization
    
    * Linting pass
    
    * Added current version
    
    * Release version 1.0.0
    
    * Added addon.json, added publishing script
    
    * Fixed "Image filtering mode" hint, reworked screen material parameters system
    
    * Fixed publishing script
    
    * Fixed lua error, disabled drawing screen placeholder on opaque monitors
    
    * Bugfix
    
    * Fixed rendering camera sometimes
    
    * Merge preparation
    
    * Merge preparation
    
    * Improved entmanipulation caution message
    
    * Code style pass
    
    * Code linting pass 2
    
    * Decided not to include entmanipulation into PR
    
    * Improved RT Cameras naming change
    
    * Fixed camera not working after pasting dupe
    
    * Another code formatting fix
    
    * Another code formatting fix (2)
    I am not so creative to make so many different commit names
    
    * Removed 'continue' usages
    
    * Added missing RT_SIZE_ enum values to .luacheckrc
    Do not want to figure out how to use generate-luacheck.sh on windows
    
    * Improved naming

---
+++ lua/entities/gmod_wire_rt_screen.lua
@@ -0,0 +1,289 @@
+AddCSLuaFile()
+
+ENT.Type = "anim"
+ENT.Base = "base_wire_entity"
+ENT.PrintName = "Improved RT Screen"
+ENT.WireDebugName = "Improved RT Screen"
+
+function ENT:SetupDataTables()
+    self:NetworkVar("Bool", 0, "Active")
+	self:NetworkVar("Entity", 0, "Camera" )
+    self:NetworkVar("String", 0, "ScreenMaterial")
+    if CLIENT then
+        self:NetworkVarNotify("ScreenMaterial", self.ScreenMaterialChanged)
+    end
+
+    self:NetworkVar("Float", 0, "ScrollX")
+    self:NetworkVar("Float", 1, "ScrollY")
+    self:NetworkVar("Float", 2, "ScaleX")
+    self:NetworkVar("Float", 3, "ScaleY")
+
+    self:NetworkVar("String", 1, "MaterialParams")
+    if CLIENT then
+        self:NetworkVarNotify("MaterialParams", self.MaterialParamsChanged)
+    end
+end
+
+local MTLPARAM_TYPE = {
+    numint = {"SetInt", "NORMAL", nil },
+    numfloat = {"SetFloat", "NORMAL", nil },
+    vector3 = {"SetVector", "VECTOR", function(tbl) return Vector(tbl.x, tbl.y, tbl.z) end},
+    --vector4 = {"SetVector4D", "VECTOR4", nil }, --May not work
+    string = {"SetString", "STRING", nil },
+    texture = {"SetTexture", "STRING", nil},
+    matrix44 = {"SetMatrix", "matrix4", function(tbl) return Matrix({
+        {tbl.a1, tbl.a2, tbl.a3, tbl.a4},
+        {tbl.b1, tbl.b2, tbl.b3, tbl.b4},
+        {tbl.c1, tbl.c2, tbl.c3, tbl.c4},
+        {tbl.d1, tbl.d2, tbl.d3, tbl.d4}
+    })
+    end}
+}
+
+local MaterialParameters = {}
+local function GetMaterialParameters(mtl)
+    if MaterialParameters[mtl] ~= nil then
+        return MaterialParameters[mtl]
+    end
+
+    local mtlFile = file.Read("materials/improvedrt_screen/monitor_"..mtl..".vmt", "GAME")
+
+    if mtlFile == nil then return end
+
+    local mtl = util.KeyValuesToTable(mtlFile)
+    local params = {}
+
+    for mtlParam, tbl in pairs(mtl["!parameters"] or {}) do
+        local defaultParse = assert(MTLPARAM_TYPE[tbl.type])[3]
+
+        params[mtlParam] = {
+            WireName = tbl.wirename,
+            WireType = MTLPARAM_TYPE[tbl.type][2],
+            MaterialFn = MTLPARAM_TYPE[tbl.type][1],
+            Default = defaultParse and defaultParse(tbl.default) or tbl.default
+        }
+    end
+
+    MaterialParameters[mtl] = params
+
+    return params
+end
+
+
+if SERVER then
+    local InputsTable = {
+        "Active", "Camera [ENTITY]",
+        "Scroll X", "Scroll Y", "Scale X", "Scale Y"
+    }
+
+    function ENT:Initialize()
+        self:PhysicsInit( SOLID_VPHYSICS )
+        self:SetMoveType( MOVETYPE_VPHYSICS )
+        self:SetSolid( SOLID_VPHYSICS )
+        self:SetCollisionGroup( COLLISION_GROUP_NONE )
+        self:DrawShadow( false )
+
+        self.Inputs = Wire_CreateInputs( self, InputsTable )
+
+        self:SetScaleX(1)
+        self:SetScaleY(1)
+
+        self.MaterialParams = {}
+        self:SetMaterialParams("{}")
+    end
+
+    function ENT:Setup(screen_material) --(model, screen_material)
+        --self:SetModel(model or "models/kobilica/wiremonitorbig.mdl")
+        self:SetScreenMaterial(screen_material or "normal")
+
+        local Inputs = table.Copy(InputsTable)
+
+        for _, tbl in pairs(GetMaterialParameters(self:GetScreenMaterial())) do
+            table.insert(Inputs, tbl.WireName.." ["..tbl.WireType.."]")
+        end
+
+        self.Inputs = Wire_CreateInputs( self, Inputs )
+    end
+
+end
+
+function ENT:TriggerInput( name, value )
+    if name == "Active" then
+        self:SetActive(value ~= 0)
+    elseif name == "Camera" then
+        if value ~= nil and not value:IsValid() then
+            return
+        end
+        if value == nil or value:GetClass() == "gmod_wire_rt_camera" then
+            self:SetCamera(value)
+        end
+    elseif name == "Scroll X" then
+        self:SetScrollX(value)
+    elseif name == "Scroll Y" then
+        self:SetScrollY(value)
+    elseif name == "Scale X" then
+        self:SetScaleX(value)
+    elseif name == "Scale Y" then
+        self:SetScaleY(value)
+    else
+        self.MaterialParams[name] = value
+        self:SetMaterialParams(util.TableToJSON(self.MaterialParams))
+    end
+end
+
+function ENT:ShouldDrawCamera(ply)
+    local maxDist = ply:GetInfoNum("wire_rt_screen_renderdistance", 512)
+
+    return ply:EyePos():DistToSqr(self:GetPos()) <= maxDist * maxDist
+end
+
+if CLIENT then
+    CreateClientConVar("wire_rt_screen_renderdistance","512", nil,true,nil, 0)
+
+    local MATERIALS = {}
+
+    local function GetOrAllocMaterial(name)
+        if MATERIALS[name] ~= nil then
+            return MATERIALS[name]
+        end
+
+        local path = "improvedrt_screen/monitor_"..name..".vmt"
+
+        if not file.Exists("materials/"..path, "GAME") then
+            MsgN("ImprovedRTCameras: material "..path.." does not exist on client for some reason!")
+            MsgN("Screen would not be rendered")
+            return nil
+        end
+
+        local mtl = Material(path)
+        MATERIALS[name] = mtl
+        return mtl
+    end
+
+    function ENT:Initialize()
+        self.MonitorDesc = WireGPU_Monitors[self:GetModel()]
+        self:ScreenMaterialChanged(nil,nil,self:GetScreenMaterial())
+        self.MaterialParams = {}
+    end
+
+    function ENT:ScreenMaterialChanged(_,_,mtl)
+        self.Material = GetOrAllocMaterial(mtl)
+        self.MaterialParamsDesc = GetMaterialParameters(mtl)
+    end
+
+    function ENT:MaterialParamsChanged(_,_,materialParams)
+        self.MaterialParams = util.JSONToTable(materialParams)
+    end
+
+    function ENT:Think()
+        local camera = self:GetCamera()
+
+        if not self:GetActive() then
+            if IsValid(camera) then
+                camera:SetIsObserved(false)
+            end
+            self.ShouldRenderCamera = false
+
+            return
+        end
+        self.ShouldRenderCamera = self:ShouldDrawCamera(LocalPlayer())
+
+        if IsValid(camera) then
+            camera:SetIsObserved(self.ShouldRenderCamera)
+        end
+    end
+
+    function ENT:DrawScreen()
+        local camera = self:GetCamera()
+
+        local monitor = self.MonitorDesc
+        local material = self.Material
+        local rt = camera.RenderTarget
+        assert(rt ~= nil)
+
+        material:SetTexture(material:GetString("!targettex1"), rt)
+
+        local tex2 = material:GetString("!targettex2")
+        if tex2 ~= nil then material:SetTexture(tex2, rt) end
+
+        for mtl_param, tbl in pairs(self.MaterialParamsDesc) do
+            --print(self.MaterialParams[tbl.WireName])
+
+            local value = self.MaterialParams[tbl.WireName] or tbl.Default
+
+            material[tbl.MaterialFn](material, mtl_param, value)
+        end
+
+        local xraw = 256 / monitor.RatioX
+        local yraw = 256
+        local x1 = -xraw
+        local x2 = xraw
+        local y1 = -yraw
+        local y2 = yraw
+
+        local u1, v1 = 0, 0
+        local u2, v2 = self:GetScaleX(), monitor.RatioX * self:GetScaleY()
+        local scrU = self:GetScrollX()
+        local scrV = self:GetScrollY()
+        u1 = u1 + scrU u2 = u2 + scrU
+        v1 = v1 + scrV v2 = v2 + scrV
+
+        cam.Start3D2D(
+            self:LocalToWorld(monitor.offset),
+            self:LocalToWorldAngles(monitor.rot),
+            monitor.RS
+        )
+            surface.SetDrawColor(255,255,255)
+            surface.SetMaterial(material)
+            surface.DrawPoly({
+                { x = x2, y = y1, u = u2, v = v1},
+                { x = x2, y = y2, u = u2, v = v2},
+                { x = x1, y = y2, u = u1, v = v2},
+                { x = x1, y = y1, u = u1, v = v1},
+            })
+        cam.End3D2D()
+    end
+
+    function ENT:DrawDummy()
+        local monitor = self.MonitorDesc
+
+        local xraw = 256 / monitor.RatioX
+        local yraw = 256
+        local x1 = -xraw
+        local x2 = xraw
+        local y1 = -yraw
+        local y2 = yraw
+
+        cam.Start3D2D(
+            self:LocalToWorld(monitor.offset),
+            self:LocalToWorldAngles(monitor.rot),
+            monitor.RS
+        )
+            draw.NoTexture()
+            surface.SetDrawColor(0,0,0)
+            surface.DrawPoly({
+                { x = x2, y = y1, u = 1, v = 0},
+                { x = x2, y = y2, u = 1, v = 1},
+                { x = x1, y = y2, u = 0, v = 1},
+                { x = x1, y = y1, u = 0, v = 0},
+            })
+        cam.End3D2D()
+    end
+
+    function ENT:Draw()
+        self:DrawModel()
+
+        if self.MonitorDesc == nil then
+            return
+        end
+
+        if self:GetActive() and self.ShouldRenderCamera and self.Material ~= nil and IsValid(self:GetCamera()) then
+            self:DrawScreen()
+        elseif not self.translucent then
+            self:DrawDummy()
+        end
+    end
+end
+
+duplicator.RegisterEntityClass("improvedrt_screen", WireLib.MakeWireEnt, "Data",--[["Model",]] "ScreenMaterial")
+duplicator.RegisterEntityClass("gmod_wire_rt_screen", WireLib.MakeWireEnt, "Data",--[["Model",]] "ScreenMaterial")
