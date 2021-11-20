5fc7de3fed5fa17ffad85392e53be65982ad4bed
kerbalcadet <65194964+kerbalcadet@users.noreply.github.com>
2021-11-20 07:26:31 -0600

    Reworked FLIR visual behaviour (#2216)
    * Reworked FLIR visual behaviour
    
    * added hook removals, fixed 3d skybox color
    
    * adjusted color modifies, streamlined hooks, removed old material system
    
    * client ragdoll hook -> flir.Start() to fix no heat on bodies
    
    * removed print(test) from function
    
    * removed flir.living material, renamed hooks flir -> wire_flir

--- lua/wire/flir.lua
+++ lua/wire/flir.lua
@@ -12,59 +12,109 @@
 if not FLIR then FLIR = { enabled = false } end
 
 if CLIENT then
-	FLIR.living = CreateMaterial("flir_living", "UnlitGeneric", {
-		["$basetexture"] = "color/white",
-		["$model"] = 1,
-	})
 
 	FLIR.normal = CreateMaterial("flir_normal", "VertexLitGeneric", {
 		["$basetexture"] = "color/white",
 		["$model"] = 1,
-		["$halflambert"] = 1 -- causes the diffuse lighting to 'wrap around' more
+		["$halflambert"] = 1, -- causes the diffuse lighting to 'wrap around' more
+		["$color2"] = "[10.0 10.0 10.0]"
 	})
 
-	FLIR.colmod = {
-		[ "$pp_colour_addr" ] = 0.4,
-		[ "$pp_colour_addg" ] = -.5,
-		[ "$pp_colour_addb" ] = -.5,
-		[ "$pp_colour_brightness" ] = .1,
-		[ "$pp_colour_contrast" ] = 1.2,
-		[ "$pp_colour_colour" ] = 0,
-		[ "$pp_colour_mulr" ] = 0,
-		[ "$pp_colour_mulg" ] = 0,
-		[ "$pp_colour_mulb" ] = 0
+	FLIR.entcol = {
+		["$pp_colour_colour" ] = 0,
+		["$pp_colour_brightness"] = -0.00,
+		["$pp_colour_contrast"] = 4
 	}
 
-	local materialOverrides = {
-			PlayerDraw = { FLIR.living, FLIR.normal },
-			DrawOpaqueRenderables = { FLIR.normal, nil },
-			DrawTranslucentRenderables = { FLIR.normal, nil },
-			DrawSkybox = { FLIR.normal, nil }
+	FLIR.mapcol = {
+		[ "$pp_colour_brightness" ] = 0,
+		[ "$pp_colour_contrast" ] = 0.2
 	}
+
+	FLIR.skycol = {
+		[ "$pp_colour_contrast" ] = 0.2,
+		[ "$pp_colour_brightness" ] = 1
+	}
+
+	local function SetFLIRMat(ent)
+		if not IsValid(ent) then return end
+
+		if ent:GetMoveType() == MOVETYPE_VPHYSICS or IsValid(ent:GetParent()) or ent:IsPlayer() or ent:IsNPC() or ent:IsRagdoll() then
+				ent.FLIRMat = ent:GetMaterial()
+				ent:SetMaterial("!flir_normal")	
+		end
+
+	end
+
+	
 
 	function FLIR.start()
 		if FLIR.enabled then return else FLIR.enabled = true end
 
-		for hookName, materials in pairs(materialOverrides) do
-			hook.Add("Pre" .. hookName, "flir", function() render.MaterialOverride(materials[1]) end)
-			hook.Add("Post" .. hookName, "flir", function() render.MaterialOverride(materials[2]) end)
-		end
+		
 
-		hook.Add("RenderScreenspaceEffects", "flir", function()
-			DrawColorModify(FLIR.colmod)
-			DrawBloom(0,100,5,5,3,0.1,0,0,0)
-			DrawSharpen(1,0.5)
+		bright = false
+		hook.Add("PreRender", "wire_flir", function()			--lighting mode 1  = fullbright
+			render.SetLightingMode(1)			
 		end)
+
+
+		hook.Add("PostDraw2DSkyBox", "wire_flir", function() --overrides 2d skybox to be gray, as it normally becomes white
+			DrawColorModify(FLIR.skycol)
+		end)
+
+		hook.Add("PostDrawTranslucentRenderables", "wire_flir", function(_a, _b, sky) 
+			if not sky then 
+				render.SetLightingMode(0)
+				DrawColorModify(FLIR.mapcol)
+			end
+		end)
+
+		hook.Add("RenderScreenspaceEffects", "wire_flir", function()
+			DrawColorModify(FLIR.entcol)
+			DrawBloom(0.5,1.0,2,2,2,1, 1, 1, 1)
+			DrawBokehDOF(1, 0.1, 0.1)
+			
+		end)
+
+
+		hook.Add("OnEntityCreated", "wire_flir", function(ent)
+			if FLIR.enabled then
+				SetFLIRMat(ent)
+			end
+		end)
+
+		hook.Add("CreateClientsideRagdoll", "wire_flir", function(ent, rag)
+			if FLIR.enabled then
+				SetFLIRMat(rag)
+			end
+		end)
+
+		for k, v in pairs(ents.GetAll()) do
+			SetFLIRMat(v)
+		end
 	end
 
 	function FLIR.stop()
 		if FLIR.enabled then FLIR.enabled = false else return end
-		for hookName, materials in pairs(materialOverrides) do
-			hook.Remove("Pre" .. hookName, "flir")
-			hook.Remove("Post" .. hookName, "flir")
-		end
-		hook.Remove("RenderScreenspaceEffects", "flir")
+
+		render.SetLightingMode(0)
+
+		hook.Remove("PostDrawTranslucentRenderables", "wire_flir")
+		hook.Remove("RenderScreenspaceEffects", "wire_flir")
+		hook.Remove("PostDraw2DSkyBox", "wire_flir")
+		hook.Remove("PreRender", "wire_flir")
+		hook.Remove("OnEntityCreated", "wire_flir")
+		hook.Remove("CreateClientsideRagdoll", "wire_flir")
 		render.MaterialOverride(nil)
+
+		
+		for k, v in pairs(ents.GetAll()) do
+			if v.FLIRMat then
+				v:SetMaterial(v.FLIRMat)
+				v.FLIRMat = nil
+			end
+		end
 	end
 
 	function FLIR.enable(enabled)
