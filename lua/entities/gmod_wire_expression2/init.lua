67310ee2c17e1e4722ed8ddb12b13861f832034c
Divran <arviddivran@gmail.com>
2021-12-19 01:18:52 +0100

    Adjust opcounters based on time passed since error/reset
    On e2 reset, reduce the opcounters based on the time passed since the
    last time the chip was reset or errored. Waiting 30 seconds results in
    the smallest 0.1 multiplier
    Fixes #2257
    Thanks to NovaAstral for reporting the issue

--- lua/entities/gmod_wire_expression2/init.lua
+++ lua/entities/gmod_wire_expression2/init.lua
@@ -234,6 +234,7 @@ function ENT:Error(message, overlaytext)
 	self:SetColor(Color(255, 0, 0, self:GetColor().a))
 
 	self.error = true
+	self.lastResetOrError = CurTime()
 	-- ErrorNoHalt(message .. "\n")
 	WireLib.ClientError(message, self.player)
 end
@@ -334,6 +335,15 @@ function ENT:PrepareIncludes(files)
 end
 
 function ENT:ResetContext()
+	local resetPrfMult = 1
+	if self.lastResetOrError then
+		-- reduces all the opcounters based on the time passed since 
+		-- the last time the chip was reset or errored
+		-- waiting up to 30s before resetting results in a 0.1 multiplier 
+		resetPrfMult = math.max(0.1,(30 - (CurTime() - self.lastResetOrError)) / 30)
+	end
+	self.lastResetOrError = CurTime()
+
 	local context = {
 		data = {},
 		vclk = {}, -- Used only by arrays and tables!
@@ -342,11 +352,11 @@ function ENT:ResetContext()
 		entity = self,
 		player = self.player,
 		uid = self.uid,
-		prf = (self.context and self.context.prf) or 0,
-		prfcount = (self.context and self.context.prfcount) or 0,
-		prfbench = (self.context and self.context.prfbench) or 0,
-		time = (self.context and self.context.time) or 0,
-		timebench = (self.context and self.context.timebench) or 0,
+		prf = (self.context and (self.context.prf*resetPrfMult)) or 0,
+		prfcount = (self.context and (self.context.prfcount*resetPrfMult)) or 0,
+		prfbench = (self.context and (self.context.prfbench*resetPrfMult)) or 0,
+		time = (self.context and (self.context.time*resetPrfMult)) or 0,
+		timebench = (self.context and (self.context.timebench*resetPrfMult)) or 0,
 		includes = self.includes
 	}
 
