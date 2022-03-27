2162d1182d59085b3af89a75add594c341a02736
Garrett Brown <grbrown93@sbcglobal.net>
2022-03-27 03:39:05 -0400

    Use objects for clutches

--- lua/entities/gmod_wire_clutch.lua
+++ lua/entities/gmod_wire_clutch.lua
@@ -6,6 +6,79 @@ ENT.WireDebugName = "Clutch"
 
 if CLIENT then return end -- No more client
 
+local Clutch = {
+	__index = {
+		createAdvBallsocket = function(self)
+			if self.friction <= 0 then return end
+
+			local ballsocket = constraint.AdvBallsocket( self.ent1, self.ent2, 0, 0,
+				Vector(0,0,0), Vector(0,0,0), 0, 0,
+				-180, -180, -180, 180, 180, 180,
+				self.friction, self.friction, self.friction, 1, 0 )
+
+			if ballsocket then
+				-- Prevent ball socket from being affected by dupe/remove functions
+				ballsocket.Type = ""
+				ballsocket:CallOnRemove( "WireClutchRemove", function()
+					if self.clutch_ballsockets[ballsocket] then
+						-- The table value is still true so something unknown killed the ballsocket
+						-- Set the table so that nothing else runs into issues
+						self.clutch_ballsockets[ballsocket] = nil
+						-- Wait a frame so nothing bad happens, then rebuild it
+						timer.Simple(0, function()
+							if self.controller:IsValid() and self:isValid() then
+								self:createAdvBallsocket()
+							end
+						end)
+					end
+				end)
+				self.ballsocket = ballsocket
+			end
+		end,
+		setFriction = function(self, friction)
+			self.friction = friction
+			self:remove()
+			self:createAdvBallsocket()
+		end,
+		saveMass = function(self)
+			self.ent1mass = self.phys1:GetMass()
+			self.ent2mass = self.phys2:GetMass()
+			self.phys1:SetMass(1)
+			self.phys2:SetMass(1)
+		end,
+		restoreMass = function(self)
+			self.phys1:SetMass(self.ent1mass)
+			self.phys2:SetMass(self.ent2mass)
+		end,
+		remove = function(self)
+			if self.ballsocket and self.ballsocket:IsValid() then
+				self.ballsocket:RemoveCallOnRemove( "WireClutchRemove" )
+				self.ballsocket:Remove()
+			end
+		end,
+		isValid = function(self)
+			return self.ent1:IsValid() and self.ent2:IsValid() and self.phys1:IsValid() and self.phys2:IsValid()
+		end
+	},
+	__call = function(meta, controller, ent1, ent2, friction)
+		local clutch = setmetatable({
+			controller = controller,
+			friction = friction,
+			ent1 = ent1,
+			ent2 = ent2,
+			phys1 = ent1:GetPhysicsObject(),
+			phys2 = ent2:GetPhysicsObject(),
+		}, meta)
+		if clutch:isValid() then
+			clutch:saveMass()
+			clutch:createAdvBallsocket()
+			clutch:restoreMass()
+			return clutch
+		end
+	end
+}
+setmetatable(Clutch, Clutch)
+
 function ENT:Initialize()
 	self:PhysicsInit( SOLID_VPHYSICS )
 	self:SetMoveType( MOVETYPE_VPHYSICS )
@@ -40,96 +113,48 @@ end
    Functions for handling clutch constraints
 ---------------------------------------------------------]]
 
-function ENT:ClutchExists( Ent1, Ent2 )
-	for k, _ in pairs( self.clutch_ballsockets ) do
-		if  ( Ent1 == k.Ent1 and Ent2 == k.Ent2 ) or
-			( Ent1 == k.Ent2 and Ent2 == k.Ent1 ) then
+function ENT:ClutchExists( ent1, ent2 )
+	for clutch in pairs( self.clutch_ballsockets ) do
+		if  ( ent1 == clutch.ent1 and ent2 == clutch.ent2 ) or
+			( ent1 == clutch.ent2 and ent2 == clutch.ent1 ) then
 			return true
 		end
 	end
-
 	return false
 end
 
 
--- Returns an array with each entry as a table containing Ent1, Ent2
+-- Returns an array with each entry as a table containing ent1, ent2
 function ENT:GetConstrainedPairs()
 	local ConstrainedPairs = {}
-	for k, _ in pairs( self.clutch_ballsockets ) do
-		if IsValid( k ) then
-			table.insert( ConstrainedPairs, {Ent1 = k.Ent1, Ent2 = k.Ent2} )
+	for clutch in pairs( self.clutch_ballsockets ) do
+		if clutch:isValid() then
+			table.insert( ConstrainedPairs, {ent1 = clutch.ent1, ent2 = clutch.ent2} )
 		else
-			self.clutch_ballsockets[k] = nil
+			self.clutch_ballsockets[clutch] = nil
 		end
 	end
 	return ConstrainedPairs
 end
 
-
-local function NewBallSocket( ent1, ent2, friction )
-	if not (ent1:IsValid() and ent2:IsValid()) then return false end
-	local phys1, phys2 = ent1:GetPhysicsObject(), ent2:GetPhysicsObject()
-	if not (phys1:IsValid() and phys2:IsValid()) then return false end
-
-	local mass1, mass2 = phys1:GetMass(), phys2:GetMass()
-	phys1:SetMass(1)
-	phys2:SetMass(1)
-
-	local ballsocket = constraint.AdvBallsocket( ent1, ent2, 0, 0,
-		Vector(0,0,0), Vector(0,0,0), 0, 0,
-		-180, -180, -180, 180, 180, 180,
-		friction, friction, friction, 1, 0 )
-
-	if ballsocket then
-		-- Prevent ball socket from being affected by dupe/remove functions
-		ballsocket.Type = ""
-	end
-
-	phys1:SetMass(mass1)
-	phys2:SetMass(mass2)
-
-	return ballsocket
-end
-
-
 -- Register a new clutch association with the controller
-function ENT:AddClutch( Ent1, Ent2 )
-	local ballsocket = NewBallSocket( Ent1, Ent2, self.clutch_friction )
-
-	if ballsocket then
-		self.clutch_ballsockets[ballsocket] = true
-		ballsocket:CallOnRemove( "WireClutchRemove", function()
-			if self.clutch_ballsockets[ballsocket] then
-				-- The table value is still true so something unknown killed the ballsocket
-				-- Set the table so that nothing else runs into issues
-				self.clutch_ballsockets[ballsocket] = nil
-				-- Wait a frame so nothing bad happens, then rebuild it
-				timer.Simple(0, function()
-					if self:IsValid() then
-						self:AddClutch( Ent1, Ent2 )
-					end
-				end)
-			end
-		end)
+function ENT:AddClutch( ent1, ent2 )
+	local clutch = Clutch( self, ent1, ent2, self.clutch_friction )
+	if clutch then
+		self.clutch_ballsockets[clutch] = true
 	end
-
-	return ballsocket
+	return clutch
 end
-
 
 -- Remove a new clutch association from the controller
-function ENT:RemoveClutch( const )
-	self.clutch_ballsockets[const] = nil
-
-	if const:IsValid() then
-		const:RemoveCallOnRemove( "WireClutchRemove" )
-		const:Remove()
-	end
+function ENT:RemoveClutch(clutch)
+	self.clutch_ballsockets[clutch] = nil
+	clutch:remove()
 end
 
 function ENT:OnRemove()
-	for k in pairs( self.clutch_ballsockets ) do
-		self:RemoveClutch( k )
+	for clutch in pairs( self.clutch_ballsockets ) do
+		clutch:remove()
 	end
 end
 
@@ -139,20 +164,18 @@ function ENT:UpdateFriction(value)
 	if self.LastUpdated == CurTime() then self:NextThink(CurTime()) return end
 	self.LastUpdated = CurTime()
 
-	-- Update all registered ball socket constraints
-	for const in pairs( self.clutch_ballsockets ) do
-		if const:IsValid() then
-			local ent1, ent2 = const.Ent1, const.Ent2
-			if ent1:IsValid() and ent2:IsValid() then
-				-- There seems to be no direct way to edit constraint friction, so we must create a new ball socket constraint
-				self:RemoveClutch( const )
-				self:AddClutch( ent1, ent2 )
-			else
-				self.clutch_ballsockets[const] = nil
-			end
+	for clutch in pairs( self.clutch_ballsockets ) do
+		if clutch:isValid() then
+			clutch:saveMass()
 		else
-			self.clutch_ballsockets[const] = nil
+			self.clutch_ballsockets[clutch] = nil
 		end
+	end
+	for clutch in pairs( self.clutch_ballsockets ) do
+		clutch:setFriction(self.clutch_friction)
+	end
+	for clutch in pairs( self.clutch_ballsockets ) do
+		clutch:restoreMass()
 	end
 
 	self:UpdateOverlay()
@@ -181,8 +204,8 @@ function ENT:BuildDupeInfo()
 
 	for k, v in pairs( self:GetConstrainedPairs() ) do
 		info.constrained_pairs[k] = {}
-		info.constrained_pairs[k].Ent1 = v.Ent1:EntIndex() or 0
-		info.constrained_pairs[k].Ent2 = v.Ent2:EntIndex() or 0
+		info.constrained_pairs[k].Ent1 = v.ent1:EntIndex() or 0
+		info.constrained_pairs[k].Ent2 = v.ent2:EntIndex() or 0
 	end
 
 	return info
@@ -197,12 +220,13 @@ function ENT:ApplyDupeInfo(ply, ent, info, GetEntByID)
 		Ent2 = GetEntByID(v.Ent2, game.GetWorld())
 
 		if IsValid(Ent1) and
-		   Ent1 ~= Ent2 and
-		   hook.Run( "CanTool", ply, WireLib.dummytrace(Ent1), "ballsocket_adv" ) and
-		   hook.Run( "CanTool", ply, WireLib.dummytrace(Ent2), "ballsocket_adv" ) then
+			Ent1 ~= Ent2 and
+			hook.Run( "CanTool", ply, WireLib.dummytrace(Ent1), "ballsocket_adv" ) and
+			hook.Run( "CanTool", ply, WireLib.dummytrace(Ent2), "ballsocket_adv" ) then
 			self:AddClutch( Ent1, Ent2 )
 		end
 	end
+	self:UpdateOverlay()
 end
 
 duplicator.RegisterEntityClass("gmod_wire_clutch", WireLib.MakeWireEnt, "Data")
