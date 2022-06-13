5fdeb01a1f3aa029bb6612ba12b19356773b14c4
TwistedTail <8784231+TwistedTail@users.noreply.github.com>
2022-06-13 01:33:50 -0400

    Fixed AdvBallsocket not using the proper argument for local offset (#2340)
    * Added ballsocket(evevvvv) and ballsocket(evevvvvn)
    
    - Added two ballsocket E2 functions that will take advantage of both local vector arguments from `constraints.AdvBallsocket`
    
    * Removed new functions, switched use of LPos2 on AdvBallsocket
    
    - Removed new functions as requested.
    - All functions using AdvBallsocket constraints will now properly use the `LPos1` argument instead of `LPos2` for the offset.

--- lua/entities/gmod_wire_expression2/core/custom/constraintcore.lua
+++ lua/entities/gmod_wire_expression2/core/custom/constraintcore.lua
@@ -71,28 +71,28 @@ end
 e2function void ballsocket(entity ent1, vector v, entity ent2, friction)
 	if not checkEnts(self, ent1, ent2) then return end
 	local vec = Vector(v[1], v[2], v[3])
-	addundo(self, constraint.AdvBallsocket(ent1, ent2, 0, 0, Vector(), vec, 0, 0, -180, -180, -180, 180, 180, 180, friction, friction, friction, 0, 0), "ballsocket")
+	addundo(self, constraint.AdvBallsocket(ent1, ent2, 0, 0, vec, Vector(), 0, 0, -180, -180, -180, 180, 180, 180, friction, friction, friction, 0, 0), "ballsocket")
 end
 
 --- Creates an adv ballsocket between <ent1> and <ent2> at <v>, which is local to <ent1>, with many settings
 e2function void ballsocket(entity ent1, vector v, entity ent2, vector mins, vector maxs, vector frictions)
 	if not checkEnts(self, ent1, ent2) then return end
 	local vec = Vector(v[1], v[2], v[3])
-	addundo(self, constraint.AdvBallsocket(ent1, ent2, 0, 0, Vector(), vec, 0, 0, mins[1], mins[2], mins[3], maxs[1], maxs[2], maxs[3], frictions[1], frictions[2], frictions[3], 0, 0), "ballsocket")
+	addundo(self, constraint.AdvBallsocket(ent1, ent2, 0, 0, vec, Vector(), 0, 0, mins[1], mins[2], mins[3], maxs[1], maxs[2], maxs[3], frictions[1], frictions[2], frictions[3], 0, 0), "ballsocket")
 end
 
 --- Creates an adv ballsocket between <ent1> and <ent2> at <v>, which is local to <ent1>, with many settings
 e2function void ballsocket(entity ent1, vector v, entity ent2, vector mins, vector maxs, vector frictions, rotateonly)
 	if not checkEnts(self, ent1, ent2) then return end
 	local vec = Vector(v[1], v[2], v[3])
-	addundo(self, constraint.AdvBallsocket(ent1, ent2, 0, 0, Vector(), vec, 0, 0, mins[1], mins[2], mins[3], maxs[1], maxs[2], maxs[3], frictions[1], frictions[2], frictions[3], rotateonly, 0), "ballsocket")
+	addundo(self, constraint.AdvBallsocket(ent1, ent2, 0, 0, vec, Vector(), 0, 0, mins[1], mins[2], mins[3], maxs[1], maxs[2], maxs[3], frictions[1], frictions[2], frictions[3], rotateonly, 0), "ballsocket")
 end
 
 --- Creates an angular weld (angles are fixed, position isn't) between <ent1> and <ent2> at <v>, which is local to <ent1>
 e2function void weldAng(entity ent1, vector v, entity ent2)
 	if not checkEnts(self, ent1, ent2) then return end
 	local vec = Vector(v[1], v[2], v[3])
-	addundo(self, constraint.AdvBallsocket(ent1, ent2, 0, 0, Vector(), vec, 0, 0, 0, -0, 0, 0, 0, 0, 0, 0, 0, 1, 0), "ballsocket")
+	addundo(self, constraint.AdvBallsocket(ent1, ent2, 0, 0, vec, Vector(), 0, 0, 0, -0, 0, 0, 0, 0, 0, 0, 0, 1, 0), "ballsocket")
 end
 
 
