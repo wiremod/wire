e8d5bdb897b686ac70df6788fb18193d5c3f827e
shadow7483147 <tylern64@gmail.com>
2022-07-06 15:05:53 -0500

    Revert "Assume mount.cfg as default if no other paths are found"
    This reverts commit 7721c48819b57a62df7a503383806e381ccd81c9.

--- lua/wire/client/sound_browser.lua
+++ lua/wire/client/sound_browser.lua
@@ -147,7 +147,7 @@ local function GetInfoTable(strfile)
 	if tabproperty then
 		T = tabproperty
 	else
-		T.Path = {strfile, strSource or "mount.cfg", strSourceType}
+		T.Path = {strfile, strSource or "n/a", strSourceType}
 		T.Duration = {strduration or "n/a", nduration and nduration.." sec"}
 		T.Size = {strsize or "n/a", nsizeB and nsizeB.." Bytes"}
 		T.Format = strformat
@@ -198,10 +198,11 @@ local function GenerateInfoTree(strfile, backnode, count)
 			subnode.IsDataNode = true
 			subnode.IsSoundNode = true
 			subnode = node:AddNode(SoundData[index][3]=="game" and TranslateGameMounts[SoundData[index][2]] or SoundData[index][2],
-				SoundData[index][3]=="game" and "games/16/"..SoundData[index][2]..".png" or 
+				SoundData[index][3]=="game" and TranslateGameMounts[SoundData[index][2]] and "games/16/"..SoundData[index][2]..".png" or 
+				SoundData[index][3]=="game" and "icon16/folder_link.png" or
 				SoundData[index][3]=="legacy" and "icon16/folder_brick.png" or
-				SoundData[index][3]=="workshop" and "icon16/bricks.png" or
-				"icon16/folder_link.png")
+				SoundData[index][3]=="workshop" and "icon16/bricks.png"
+				or "icon16/page.png")
 			subnode.IsDataNode = true
 		end
 		do
