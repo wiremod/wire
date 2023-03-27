d8641a47dcc8cf69552e8d7a98cf770238982e52
relt-1 <michalpulawski1@gmail.com>
2023-03-27 19:35:27 +0200

    Make digital screen better

--- lua/entities/gmod_wire_digitalscreen/cl_init.lua
+++ lua/entities/gmod_wire_digitalscreen/cl_init.lua
@@ -248,17 +248,12 @@ function ENT:RedrawPixel(a)
 		cr, cg, cb = (transformcolor[colormode] or transformcolor[0])(c)
 	end
 
-	local xstep = (1024/self.ScreenWidth)
-	local ystep = (1024/self.ScreenHeight)
 
 	surface.SetDrawColor(cr,cg,cb,255)
-	local tx, ty = floor(x*xstep), floor(y*ystep)
-	surface.DrawRect( tx, ty, floor((x+1)*xstep-tx), floor((y+1)*ystep-ty) )
+	surface.DrawRect( x, y, 1, 1 )
 end
 
 function ENT:RedrawRow(y)
-	local xstep = (1024/self.ScreenWidth)
-	local ystep = (1024/self.ScreenHeight)
 	if y >= self.ScreenHeight then return end
 	local a = y*self.ScreenWidth
 
@@ -277,8 +272,7 @@ function ENT:RedrawRow(y)
 		end
 
 		surface.SetDrawColor(cr,cg,cb,255)
-		local tx, ty = floor(x*xstep), floor(y*ystep)
-		surface.DrawRect( tx, ty, floor((x+1)*xstep-tx), floor((y+1)*ystep-ty) )
+		surface.DrawRect( x, y, 1, 1 )
 	end
 end
 
@@ -320,7 +314,7 @@ function ENT:Draw()
 		end)
 	end
 
-	self.GPU:Render()
+	self.GPU:Render(0,0,1024,1024,nil,-(1024-self.ScreenWidth)/1024,-(1024-self.ScreenHeight)/1024)
 	Wire_Render(self)
 end
 
