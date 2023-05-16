ce603f8a2f28cf4870ba1dfbf452ffb19ddb3b7f
Denneisk <denneisk@protonmail.com>
2023-05-16 02:00:24 -0400

    setPhysicalMaterial, setVelocity

--- lua/entities/gmod_wire_expression2/core/custom/bonecore.lua
+++ lua/entities/gmod_wire_expression2/core/custom/bonecore.lua
@@ -28,11 +28,13 @@ local function validAction(self, bone, cmd)
 end
 ]]
 
+-- Freezing, Pos, and Angle --
+
 __e2setcost(20)
 
-e2function void bone:boneFreeze(num)
+e2function void bone:boneFreeze(isFrozen)
 	if not boneVerify(self, this) then return end
-	this:EnableMotion( num == 0 )
+	this:EnableMotion( isFrozen == 0 )
 	this:Wake()
 end
 	
@@ -46,17 +48,19 @@ e2function void bone:setAng(angle rot)
 	WireLib.setAng( this, rot )
 end
 
-e2function void entity:ragdollFreeze(num)
+e2function void entity:ragdollFreeze(isFrozen)
 	if not IsValid(this) then return {} end
 	local maxn = this:GetPhysicsObjectCount()-1
 	
 	for i = 0,maxn do
 		bone = getBone(this, i)
 		
-		bone:EnableMotion( num == 0 )
+		bone:EnableMotion( isFrozen == 0 )
 		bone:Wake()
 	end
 end
+
+-- Set Miscellaneous --
 
 __e2setcost(10)
 
@@ -68,13 +72,50 @@ end
 e2function void bone:setInertia( vector inertia )
 	if not boneVerify(self, this) then return end
 	if Vector( inertia[1], inertia[2], inertia[3] ):IsZero() then return end
-	if IsValid( phys ) then
-		this:SetInertia(Vector(inertia[1], inertia[2], inertia[3]))
-	end
+	this:SetInertia(Vector(inertia[1], inertia[2], inertia[3]))
 end
 
 e2function void bone:setBuoyancy(number buoyancy)
 	if not boneVerify(self, this) then return end
 	this:SetBuoyancyRatio( math.Clamp(buoyancy, 0, 1) )
+end
+
+e2function void bone:setPhysicalMaterial(string material)
+	if not boneVerify(self, this) then return end
+	this:SetMaterial(material)
+end
+
+-- Set Velocity --
+
+e2function void bone:setVelocity(vector velocity)
+	ent = boneVerify(self, this)
+	if not ent then return end
+	ent:PhysWake()
+	this:SetVelocity(Vector(velocity[1], velocity[2], velocity[3]))
+	this:Wake()
+end
+
+e2function void bone:setVelocityInstant(vector velocity)
+	ent = boneVerify(self, this)
+	if not ent then return end
+	this:SetVelocityInstantaneous(Vector(velocity[1], velocity[2], velocity[3]))
+	ent:PhysWake()
+	this:Wake()
+end
+
+e2function void bone:setAngVelocity(vector velocity)
+	ent = boneVerify(self, this)
+	if not ent then return end
+	this:SetAngleVelocity(Vector(velocity[1], velocity[2], velocity[3]))
+	ent:PhysWake()
+	this:Wake()
+end
+
+e2function void bone:setAngVelocityInstant(vector velocity)
+	ent = boneVerify(self, this)
+	if not ent then return end
+	this:SetAngleVelocityInstantaneous(Vector(velocity[1], velocity[2], velocity[3]))
+	ent:PhysWake()
+	this:Wake()
 end
 
