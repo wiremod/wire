eb7d2e907032148ad9a29750f2f4f43e67ee2549
Vurv <56230599+Vurv78@users.noreply.github.com>
2023-07-09 03:04:16 -0700

    fix: error with using incorrect e2function vararg type

--- lua/entities/gmod_wire_expression2/core/string.lua
+++ lua/entities/gmod_wire_expression2/core/string.lua
@@ -393,7 +393,7 @@ end
 __e2setcost(3)
 
 --- Formats a values exactly like Lua's [http://www.lua.org/manual/5.1/manual.html#pdf-string.format string.format]. Any number and type of parameter can be passed through the "...". Prints errors to the chat area.
-e2function string format(string fmt, ...args)
+e2function string format(string fmt, ...)
 	self.prf = self.prf + select("#", ...) * 2
 
 	-- TODO: call toString for table-based types
