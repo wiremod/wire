9904f124c4247b49270fefb2ad0288a2d7906524
Vurv <56230599+Vurv78@users.noreply.github.com>
2023-07-27 10:36:21 -0700

    Change perm checks and add weapon/ammo checks
    Added check to stripWeapon to throw a runtime error in case the weapon class is invalid.
    
    Changed logic for permissions to AND, so you could be either a friend or have cppi perms rather than needing both to access the functions.
    
    Added check to the ammo functions to ensure the ammo type is valid, or else throw a runtime error
    
    self.entity.player -> self.player
    
    Changed invalid weapon error message for giveWeapon functions
    
    Fix checkGive using incorrect variable (this) instead of (target)

--- lua/entities/gmod_wire_expression2/core/weapon.lua
+++ lua/entities/gmod_wire_expression2/core/weapon.lua
@@ -95,7 +95,7 @@ e2function number entity:setClip1(amount)
 	if not setAmmoCVar:GetBool() then return self:throw("The server has disabled setting ammo", 0) end
 	if not IsValid(this) then return self:throw("Invalid entity!", 0) end
 	if not this:IsWeapon() then return self:throw("Expected a Weapon but got Entity", 0) end
-	if not isFriend(self.entity.player, this) or not CanTool(self.entity.player, this, "wire_expression2") then return self:throw("You cannot target this weapon", 0) end
+	if not isFriend(self.player, this) and not CanTool(self.player, this, "wire_expression2") then return self:throw("You cannot target this weapon", 0) end
 
 	return this:SetClip1(amount)
 end
@@ -120,7 +120,7 @@ e2function number entity:setClip2(amount)
 	if not setAmmoCVar:GetBool() then return self:throw("The server has disabled setting ammo", 0) end
 	if not IsValid(this) then return self:throw("Invalid entity!", 0) end
 	if not this:IsWeapon() then return self:throw("Expected a Weapon but got Entity", 0) end
-	if not isFriend(self.entity.player, this) or not CanTool(self.entity.player, this, "wire_expression2") then return self:throw("You cannot target this weapon", 0) end
+	if not isFriend(self.player, this) and not CanTool(self.player, this, "wire_expression2") then return self:throw("You cannot target this weapon", 0) end
 
 	return this:SetClip2(amount)
 end
@@ -141,8 +141,8 @@ local function checkGive(self, target, classname)
 	if not giveWeaponCVar:GetBool() then return self:throw("The server has disabled giving weapons", false) end
 	if not IsValid(target) then return self:throw("Invalid entity!", false) end
 	if not target:IsPlayer() then return self:throw("Expected a Player but got Entity", false) end
-	if not isFriend(self.entity.player, this) or not CanTool(self.entity.player, target, "wire_expression2") then return self:throw("You cannot target this player", false) end
-	if not list.HasEntry("Weapon", classname) then return self:throw("Expected a weapon class, but the weapon was not found", false) end
+	if not isFriend(self.player, target) and not CanTool(self.player, target, "wire_expression2") then return self:throw("You cannot target this player", false) end
+	if not list.HasEntry("Weapon", classname) then return self:throw("Invalid weapon class '" .. classname .. "'", false) end
 	if hook.Run("PlayerGiveSWEP", target, classname, list.Get("Weapon")[classname]) == false then
 		return self:throw("The server blocked the weapon from being given", false)
 	end
@@ -165,7 +165,8 @@ end
 e2function void entity:selectWeapon(string classname)
 	if not IsValid(this) then return self:throw("Invalid entity!", nil) end
 	if not this:IsPlayer() then return self:throw("Expected a Player but got Entity", nil) end
-	if not isFriend(self.entity.player, this) or not CanTool(self.entity.player, this, "wire_expression2") then return self:throw("You cannot target this player", nil) end
+	if not isFriend(self.player, this) and not CanTool(self.player, this, "wire_expression2") then return self:throw("You cannot target this player", nil) end
+	if not list.HasEntry("Weapon", classname) then return self:throw("Invalid weapon class '" .. classname .. "'", nil) end
 
 	this:SelectWeapon(classname)
 end
@@ -174,7 +175,8 @@ e2function number entity:giveAmmo(amount, string type)
 	if not giveAmmoCVar:GetBool() then return self:throw("The server has disabled giving ammo", 0) end
 	if not IsValid(this) then return self:throw("Invalid entity!", 0) end
 	if not this:IsPlayer() then return self:throw("Expected a Player but got Entity", 0) end
-	if not isFriend(self.entity.player, this) or not CanTool(self.entity.player, this, "wire_expression2") then return self:throw("You cannot target this player", 0) end
+	if not isFriend(self.player, this) and not CanTool(self.player, this, "wire_expression2") then return self:throw("You cannot target this player", 0) end
+	if not table.HasValue(game.GetAmmoTypes(), type) then return self:throw("Invalid ammo type: '" .. type .. "'", -1) end
 
 	return this:GiveAmmo(amount, type)
 end
@@ -183,7 +185,8 @@ e2function number entity:giveAmmo(amount, string type, hidePopUp)
 	if not giveAmmoCVar:GetBool() then return self:throw("The server has disabled giving ammo", 0) end
 	if not IsValid(this) then return self:throw("Invalid entity!", 0) end
 	if not this:IsPlayer() then return self:throw("Expected a Player but got Entity", 0) end
-	if not isFriend(self.entity.player, this) or not CanTool(self.entity.player, this, "wire_expression2") then return self:throw("You cannot target this player", 0) end
+	if not isFriend(self.player, this) and not CanTool(self.player, this, "wire_expression2") then return self:throw("You cannot target this player", 0) end
+	if not table.HasValue(game.GetAmmoTypes(), type) then return self:throw("Invalid ammo type: '" .. type .. "'", -1) end
 
 	return this:GiveAmmo(amount, type, hidePopUp ~= 0)
 end
@@ -192,7 +195,8 @@ e2function void entity:setAmmo(ammoCount, string type)
 	if not setAmmoCVar:GetBool() then return self:throw("The server has disabled setting ammo", 0) end
 	if not IsValid(this) then return self:throw("Invalid entity!", nil) end
 	if not this:IsPlayer() then return self:throw("Expected a Player but got Entity", nil) end
-	if not isFriend(self.entity.player, this) or not CanTool(self.entity.player, this, "wire_expression2") then return self:throw("You cannot target this player", nil) end
+	if not isFriend(self.player, this) and not CanTool(self.player, this, "wire_expression2") then return self:throw("You cannot target this player", nil) end
+	if not table.HasValue(game.GetAmmoTypes(), type) then return self:throw("Invalid ammo type: '" .. type .. "'", nil) end
 
 	this:SetAmmo(ammoCount, type)
 end
@@ -201,7 +205,8 @@ e2function void entity:removeAmmo(ammoCount, string type)
 	if not setAmmoCVar:GetBool() then return self:throw("The server has disabled setting ammo", nil) end
 	if not IsValid(this) then return self:throw("Invalid entity!", nil) end
 	if not this:IsPlayer() then return self:throw("Expected a Player but got Entity", nil) end
-	if not isFriend(self.entity.player, this) or not CanTool(self.entity.player, this, "wire_expression2") then return self:throw("You cannot target this player", nil) end
+	if not isFriend(self.player, this) and not CanTool(self.player, this, "wire_expression2") then return self:throw("You cannot target this player", nil) end
+	if not table.HasValue(game.GetAmmoTypes(), type) then return self:throw("Invalid ammo type: '" .. type .. "'", nil) end
 
 	this:RemoveAmmo(ammoCount, type)
 end
@@ -210,7 +215,7 @@ e2function void entity:removeAllAmmo()
 	if not setAmmoCVar:GetBool() then return self:throw("The server has disabled setting ammo", nil) end
 	if not IsValid(this) then return self:throw("Invalid entity!", nil) end
 	if not this:IsPlayer() then return self:throw("Expected a Player but got Entity", nil) end
-	if not isFriend(self.entity.player, this) or not CanTool(self.entity.player, this, "wire_expression2") then return self:throw("You cannot target this player", nil) end
+	if not isFriend(self.player, this) and not CanTool(self.player, this, "wire_expression2") then return self:throw("You cannot target this player", nil) end
 
 	this:RemoveAllAmmo()
 end
@@ -219,7 +224,8 @@ e2function void entity:stripWeapon(string classname)
 	if not stripWeaponCVar:GetBool() then return self:throw("The server has disabled stripping weapons", nil) end
 	if not IsValid(this) then return self:throw("Invalid entity!", nil) end
 	if not this:IsPlayer() then return self:throw("Expected a Player but got Entity", nil) end
-	if not isFriend(self.entity.player, this) or not CanTool(self.entity.player, this, "wire_expression2") then return self:throw("You cannot target this player", nil) end
+	if not isFriend(self.player, this) and not CanTool(self.player, this, "wire_expression2") then return self:throw("You cannot target this player", nil) end
+	if not list.HasEntry("Weapon", classname) then return self:throw("Invalid weapon class '" .. classname .. "'", nil) end
 
 	this:StripWeapon(classname)
 end
@@ -228,7 +234,7 @@ e2function void entity:stripWeapons()
 	if not stripWeaponCVar:GetBool() then return self:throw("The server has disabled stripping weapons", nil) end
 	if not IsValid(this) then return self:throw("Invalid entity!", nil) end
 	if not this:IsPlayer() then return self:throw("Expected a Player but got Entity", nil) end
-	if not isFriend(self.entity.player, this) or not CanTool(self.entity.player, this, "wire_expression2") then return self:throw("You cannot target this player", nil) end
+	if not isFriend(self.player, this) and not CanTool(self.player, this, "wire_expression2") then return self:throw("You cannot target this player", nil) end
 
 	this:StripWeapons()
 end
