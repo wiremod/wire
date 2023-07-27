e92d48938bd65b3cc94f20677201d05cf3e25034
Aske <aske020304@gmail.com>
2023-07-27 19:52:29 +0200

    Extend weapon functionality (#2683)
    * Add new functions and events to e2 weapons
    
    * Added permission checks and cvars
    
    * Add convar requiored to E2helper and check isFriend
    
    * Fixed lint
    
    * Change perm checks and add weapon/ammo checks
    
    Added check to stripWeapon to throw a runtime error in case the weapon class is invalid.
    
    Changed logic for permissions to AND, so you could be either a friend or have cppi perms rather than needing both to access the functions.
    
    Added check to the ammo functions to ensure the ammo type is valid, or else throw a runtime error
    
    self.entity.player -> self.player
    
    Changed invalid weapon error message for giveWeapon functions
    
    Fix checkGive using incorrect variable (this) instead of (target)
    
    ---------
    
    Co-authored-by: Vurv <56230599+Vurv78@users.noreply.github.com>

--- lua/entities/gmod_wire_expression2/core/weapon.lua
+++ lua/entities/gmod_wire_expression2/core/weapon.lua
@@ -1,9 +1,18 @@
-/******************************************************************************\
+--[[----------------------------------------------------------------------------
   Player-weapon support
-\******************************************************************************/
+------------------------------------------------------------------------------]]
+
+local CanTool = WireLib.CanTool
+local isFriend = E2Lib.isFriend
+
+local setAmmoCVar = CreateConVar("wire_expression2_weapon_ammo_set_enable", 0, FCVAR_ARCHIVE, "Whether or not to allow E2s to set ammo for weapons and players")
+local giveAmmoCVar = CreateConVar("wire_expression2_weapon_ammo_give_enable", 0, FCVAR_ARCHIVE, "Whether or not to allow E2s to give ammo to players")
+local giveWeaponCVar = CreateConVar("wire_expression2_weapon_give_enable", 0, FCVAR_ARCHIVE, "Whether or not to allow E2s to give weapons to players")
+local stripWeaponCVar = CreateConVar("wire_expression2_weapon_strip_enable", 0, FCVAR_ARCHIVE, "Whether or not to allow E2s to strip weapons from players")
 
 __e2setcost(2) -- temporary
 
+[nodiscard]
 e2function entity entity:weapon()
 	if not IsValid(this) then return nil end
 	if not this:IsPlayer() and not this:IsNPC() then return nil end
@@ -11,7 +20,7 @@ e2function entity entity:weapon()
 	return this:GetActiveWeapon()
 end
 
-
+[nodiscard]
 e2function entity entity:weapon(string weaponclassname)
 	if not IsValid(this) then return nil end
 	if not this:IsPlayer() and not this:IsNPC() then return nil end
@@ -19,6 +28,15 @@ e2function entity entity:weapon(string weaponclassname)
 	return this:GetWeapon(weaponclassname)
 end
 
+[nodiscard]
+e2function number entity:hasWeapon(string classname)
+	if not IsValid(this) then return self:throw("Invalid entity!", 0) end
+	if not this:IsPlayer() then return self:throw("Expected a Player but got Entity", 0) end
+
+	return this:HasWeapon(classname) and 1 or 0
+end
+
+[nodiscard]
 e2function array entity:weapons()
 	if not IsValid(this) then return {} end
 	if not this:IsPlayer() then return {} end
@@ -29,6 +47,7 @@ e2function array entity:weapons()
 	return ret
 end
 
+[nodiscard]
 e2function string entity:primaryAmmoType()
 	if not IsValid(this) then return "" end
 	if not this:IsWeapon() then return "" end
@@ -38,6 +57,7 @@ e2function string entity:primaryAmmoType()
 	return game.GetAmmoName(ammoId) or ""
 end
 
+[nodiscard]
 e2function string entity:secondaryAmmoType()
 	if not IsValid(this) then return "" end
 	if not this:IsWeapon() then return "" end
@@ -47,6 +67,7 @@ e2function string entity:secondaryAmmoType()
 	return game.GetAmmoName(ammoId) or ""
 end
 
+[nodiscard]
 e2function number entity:ammoCount(string ammo_type)
 	if not IsValid(this) then return 0 end
 	if not this:IsPlayer() then return 0 end
@@ -54,6 +75,7 @@ e2function number entity:ammoCount(string ammo_type)
 	return this:GetAmmoCount(ammo_type)
 end
 
+[nodiscard]
 e2function number entity:clip1()
 	if not IsValid(this) then return 0 end
 	if not this:IsWeapon() then return 0 end
@@ -61,6 +83,24 @@ e2function number entity:clip1()
 	return this:Clip1()
 end
 
+[nodiscard]
+e2function number entity:clip1Size()
+	if not IsValid(this) then return self:throw("Invalid entity!", 0) end
+	if not this:IsWeapon() then return self:throw("Expected a Weapon but got Entity", 0) end
+
+	return this:GetMaxClip1()
+end
+
+e2function number entity:setClip1(amount)
+	if not setAmmoCVar:GetBool() then return self:throw("The server has disabled setting ammo", 0) end
+	if not IsValid(this) then return self:throw("Invalid entity!", 0) end
+	if not this:IsWeapon() then return self:throw("Expected a Weapon but got Entity", 0) end
+	if not isFriend(self.player, this) and not CanTool(self.player, this, "wire_expression2") then return self:throw("You cannot target this weapon", 0) end
+
+	return this:SetClip1(amount)
+end
+
+[nodiscard]
 e2function number entity:clip2()
 	if not IsValid(this) then return 0 end
 	if not this:IsWeapon() then return 0 end
@@ -68,6 +108,24 @@ e2function number entity:clip2()
 	return this:Clip2()
 end
 
+[nodiscard]
+e2function number entity:clip2Size()
+	if not IsValid(this) then return self:throw("Invalid entity!", 0) end
+	if not this:IsWeapon() then return self:throw("Expected a Weapon but got Entity", 0) end
+
+	return this:GetMaxClip2()
+end
+
+e2function number entity:setClip2(amount)
+	if not setAmmoCVar:GetBool() then return self:throw("The server has disabled setting ammo", 0) end
+	if not IsValid(this) then return self:throw("Invalid entity!", 0) end
+	if not this:IsWeapon() then return self:throw("Expected a Weapon but got Entity", 0) end
+	if not isFriend(self.player, this) and not CanTool(self.player, this, "wire_expression2") then return self:throw("You cannot target this weapon", 0) end
+
+	return this:SetClip2(amount)
+end
+
+[nodiscard]
 e2function string entity:tool()
 	if not IsValid(this) then return "" end
 	if not this:IsPlayer() then return "" end
@@ -78,3 +136,124 @@ e2function string entity:tool()
 
 	return weapon.Mode
 end
+
+local function checkGive(self, target, classname)
+	if not giveWeaponCVar:GetBool() then return self:throw("The server has disabled giving weapons", false) end
+	if not IsValid(target) then return self:throw("Invalid entity!", false) end
+	if not target:IsPlayer() then return self:throw("Expected a Player but got Entity", false) end
+	if not isFriend(self.player, target) and not CanTool(self.player, target, "wire_expression2") then return self:throw("You cannot target this player", false) end
+	if not list.HasEntry("Weapon", classname) then return self:throw("Invalid weapon class '" .. classname .. "'", false) end
+	if hook.Run("PlayerGiveSWEP", target, classname, list.Get("Weapon")[classname]) == false then
+		return self:throw("The server blocked the weapon from being given", false)
+	end
+
+	return true
+end
+
+e2function entity entity:giveWeapon(string classname)
+	if not checkGive(self, this, classname) then return NULL end
+
+	return this:Give(classname)
+end
+
+e2function entity entity:giveWeapon(string classname, noAmmo)
+	if not checkGive(self, this, classname) then return NULL end
+
+	return this:Give(classname, noAmmo ~= 0)
+end
+
+e2function void entity:selectWeapon(string classname)
+	if not IsValid(this) then return self:throw("Invalid entity!", nil) end
+	if not this:IsPlayer() then return self:throw("Expected a Player but got Entity", nil) end
+	if not isFriend(self.player, this) and not CanTool(self.player, this, "wire_expression2") then return self:throw("You cannot target this player", nil) end
+	if not list.HasEntry("Weapon", classname) then return self:throw("Invalid weapon class '" .. classname .. "'", nil) end
+
+	this:SelectWeapon(classname)
+end
+
+e2function number entity:giveAmmo(amount, string type)
+	if not giveAmmoCVar:GetBool() then return self:throw("The server has disabled giving ammo", 0) end
+	if not IsValid(this) then return self:throw("Invalid entity!", 0) end
+	if not this:IsPlayer() then return self:throw("Expected a Player but got Entity", 0) end
+	if not isFriend(self.player, this) and not CanTool(self.player, this, "wire_expression2") then return self:throw("You cannot target this player", 0) end
+	if not table.HasValue(game.GetAmmoTypes(), type) then return self:throw("Invalid ammo type: '" .. type .. "'", -1) end
+
+	return this:GiveAmmo(amount, type)
+end
+
+e2function number entity:giveAmmo(amount, string type, hidePopUp)
+	if not giveAmmoCVar:GetBool() then return self:throw("The server has disabled giving ammo", 0) end
+	if not IsValid(this) then return self:throw("Invalid entity!", 0) end
+	if not this:IsPlayer() then return self:throw("Expected a Player but got Entity", 0) end
+	if not isFriend(self.player, this) and not CanTool(self.player, this, "wire_expression2") then return self:throw("You cannot target this player", 0) end
+	if not table.HasValue(game.GetAmmoTypes(), type) then return self:throw("Invalid ammo type: '" .. type .. "'", -1) end
+
+	return this:GiveAmmo(amount, type, hidePopUp ~= 0)
+end
+
+e2function void entity:setAmmo(ammoCount, string type)
+	if not setAmmoCVar:GetBool() then return self:throw("The server has disabled setting ammo", 0) end
+	if not IsValid(this) then return self:throw("Invalid entity!", nil) end
+	if not this:IsPlayer() then return self:throw("Expected a Player but got Entity", nil) end
+	if not isFriend(self.player, this) and not CanTool(self.player, this, "wire_expression2") then return self:throw("You cannot target this player", nil) end
+	if not table.HasValue(game.GetAmmoTypes(), type) then return self:throw("Invalid ammo type: '" .. type .. "'", nil) end
+
+	this:SetAmmo(ammoCount, type)
+end
+
+e2function void entity:removeAmmo(ammoCount, string type)
+	if not setAmmoCVar:GetBool() then return self:throw("The server has disabled setting ammo", nil) end
+	if not IsValid(this) then return self:throw("Invalid entity!", nil) end
+	if not this:IsPlayer() then return self:throw("Expected a Player but got Entity", nil) end
+	if not isFriend(self.player, this) and not CanTool(self.player, this, "wire_expression2") then return self:throw("You cannot target this player", nil) end
+	if not table.HasValue(game.GetAmmoTypes(), type) then return self:throw("Invalid ammo type: '" .. type .. "'", nil) end
+
+	this:RemoveAmmo(ammoCount, type)
+end
+
+e2function void entity:removeAllAmmo()
+	if not setAmmoCVar:GetBool() then return self:throw("The server has disabled setting ammo", nil) end
+	if not IsValid(this) then return self:throw("Invalid entity!", nil) end
+	if not this:IsPlayer() then return self:throw("Expected a Player but got Entity", nil) end
+	if not isFriend(self.player, this) and not CanTool(self.player, this, "wire_expression2") then return self:throw("You cannot target this player", nil) end
+
+	this:RemoveAllAmmo()
+end
+
+e2function void entity:stripWeapon(string classname)
+	if not stripWeaponCVar:GetBool() then return self:throw("The server has disabled stripping weapons", nil) end
+	if not IsValid(this) then return self:throw("Invalid entity!", nil) end
+	if not this:IsPlayer() then return self:throw("Expected a Player but got Entity", nil) end
+	if not isFriend(self.player, this) and not CanTool(self.player, this, "wire_expression2") then return self:throw("You cannot target this player", nil) end
+	if not list.HasEntry("Weapon", classname) then return self:throw("Invalid weapon class '" .. classname .. "'", nil) end
+
+	this:StripWeapon(classname)
+end
+
+e2function void entity:stripWeapons()
+	if not stripWeaponCVar:GetBool() then return self:throw("The server has disabled stripping weapons", nil) end
+	if not IsValid(this) then return self:throw("Invalid entity!", nil) end
+	if not this:IsPlayer() then return self:throw("Expected a Player but got Entity", nil) end
+	if not isFriend(self.player, this) and not CanTool(self.player, this, "wire_expression2") then return self:throw("You cannot target this player", nil) end
+
+	this:StripWeapons()
+end
+
+E2Lib.registerEvent("weaponPickup", {
+	{ "Weapon", "e" },
+	{ "Owner", "e" }
+})
+
+hook.Add("WeaponEquip", "E2_weaponPickup", function(weapon, owner)
+	E2Lib.triggerEvent("weaponPickup", { weapon, owner })
+end)
+
+E2Lib.registerEvent("weaponSwitched", {
+	{ "Player", "e" },
+	{ "OldWeapon", "e" },
+	{ "NewWeapon", "e" }
+})
+
+hook.Add("PlayerSwitchWeapon", "E2_weaponSwitched", function(player, oldWeapon, newWeapon)
+	E2Lib.triggerEvent("weaponSwitched", { player, oldWeapon, newWeapon })
+end)
