83b57a33f62425e6d8c2de6f2a7bf39a8c80ce44
Vurv <56230599+Vurv78@users.noreply.github.com>
2023-09-20 10:51:53 -0700

    Deprecate findBy and findIn functions

--- lua/entities/gmod_wire_expression2/core/find.lua
+++ lua/entities/gmod_wire_expression2/core/find.lua
@@ -1,4 +1,4 @@
-E2Lib.RegisterExtension("find", true, "Allows an E2 to search for entities matching a filter.")
+E2Lib.RegisterExtension("find", true, "Allows an E2 to search for entities matching a filter. Deprecated in favor of the findquery extension")
 
 local function table_IsEmpty(t) return not next(t) end
 
@@ -381,6 +381,7 @@ end
 __e2setcost(30)
 
 --- Finds entities in a sphere around V with a radius of N, returns the number found after filtering
+[deprecated = "Use findQuery():inSphere(vn) instead"]
 e2function number findInSphere(vector center, radius)
 	if query_blocked(self, 1) then return 0 end
 	center = Vector(center[1], center[2], center[3])
@@ -389,6 +390,7 @@ e2function number findInSphere(vector center, radius)
 end
 
 --- Like findInSphere but with a [[http://mathworld.wolfram.com/SphericalCone.html Spherical cone]], arguments are for position, direction, length, and degrees (works now)
+[deprecated = "Use findQuery():inCone(vvnn) instead"]
 e2function number findInCone(vector position, vector direction, length, degrees)
 	if query_blocked(self, 4) then return 0 end
 
@@ -414,6 +416,7 @@ e2function number findInCone(vector position, vector direction, length, degrees)
 end
 
 --- Like findInSphere but with a globally aligned box, the arguments are the diagonal corners of the box
+[deprecated = "Use findQuery():inBox(vv) instead"]
 e2function number findInBox(vector min, vector max)
 	if query_blocked(self, 1) then return 0 end
 	min = Vector(min[1], min[2], min[3])
@@ -428,12 +431,14 @@ e2function number findByName(string name)
 end
 
 --- Find all entities with the given model
+[deprecated = "Use findQuery():withModel(s) instead"]
 e2function number findByModel(string model)
 	if query_blocked(self, 1) then return 0 end
 	return applyFindList(self, ents.FindByModel(model))
 end
 
 --- Find all entities with the given class
+[deprecated = "Use findQuery():withClass(s) instead"]
 e2function number findByClass(string class)
 	if query_blocked(self, 1) then return 0 end
 	return applyFindList(self, ents.FindByClass(class))
