be75c7df2cc392331e4379da4ed58528ea0959a0
Vurv <56230599+Vurv78@users.noreply.github.com>
2023-10-30 22:42:24 -0700

    Temporary friendslist patch

--- lua/wire/stools/friendslist.lua
+++ lua/wire/stools/friendslist.lua
@@ -40,10 +40,16 @@ local function netWriteValues( values )
 	end
 end
 local function netReadValues()
-	local t = {}
-	local amount = net.ReadUInt(11)
-	for i=1,amount do
-		t[i] = net.ReadString()
+	local t, bytes = {}, 0
+	for i = 1, net.ReadUInt(11) do
+		local str = net.ReadString() -- todo: delete and redo practically this entire file cause this is horrible. we should just be sending steamid64s as numbers. theres writeuint64 and readuint64 now which would be perfect for this.
+		bytes = bytes + #str
+
+		t[i] = string.sub(str, 1, 32)
+
+		if bytes > 32 * 2 ^ 11 --[[ someone is writing more than they are supposed to be. ]] then
+			return t
+		end
 	end
 	return t
 end
