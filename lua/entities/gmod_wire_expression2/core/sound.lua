c27b3b2f6a415a762c754eb70dae17a3db28227b
Titouan Guionneau <titouan.guionneau@epita.fr>
2023-12-23 15:38:01 +0100

    Reduced size of E2_EmitSound_stop_all table

--- lua/entities/gmod_wire_expression2/core/sound.lua
+++ lua/entities/gmod_wire_expression2/core/sound.lua
@@ -304,14 +304,16 @@ local function EmitSound(e2, ent, snd, level, pitch, volume)
     end
 
     local emitting_sounds = ent:GetVar("E2_emitting_sounds", {})
-    table.insert(emitting_sounds, snd)
+    emitting_sounds[snd] = (emitting_sounds[snd] or 0) + 1
     ent:SetVar("E2_emitting_sounds", emitting_sounds)
 
     ent:CallOnRemove("E2_EmitSound_stop_all", function()
         local emitting_sounds = ent:GetVar("E2_emitting_sounds")
         if not emitting_sounds then return end
-        for _, snd in ipairs(emitting_sounds) do
-            ent:StopSound(snd)
+        for snd, count in pairs(emitting_sounds) do
+            for i = 1, count do
+                ent:StopSound(snd)
+            end
         end
     end)
 
