26f2b8a4b528ca3d9138baf3c81bc00e9ecb01b2
DerelictDrone <gage@echelonfit.com>
2023-12-27 20:31:12 -0600

    Prevent an error on disabling cd lock

--- lua/entities/gmod_wire_cd_lock.lua
+++ lua/entities/gmod_wire_cd_lock.lua
@@ -97,7 +97,9 @@ function ENT:AttachDisk(disk)
 
 	self.Const:CallOnRemove("wire_cd_remove_on_weld",function()
 		self.Const = nil
-		self.Disk.Lock = nil
+		if(IsValid(self.Disk)) then
+			self.Disk.Lock = nil
+		end
 		self.Disk = nil
 		self.NoCollideConst = nil
 
