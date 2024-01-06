94ded385b18f40fd237b79c52871faf60aa7d786
stpM64 <sssstepa452@yandex.ru>
2024-01-06 20:34:31 +0300

    Voice transmission optimization

--- lua/entities/gmod_wire_adv_microphone.lua
+++ lua/entities/gmod_wire_adv_microphone.lua
@@ -46,6 +46,26 @@ function ENT:Initialize()
     self:OnActiveChanged(nil, nil, self:GetActive())
 end
 
+local function PlayerCanHearPlayersVoice_Hook(listener, talker)
+    local talkerPos = talker:GetPos()
+    local listenerPos = listener:GetPos()
+
+    -- Note: any given speaker can only be connected to one microphone,
+    -- so this loops can be considered O(nMic), not O(nMic*nSpeaker)
+    for _, mic in ipairs(LiveMics) do
+        if mic:GetPos():DistToSqr(talkerPos) > PLAYER_VOICE_MAXDIST_SQR then goto mic_next end
+
+        for speaker in pairs(mic._activeSpeakers) do
+            if IsValid(speaker) and
+                speaker:GetPos():DistToSqr(listenerPos) <= PLAYER_VOICE_MAXDIST_SQR
+            then
+                return true, false -- Can hear, not in 3D
+            end
+        end
+        ::mic_next::
+    end
+end
+
 local function Mic_SetLive(self, isLive)
     if not IsValid(self) then
         isLive = false
@@ -55,10 +75,18 @@ local function Mic_SetLive(self, isLive)
     end
 
     if isLive then
+        if LiveMics[1] == nil then
+            hook.Add("PlayerCanHearPlayersVoice", "Wire.AdvMicrophone", PlayerCanHearPlayersVoice_Hook)
+        end 
+
         if not table.HasValue(LiveMics, self) then
             table.insert(LiveMics, self)
         end
     else
+        if LiveMics[1] ~= nil and LiveMics[2] == nil then
+            hook.Remove("PlayerCanHearPlayersVoice", "Wire.AdvMicrophone")
+        end
+
         table.RemoveByValue(LiveMics, self)
     end
 end
@@ -193,26 +221,6 @@ function ENT:ReproduceSound(snd, vol, pitch, dsp, emittype)
         speaker:ReproduceSound(snd, vol, pitch, dsp, emittype)
     end
 end
-
-hook.Add("PlayerCanHearPlayersVoice", "Wire.AdvMicrophone", function(listener, talker)
-    local talkerPos = talker:GetPos()
-    local listenerPos = listener:GetPos()
-
-    -- Note: any given speaker can only be connected to one microphone,
-    -- so this loops can be considered O(nMic), not O(nMic*nSpeaker)
-    for _, mic in ipairs(LiveMics) do
-        if mic:GetPos():DistToSqr(talkerPos) > PLAYER_VOICE_MAXDIST_SQR then goto mic_next end
-
-        for speaker in pairs(mic._activeSpeakers) do
-            if IsValid(speaker) and
-                speaker:GetPos():DistToSqr(listenerPos) <= PLAYER_VOICE_MAXDIST_SQR
-            then
-                return true, false -- Can hear, not in 3D
-            end
-        end
-        ::mic_next::
-    end
-end)
 
 -- TODO: hook into sound.PlayFile
 -- TODO: hook into sound.PlayURL
