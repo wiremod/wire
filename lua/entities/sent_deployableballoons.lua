93cd4f826bc46ab1b1f6f18b14a9a75e2d3dae53
thegrb93 <grbrown93@sbcglobal.net>
2024-02-12 15:17:30 -0500

    Clamp force. Fixes: #2994 (#3000)

--- lua/entities/sent_deployableballoons.lua
+++ lua/entities/sent_deployableballoons.lua
@@ -119,7 +119,7 @@ function ENT:TriggerInput(key,value)
 			Wire_TriggerOutput(self, "Deployed", self.Deployed)
 		end
 	elseif (key == "Force") then
-		self.force = value
+		self.force = math.Clamp(value, -1000000, 1000000)
 		if self.Deployed ~= 0 then
 			self.Balloon:SetForce(value)
 		end
