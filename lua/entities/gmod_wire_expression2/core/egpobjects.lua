793f3ac342d7957165887d4defd2586fa524ac42
Denneisk <20892685+Denneisk@users.noreply.github.com>
2024-04-05 11:33:44 -0400

    Fix egp draw again

--- lua/entities/gmod_wire_expression2/core/egpobjects.lua
+++ lua/entities/gmod_wire_expression2/core/egpobjects.lua
@@ -547,7 +547,10 @@ e2function void egpobject:draw()
 	this._nodraw = nil
 	if not EGP:IsAllowed(self, egp) then return end
 
-	local args = { unpack(this) }
+	local args = {}
+	for k, v in pairs(this) do
+		args[k] = v
+	end
 
 	if EGP:CreateObject(egp, this.ID, args) then
 		EGP:DoAction(egp, self, "SendObject", this)
