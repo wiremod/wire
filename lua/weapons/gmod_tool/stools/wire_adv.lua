679227deb9672208d43455c92aa7fbdd680ba116
Redox <mail@redox.to>
2024-05-21 16:43:27 +0200

    Fix linter warning

--- lua/weapons/gmod_tool/stools/wire_adv.lua
+++ lua/weapons/gmod_tool/stools/wire_adv.lua
@@ -971,7 +971,7 @@ elseif CLIENT then
 			end
 			return true
 		elseif name == "Selected" and self:GetStage() == 2 then
-			local inputs, outputs = self:GetPorts( ent )
+			local _, outputs = self:GetPorts( ent )
 			if not isTableEmpty(outputs) then return false end
 			if self:GetOwner():KeyDown( IN_WALK ) then -- Gray out the ones that won't be able to be wired to any input
 				for i=1,#outputs do
