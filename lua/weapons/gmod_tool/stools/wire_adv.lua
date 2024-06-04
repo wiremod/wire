7a81f713764dac671d83cff73ec04a52bf88e9c5
Redox <mail@redox.to>
2024-06-04 16:35:46 +0200

    Fix NULL error

--- lua/weapons/gmod_tool/stools/wire_adv.lua
+++ lua/weapons/gmod_tool/stools/wire_adv.lua
@@ -1272,7 +1272,7 @@ elseif CLIENT then
 
 					traceData.collisiongroup = LAST_SHARED_COLLISION_GROUP
 					local traceResult = util.TraceLine(traceData)
-					if WireLib.HasPorts(traceResult.Entity) then
+					if IsValid(traceResult.Entity) and WireLib.HasPorts(traceResult.Entity) then
 						self:UpdateTraceForSurface(traceResult, traceResult.Entity:GetParent())
 					end
 					render.AddBeam(traceResult.HitPos, width, scroll+(traceResult.HitPos-start):Length()/10, Color(100,100,100,255))
