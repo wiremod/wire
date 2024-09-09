bd5d35c410b1f30adad9f2a3192f4f9654a3fd55
Zvbhrf <79792873+Zvbhrf@users.noreply.github.com>
2024-09-10 01:42:58 +0500

    Update lua/entities/gmod_wire_expression2/core/hologram.lua
    Remove local
    
    Co-authored-by: thegrb93 <grbrown93@sbcglobal.net>

--- lua/entities/gmod_wire_expression2/core/hologram.lua
+++ lua/entities/gmod_wire_expression2/core/hologram.lua
@@ -521,7 +521,6 @@ end
 
 -- Returns the hologram with the given index or nil if it doesn't exist.
 -- if shouldbenil is nil or false, assert that the hologram exists on @strict with an error. Otherwise, don't check (for holo creation, etc)
-local MAX_INDEX = 2 ^ 31
 local function CheckIndex(self, index, shouldbenil)
 	if index ~= index then return self:throw("holo index is NaN!", nil) end
 	index = math.Clamp(math.floor(index), -2^31, 2^31)
