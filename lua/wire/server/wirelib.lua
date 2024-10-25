f5cad247b4ab78e25ca8dfac8b8b0d306a53f3d5
thegrb93 <grbrown93@sbcglobal.net>
2024-10-25 07:31:11 -0500

    match is functionally identical except returns match instead of index+match

--- lua/wire/server/wirelib.lua
+++ lua/wire/server/wirelib.lua
@@ -1545,7 +1545,7 @@ function WireLib.SoundExists(path, ply)
 
 	-- Extract sound flags. See https://developer.valvesoftware.com/wiki/Soundscripts#Sound_characters
 	local flags, checkpath = string.match(path, "^([^%w_/%.]*)(.*)")
-	if #flags > 2 or string.find(flags, "[^#@<>%^%)}]") then
+	if #flags > 2 or string.match(flags, "[^#@<>%^%)}]") then
 		path = checkpath
 	end
 
