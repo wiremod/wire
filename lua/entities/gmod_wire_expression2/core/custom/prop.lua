01fa17cdf7b6548041762cb815505e19b09eaf97
Denneisk <20892685+Denneisk@users.noreply.github.com>
2024-11-17 18:47:24 +0000

    Fix setting angle was always relative (#3180)
    Added ragdollGetAng to reduce confusion with ragdollSetAng

--- lua/entities/gmod_wire_expression2/core/custom/prop.lua
+++ lua/entities/gmod_wire_expression2/core/custom/prop.lua
@@ -1145,6 +1145,15 @@ e2function void entity:ragdollFreeze(isFrozen)
 
 end
 
+__e2setcost(5)
+
+e2function angle entity:ragdollGetAng()
+	if not ValidAction(self, this) then return end
+
+	local phys = this:GetPhysicsObject()
+
+	return phys:IsValid() and phys:GetAngles() or self:throw("Tried to use entity without physics", Angle())
+
 __e2setcost(150)
 
 e2function void entity:ragdollSetPos(vector pos)
@@ -1160,8 +1169,9 @@ end
 e2function void entity:ragdollSetAng(angle rot)
 	if not ValidAction(self, this, "rot") then return end
 
+	local o = this:GetPhysicsObject():GetAngles()
 	for _, bone in pairs(GetBones(this)) do
-		setAng(bone, bone:AlignAngles(this:GetForward():Angle(), rot))
+		setAng(bone, bone:AlignAngles(o, rot))
 	end
 
 	this:PhysWake()
