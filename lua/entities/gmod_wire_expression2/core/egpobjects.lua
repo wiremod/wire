1fc4b7b564dab2b51ee20fdc93737866d060fedd
Denneisk <20892685+Denneisk@users.noreply.github.com>
2024-12-02 13:37:27 -0500

    Fix bad EGPObject invalid function

--- lua/entities/gmod_wire_expression2/core/egpobjects.lua
+++ lua/entities/gmod_wire_expression2/core/egpobjects.lua
@@ -9,6 +9,7 @@ local isValid = EGP.EGPObject.IsValid
 local hasObject = EGP.HasObject
 local egp_create = EGP.Create
 local isAllowed = EGP.IsAllowed
+local isEGPObject = EGP.IsEGPObject
 
 -- Table of allowed arguments and their types
 local EGP_ALLOWED_ARGS =
@@ -46,7 +47,7 @@ registerType("egpobject", "xeo", NULL_EGPOBJECT,
 	nil,
 	nil,
 	function(v)
-		return not isValid(v)
+		return not isEGPObject(v)
 	end
 )
 
