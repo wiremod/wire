656c2556a6f82b5fad9e7868f5af7871364775f5
Astralcircle <142503363+Astralcircle@users.noreply.github.com>
2024-12-28 22:05:04 +0300

    Laser Pointer refactoring (#3232)
    * Laser Pointer refactoring
    
    * Linter pass
    
    * Remove COLOSSAL_SANDBOX check

--- lua/weapons/laserpointer/init.lua
+++ lua/weapons/laserpointer/init.lua
@@ -1,6 +1,6 @@
 AddCSLuaFile("cl_init.lua")
 AddCSLuaFile("shared.lua")
-include('shared.lua')
+include("shared.lua")
 
 SWEP.Weight = 8
 SWEP.AutoSwitchTo = false
@@ -13,51 +13,61 @@ function SWEP:Initialize()
 	self.Pointing = false
 end
 
-function SWEP:Equip( newOwner )
+function SWEP:Equip(newOwner)
 	if IsValid(newOwner.LasReceiver) then
 		self.Receiver = newOwner.LasReceiver
 		newOwner.LasReceiver = nil
-		newOwner:PrintMessage( HUD_PRINTTALK, "Relinked Sucessfully" )
+		newOwner:PrintMessage(HUD_PRINTTALK, "Relinked Sucessfully")
 	end
 end
 
 function SWEP:PrimaryAttack()
 	self.Pointing = not self.Pointing
-	self.Weapon:SetNWBool("Active", self.Pointing)
+	self:SetLaserEnabled(self.Pointing)
+
 	if self.Pointing and IsValid(self.Receiver) then
-		Wire_TriggerOutput(self.Receiver,"Active",1)
+		Wire_TriggerOutput(self.Receiver,"Active", 1)
 	else
-		Wire_TriggerOutput(self.Receiver,"Active",0)
+		Wire_TriggerOutput(self.Receiver,"Active", 0)
 	end
 end
 
 function SWEP:SecondaryAttack()
-	local trace = self:GetOwner():GetEyeTrace()
+	local owner = self:GetOwner()
+	if not IsValid(owner) then return end
 
-	if IsValid(trace.Entity) and trace.Entity:GetClass() == "gmod_wire_las_receiver" and gamemode.Call("CanTool", self:GetOwner(), trace, "wire_las_receiver") then
+	local trace = owner:GetEyeTrace()
+
+	if IsValid(trace.Entity) and trace.Entity:GetClass() == "gmod_wire_las_receiver" and gamemode.Call("CanTool", owner, trace, "wire_las_receiver") then
 		self.Receiver = trace.Entity
-		self:GetOwner():PrintMessage( HUD_PRINTTALK, "Linked Sucessfully" )
+		owner:PrintMessage(HUD_PRINTTALK, "Linked Sucessfully")
+
 		return true
 	end
 end
 
 function SWEP:Think()
-	if(self.Pointing and self.Receiver and self.Receiver:IsValid())then
+	if self.Pointing and IsValid(self.Receiver) then
 		local owner = self:GetOwner()
+		if not IsValid(owner) then return end
+
 		local trace
+
 		if IsValid(owner) then
 			trace = owner:GetEyeTrace()
 		else
 			local att = self:GetAttachment(self:LookupAttachment("muzzle"))
-			trace = util.TraceLine({start = att.Pos, endpos = att.Pos+att.Ang:Forward()*16384, filter = self})
+			trace = util.TraceLine({ start = att.Pos, endpos = att.Pos + att.Ang:Forward() * 16384, filter = self })
 		end
+
 		local point = trace.HitPos
-		if (COLOSSAL_SANDBOX) then point = point * 6.25 end
+
 		Wire_TriggerOutput(self.Receiver, "X", point.x)
 		Wire_TriggerOutput(self.Receiver, "Y", point.y)
 		Wire_TriggerOutput(self.Receiver, "Z", point.z)
 		Wire_TriggerOutput(self.Receiver, "Pos", point)
 		Wire_TriggerOutput(self.Receiver, "RangerData", trace)
+
 		self.Receiver.VPos = point
 	end
 end
