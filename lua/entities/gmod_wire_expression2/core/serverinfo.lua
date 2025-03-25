625353ab06128b39f969f688f890b4e2ecd99585
Astralcircle <142503363+Astralcircle@users.noreply.github.com>
2025-03-25 13:19:43 +0300

    Make most of the `serverinfo` core constants

--- lua/entities/gmod_wire_expression2/core/serverinfo.lua
+++ lua/entities/gmod_wire_expression2/core/serverinfo.lua
@@ -2,15 +2,23 @@
   Server Information
 \******************************************************************************/
 
+E2Lib.registerConstant("DEDICATED", game.IsDedicated() and 1 or 0)
+E2Lib.registerConstant("GAMEMODE", gmod.GetGamemode().Name)
+E2Lib.registerConstant("MAP", game.GetMap())
+E2Lib.registerConstant("MAXPLAYERS", game.MaxPlayers())
+E2Lib.registerConstant("SERVERUUID", WireLib.GetServerUUID())
+E2Lib.registerConstant("SINGLEPLAYER", game.SinglePlayer() and 1 or 0)
+E2Lib.registerConstant("TICKINTERVAL", engine.TickInterval())
+
 __e2setcost(1)
 
+[deprecated = "Use the constant MAP instead"]
 e2function string map()
 	return game.GetMap()
 end
 
-local hostname = GetConVar("hostname")
 e2function string hostname()
-	return hostname:GetString()
+	return GetHostName()
 end
 
 e2function string hostip()
@@ -22,18 +30,22 @@ e2function number isLan()
 	return sv_lan:GetBool() and 1 or 0
 end
 
+[deprecated = "Use the constant GAMEMODE instead"]
 e2function string gamemode()
 	return gmod.GetGamemode().Name
 end
 
+[deprecated = "Use the constant SERVERUUID instead"]
 e2function string serverUUID()
 	return WireLib.GetServerUUID()
 end
 
+[deprecated = "Use the constant SINGLEPLAYER instead"]
 e2function number isSinglePlayer()
 	return game.SinglePlayer() and 1 or 0
 end
 
+[deprecated = "Use the constant DEDICATED instead"]
 e2function number isDedicated()
 	return game.IsDedicated() and 1 or 0
 end
@@ -42,11 +54,13 @@ e2function number numPlayers()
 	return player.GetCount()
 end
 
+[deprecated = "Use the constant MAXPLAYERS instead"]
 e2function number maxPlayers()
 	return game.MaxPlayers()
 end
 
 local sv_gravity = GetConVar("sv_gravity")
+
 e2function number gravity()
 	return sv_gravity:GetFloat()
 end
@@ -75,6 +89,7 @@ e2function number angSpeedLimit()
 	return physenv.GetPerformanceSettings()["MaxAngularVelocity"]
 end
 
+[deprecated = "Use the constant TICKINTERVAL instead"]
 e2function number tickInterval()
 	return engine.TickInterval()
 end
