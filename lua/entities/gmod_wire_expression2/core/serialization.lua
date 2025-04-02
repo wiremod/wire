897474b1a1c9ab6eba13b7a52907444bc817b7e4
thegrb93 <grbrown93@sbcglobal.net>
2025-04-02 08:05:15 -0400

    Fix json memleak

--- lua/entities/gmod_wire_expression2/core/serialization.lua
+++ lua/entities/gmod_wire_expression2/core/serialization.lua
@@ -270,10 +270,29 @@ end
 -- json
 ---------------------------------------------------------------------------
 
+local function CheckCyclic(tbl, parents) -- https://github.com/Facepunch/garrysmod-issues/issues/6259
+    parents[tbl] = true
+    for _, v in pairs(tbl) do
+        if type(v) == "table" then
+            if parents[v] then error("Cannot encode a table with cyclic references") end
+            CheckCyclic(v, parents)
+        end
+    end
+    parents[tbl] = nil
+end
+
 local last_json_error
 
 -- this encodes the table into json
 local function jsonEncode( self, data, prettyprint )
+	local ok, ret = pcall(CheckCyclic, data, {})
+	if not ok then
+		last_json_error = ret
+		if not antispam(self) then return "" end
+		WireLib.ClientError("jsonEncode error: "..ret, self.player)
+		return ""
+	end
+
 	local ok, ret = pcall(util.TableToJSON, data, prettyprint ~= 0)
 	if not ok then
 		last_json_error = ret
