21c50c17272c0dc16c1bad11a57429c3cf67b9cd
Astralcircle <142503363+Astralcircle@users.noreply.github.com>
2025-06-18 04:43:42 +0300

    Use WireLib.AddNotify

--- lua/wire/client/text_editor/wire_expression2_editor.lua
+++ lua/wire/client/text_editor/wire_expression2_editor.lua
@@ -1970,7 +1970,7 @@ function Editor:LoadFile(Line, forcenewtab)
 
 	local f = file.Open(Line, "r", "DATA")
 	if not f then
-		WireLib.AddNotify("Erroring opening file: " .. Line, NOTIFY_ERROR, 5, NOTIFYSOUND_DRIP3)
+		WireLib.AddNotify("Erroring opening file: " .. Line, NOTIFY_ERROR, 5, NOTIFYSOUND_ERROR1)
 	else
 		local str = f:Read(f:Size()) or ""
 		f:Close()
