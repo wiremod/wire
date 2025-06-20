82df117a6389be195728178bb936fb5023d42da0
Astralcircle <142503363+Astralcircle@users.noreply.github.com>
2025-06-20 09:28:41 +0300

    Remove useless timer
    I don't see any reason to recreate the hook every 5 seconds

--- lua/entities/gmod_wire_expression2/core/gametick.lua
+++ lua/entities/gmod_wire_expression2/core/gametick.lua
@@ -27,29 +27,25 @@ e2function number tickClk()
 	return self.data.tickrun and 1 or 0
 end
 
-local function Expression2TickClock()
-	local ents = {}
-
-	-- this additional step is needed because we cant modify registered_chips while it is being iterated.
+hook.Add("Think", "Expression2TickClock", function()
+	-- This additional step is needed because we cant modify registered_chips while it is being iterated.
+	local entities = {}
 	local i = 1
-	for entity,_ in pairs(registered_chips) do
+
+	for entity in pairs(registered_chips) do
 		if entity:IsValid() then
 			ents[i] = entity
 			i = i + 1
 		end
 	end
 
-	for _,entity in ipairs(ents) do
+	for _, entity in ipairs(ents) do
 		entity.context.data.tickrun = true
 		entity:Execute()
 		entity.context.data.tickrun = nil
 	end
 
 	E2Lib.triggerEvent("tick")
-end
-hook.Add("Think", "Expression2TickClock", Expression2TickClock)
-timer.Create("Expression2TickClock", 5, 0, function()
-	hook.Add("Think", "Expression2TickClock", Expression2TickClock)
 end)
 
 E2Lib.registerEvent("tick")
