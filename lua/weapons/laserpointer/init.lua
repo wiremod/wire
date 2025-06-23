8ba6edfe146c87b836c98962382b7e5b3c92cb08
thecraftianman <64441307+thecraftianman@users.noreply.github.com>
2025-06-23 18:15:50 -0400

    Improve SWEP visuals/audio

--- lua/weapons/laserpointer/init.lua
+++ lua/weapons/laserpointer/init.lua
@@ -2,47 +2,11 @@ AddCSLuaFile("cl_init.lua")
 AddCSLuaFile("shared.lua")
 include("shared.lua")
 
-SWEP.Weight = 8
-SWEP.AutoSwitchTo = false
-SWEP.AutoSwitchFrom = false
-
-SWEP.Receiver = nil
-SWEP.Pointing = false
-
-function SWEP:Initialize()
-	self.Pointing = false
-end
-
 function SWEP:Equip(newOwner)
 	if IsValid(newOwner.LasReceiver) then
 		self.Receiver = newOwner.LasReceiver
 		newOwner.LasReceiver = nil
-		newOwner:PrintMessage(HUD_PRINTTALK, "Relinked Sucessfully")
-	end
-end
-
-function SWEP:PrimaryAttack()
-	self.Pointing = not self.Pointing
-	self:SetLaserEnabled(self.Pointing)
-
-	if self.Pointing and IsValid(self.Receiver) then
-		Wire_TriggerOutput(self.Receiver,"Active", 1)
-	else
-		Wire_TriggerOutput(self.Receiver,"Active", 0)
-	end
-end
-
-function SWEP:SecondaryAttack()
-	local owner = self:GetOwner()
-	if not IsValid(owner) then return end
-
-	local trace = owner:GetEyeTrace()
-
-	if IsValid(trace.Entity) and trace.Entity:GetClass() == "gmod_wire_las_receiver" and gamemode.Call("CanTool", owner, trace, "wire_las_receiver") then
-		self.Receiver = trace.Entity
-		owner:PrintMessage(HUD_PRINTTALK, "Linked Sucessfully")
-
-		return true
+		newOwner:PrintMessage(HUD_PRINTTALK, "Relinked Successfully!")
 	end
 end
 
--- lua/weapons/laserpointer/shared.lua
+++ lua/weapons/laserpointer/shared.lua
@@ -1,14 +1,15 @@
 SWEP.Author = ""
 SWEP.Contact = ""
-SWEP.Purpose = ""
-SWEP.Instructions = "Left Click to designate targets. Right click to select laser receiver."
+SWEP.Purpose = "Input source for Laser Pointer Receivers in Wiremod."
+SWEP.Instructions = "Left click to designate targets. Right click to select laser receiver."
 SWEP.Category = "Wiremod"
 
 SWEP.Spawnable = true
 SWEP.AdminOnly = false
 
-SWEP.viewModel = "models/weapons/v_pistol.mdl"
-SWEP.worldModel = "models/weapons/w_pistol.mdl"
+SWEP.UseHands = true
+SWEP.ViewModel = "models/weapons/c_pistol.mdl"
+SWEP.WorldModel = "models/weapons/w_pistol.mdl"
 
 SWEP.Primary.ClipSize = -1
 SWEP.Primary.DefaultClip = -1
@@ -20,6 +21,61 @@ SWEP.Secondary.DefaultClip = -1
 SWEP.Secondary.Automatic = false
 SWEP.Secondary.Ammo = "none"
 
+SWEP.Weight = 8
+SWEP.AutoSwitchTo = false
+SWEP.AutoSwitchFrom = false
+
+SWEP.Receiver = nil
+SWEP.Pointing = false
+
+local singleplayer = game.SinglePlayer()
+
 function SWEP:SetupDataTables()
-    self:NetworkVar("Bool", 0, "LaserEnabled")
+	self:NetworkVar("Bool", 0, "LaserEnabled")
+end
+
+function SWEP:Initialize()
+	self.Pointing = false
+
+	if CLIENT then
+		self:Setup(self:GetOwner())
+	end
+end
+
+function SWEP:PrimaryAttack()
+	self.Pointing = not self.Pointing
+	self:SetLaserEnabled(self.Pointing)
+
+	if CLIENT or singleplayer then
+		local pitch = self.Pointing and 120 or 80
+		self:EmitSound("ambient/energy/newspark03.wav", 100, pitch, 0.5)
+
+		return
+	end
+
+	if self.Pointing and IsValid(self.Receiver) then
+		Wire_TriggerOutput(self.Receiver, "Active", 1)
+	else
+		Wire_TriggerOutput(self.Receiver, "Active", 0)
+	end
+end
+
+function SWEP:SecondaryAttack()
+	local owner = self:GetOwner()
+	if not IsValid(owner) then return end
+
+	local trace = owner:GetEyeTrace()
+
+	if IsValid(trace.Entity) and trace.Entity:GetClass() == "gmod_wire_las_receiver" and gamemode.Call("CanTool", owner, trace, "wire_las_receiver") then
+		if SERVER then
+			self.Receiver = trace.Entity
+			owner:PrintMessage(HUD_PRINTTALK, "Linked Successfully!")
+		elseif (CLIENT or singleplayer) and IsFirstTimePredicted() then
+			self:EmitSound("buttons/bell1.wav")
+		end
+
+		return true
+	elseif (CLIENT or singleplayer) and IsFirstTimePredicted() then
+		self:EmitSound("buttons/button16.wav", 100, 50, 0.5)
+	end
 end
