d699c1b3fd2e1db0faaaf39a2c12c17bb3a92c99
Astralcircle <142503363+Astralcircle@users.noreply.github.com>
2025-07-26 07:18:49 +0300

    Replace data migration mechanism

--- lua/wire/default_data_decompressor.lua
+++ lua/wire/default_data_decompressor.lua
@@ -1,45 +1,29 @@
--- Garry has imposed a file extension whitelist for the Steam Workshop which does not permit the dangerous format .txt
--- Therefore, we must store our .txt's in default_data_files.lua, and then extract them when first run
+-- This file is designed to migrate default data files from data_static to data. It is mainly used for E2 tests, but can be used for everything
+local function RecursivelyGenerateFolder(path)
+	local subpath = string.gsub(path, "data_static/", "")
+	local files, dirs = file.Find(path .. "*", "GAME")
 
-local ignored_dirs = {
-	["expression2/tests"] = true,
-}
-
--- Compress all files in addons/wire/data recursively into 1 json string
-local function ReadDir(root)
-	if ignored_dirs[root] then return nil end
-	local tab = {}
-	local files,dirs = file.Find("addons/wire/data/"..root.."*","GAME")
-	for _, f in pairs(files) do
-		f = root..f
-		tab[f] = file.Read("addons/wire/data/"..f, "GAME")
+	for _, filename in ipairs(files) do
+		local filepath = path .. filename
+		file.Write(subpath .. filename, file.Read(filepath, "GAME"))
 	end
-	for _, f in pairs(dirs) do
-		f = root..f.."/"
-		tab[f] = ReadDir(f)
-	end
-	return tab
-end
--- Uncomment and Rename this file to wire/lua/wire/default_data_files.lua to update it
--- file.Write("default_data_files.txt", "//"..util.TableToJSON(ReadDir("")))
 
--- Decompress the json string wire/lua/wire/default_data_files.lua into the corresponding 36+ default data files
-local function WriteDir(tab)
-	for f, contents in pairs(tab) do
-		if isstring(contents) then
-			file.Write(f, contents)
-		else
-			file.CreateDir(f)
-			WriteDir(contents)
-		end
+	for _, dir in ipairs(dirs) do
+		file.CreateDir(subpath .. dir)
+		RecursivelyGenerateFolder(path .. dir .. "/", "GAME")
 	end
 end
--- Only expand the files if they aren't present already
-if not file.Exists("expression2/_helloworld_.txt", "DATA") then
-	local compressed = file.Read("wire/default_data_files.lua","LUA")
-	-- The client cannot read lua files sent by the server (for security?), so clientside this'll only work
-	-- if the client actually has Wiremod installed, though with workshop autodownload that'll be common
-	if compressed ~= nil then
-		WriteDir(util.JSONToTable(string.sub(compressed, 3)))
-	end
+
+local function GenerateDataFolders()
+	-- When adding new folders that need to be migrated, add them to this list.
+	RecursivelyGenerateFolder("data_static/expression2/")
+	RecursivelyGenerateFolder("data_static/soundlists/")
 end
+
+-- Generate this only once
+if not cookie.GetString("wire_data_generated") then
+	cookie.Set("wire_data_generated", "true")
+	GenerateDataFiles()
+end
+
+concommand.Add("wire_generate_data_files", GenerateDataFolders)
