305c451618134d2bba80bdf01ef88d1be4c32f63
Astralcircle <142503363+Astralcircle@users.noreply.github.com>
2025-08-12 19:08:17 +0300

    Simplify it

--- lua/entities/gmod_wire_painter.lua
+++ lua/entities/gmod_wire_painter.lua
@@ -19,21 +19,21 @@ end
 local wire_painters_maxlen = CreateConVar("sbox_wire_painters_maxlen", "16384", nil, nil, 0, 16384)
 
 function ENT:Setup(decal, range)
-	self.PainterDecal = list.Contains("PaintMaterials", decal) and tostring(decal) or "Blood"
+	self.Decal = list.Contains("PaintMaterials", decal) and tostring(decal) or "Blood"
 	self:SetBeamLength(math.Clamp(tonumber(range) or 0, 0, wire_painters_maxlen:GetInt()))
-	self:ShowOutput(self.PainterDecal)
+	self:ShowOutput(self.Decal)
 end
 
 function ENT:TriggerInput(iname, value)
 	if iname == "Paint" then
 		if value ~= 0 then
-			util.Decal(self.PainterDecal, self:GetPos(), self:GetPos() + self:GetUp() * self:GetBeamLength(), self)
+			util.Decal(self.Decal, self:GetPos(), self:GetPos() + self:GetUp() * self:GetBeamLength(), self)
 		end
 	elseif iname == "Length" then
 		self:SetBeamLength(math.Clamp(value, 0, wire_painters_maxlen:GetInt()))
 	elseif iname == "Decal" then
 		if list.Contains("PaintMaterials", value) then
-			self.PainterDecal = value
+			self.Decal = value
 			self:ShowOutput(value)
 		end
 	end
@@ -43,4 +43,4 @@ function ENT:ShowOutput(value)
 	self:SetOverlayText("Decal: " .. value)
 end
 
-duplicator.RegisterEntityClass("gmod_wire_painter", WireLib.MakeWireEnt, "Data", "PainterDecal", "Range")
+duplicator.RegisterEntityClass("gmod_wire_painter", WireLib.MakeWireEnt, "Data", "Decal", "Range")
