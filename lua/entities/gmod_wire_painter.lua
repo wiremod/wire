9c41333cd1011f9367373b95bf734dff17242049
Astralcircle <142503363+Astralcircle@users.noreply.github.com>
2025-08-13 04:30:52 +0300

    Don't think it's abusable + minor changes

--- lua/entities/gmod_wire_painter.lua
+++ lua/entities/gmod_wire_painter.lua
@@ -6,7 +6,7 @@ ENT.RenderGroup = RENDERGROUP_BOTH
 ENT.WireDebugName = "Painter"
 
 function ENT:SetupDataTables()
-	self:NetworkVar("Int", 0, "BeamLength")
+	self:NetworkVar("Float", 0, "BeamLength")
 end
 
 if CLIENT then return end
@@ -16,21 +16,20 @@ function ENT:Initialize()
 	WireLib.CreateInputs(self, { "Paint", "Length", "Decal [STRING]" })
 end
 
-local wire_painters_maxlen = CreateConVar("sbox_wire_painters_maxlen", "16384", nil, nil, 0, 16384)
-
 function ENT:Setup(decal, range)
-	self.Decal = list.Contains("PaintMaterials", decal) and tostring(decal) or "Blood"
-	self:SetBeamLength(math.Clamp(tonumber(range) or 0, 0, wire_painters_maxlen:GetInt()))
+	self.Decal = list.Contains("PaintMaterials", decal) and decal or "Blood"
+	if range then self:SetBeamLength(range)
 	self:ShowOutput(self.Decal)
 end
 
 function ENT:TriggerInput(iname, value)
 	if iname == "Paint" then
 		if value ~= 0 then
-			util.Decal(self.Decal, self:GetPos(), self:GetPos() + self:GetUp() * self:GetBeamLength(), self)
+			local pos = self:GetPos()
+			util.Decal(self.Decal, pos, pos + self:GetUp() * self:GetBeamLength(), self)
 		end
 	elseif iname == "Length" then
-		self:SetBeamLength(math.Clamp(value, 0, wire_painters_maxlen:GetInt()))
+		self:SetBeamLength(value)
 	elseif iname == "Decal" then
 		if list.Contains("PaintMaterials", value) then
 			self.Decal = value
@@ -43,4 +42,4 @@ function ENT:ShowOutput(value)
 	self:SetOverlayText("Decal: " .. value)
 end
 
-duplicator.RegisterEntityClass("gmod_wire_painter", WireLib.MakeWireEnt, "Data", "Decal", "Range")
+duplicator.RegisterEntityClass("gmod_wire_painter", WireLib.MakeWireEnt, "Data", "Decal")
