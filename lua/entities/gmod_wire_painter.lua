516be97ea5bc010baa898b4c8c90b005e2f35a49
Astralcircle <142503363+Astralcircle@users.noreply.github.com>
2025-08-16 16:49:47 +0300

    Add painter (#3411)
    * Add painter
    My version of painter entity from wire-extras, maybe i'll improve it more. I'm ready to listen to all suggestions about it
    
    * Fix error with spawnmenu_reload
    
    * Minor change
    
    * Add tool icon
    
    * Fix duplicator decal
    
    * Simplify it
    
    * Don't think it's abusable + minor changes
    
    * Fix
    
    * Allow any decal
    
    * Do derma like selection logic

---
+++ lua/entities/gmod_wire_painter.lua
@@ -0,0 +1,43 @@
+AddCSLuaFile()
+
+ENT.Base = "base_wire_entity"
+ENT.PrintName = "Wire Painter"
+ENT.RenderGroup = RENDERGROUP_BOTH
+ENT.WireDebugName = "Painter"
+
+function ENT:SetupDataTables()
+	self:NetworkVar("Float", 0, "BeamLength")
+end
+
+if CLIENT then return end
+
+function ENT:Initialize()
+	self:PhysicsInit(SOLID_VPHYSICS)
+	WireLib.CreateInputs(self, { "Paint", "Length", "Decal [STRING]" })
+end
+
+function ENT:Setup(decal, range)
+	self.Decal = decal
+	if range then self:SetBeamLength(range) end
+	self:ShowOutput(self.Decal)
+end
+
+function ENT:TriggerInput(iname, value)
+	if iname == "Paint" then
+		if value ~= 0 then
+			local pos = self:GetPos()
+			util.Decal(self.Decal, pos, pos + self:GetUp() * self:GetBeamLength(), self)
+		end
+	elseif iname == "Length" then
+		self:SetBeamLength(value)
+	elseif iname == "Decal" then
+		self.Decal = value
+		self:ShowOutput(value)
+	end
+end
+
+function ENT:ShowOutput(value)
+	self:SetOverlayText("Decal: " .. value)
+end
+
+duplicator.RegisterEntityClass("gmod_wire_painter", WireLib.MakeWireEnt, "Data", "Decal")
