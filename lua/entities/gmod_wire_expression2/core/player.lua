28578b3128fdd0cf6305eb6c8512fc9d20c80fdb
Astralcircle <142503363+Astralcircle@users.noreply.github.com>
2025-10-20 21:48:00 +0300

    Return number

--- lua/entities/gmod_wire_expression2/core/player.lua
+++ lua/entities/gmod_wire_expression2/core/player.lua
@@ -503,7 +503,7 @@ end)
 e2function number entity:isTyping()
 	if not IsValid(this) then return self:throw("Invalid entity!", 0) end
 	if not this:IsPlayer() then return self:throw("Expected a Player but got Entity", 0) end
-	return self:IsTyping()
+	return self:IsTyping() and 1 or 0
 end
 
 --------------------------------------------------------------------------------
