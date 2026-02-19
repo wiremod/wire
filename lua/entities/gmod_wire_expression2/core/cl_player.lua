335f41698c6df063ccc8742ab09262573331d06a
Astralcircle <142503363+Astralcircle@users.noreply.github.com>
2026-02-19 05:23:15 +0300

    More compression

--- lua/entities/gmod_wire_expression2/core/cl_player.lua
+++ lua/entities/gmod_wire_expression2/core/cl_player.lua
@@ -1,17 +1,13 @@
-local function SendFriendStatus()
-	local friends = {}
-
-	for _, ply in ipairs(player.GetHumans()) do
-		if ply:GetFriendStatus() == "friend" then
-			table.insert(friends, ply:EntIndex())
-		end
-	end
-
-	RunConsoleCommand("wire_expression2_friend_status", table.concat(friends, ","))
-end
-
 hook.Add("OnEntityCreated", "wire_expression2_extension_player", function(ent)
 	if ent:IsPlayer() then
-		SendFriendStatus()
+		local friends = {}
+
+		for _, ply in ipairs(player.GetHumans()) do
+			if ply:GetFriendStatus() == "friend" then
+				table.insert(friends, ply:EntIndex())
+			end
+		end
+
+		RunConsoleCommand("wire_expression2_friend_status", table.concat(friends, ","))
 	end
 end)
