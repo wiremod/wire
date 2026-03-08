d7808267a724015aeb0fe992218b687322f1d2bb
Astralcircle <142503363+Astralcircle@users.noreply.github.com>
2026-03-08 23:33:53 +0300

    Fix missing return (#3551)

--- lua/entities/gmod_wire_expression2/core/hologram.lua
+++ lua/entities/gmod_wire_expression2/core/hologram.lua
@@ -1168,7 +1168,7 @@ e2function void holoRenderFX(index, effect)
 	if not Holo then return end
 
 	effect = effect - effect % 1
-	if effect == 23 then self:throw("Cannot use kRenderFxRagdoll renderfx!", nil) end
+	if effect == 23 then return self:throw("Cannot use kRenderFxRagdoll renderfx!", nil) end
 	Holo.ent:SetKeyValue("renderfx",effect)
 end
 
