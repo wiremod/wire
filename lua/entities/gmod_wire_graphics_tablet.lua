3b48c2794cc0feeaeb917892b455a43aefdefdfb
Evgeny Akabenko <danlimons322@gmail.com>
2026-05-20 16:21:05 +0300

    Update gmod_wire_graphics_tablet.lua

--- lua/entities/gmod_wire_graphics_tablet.lua
+++ lua/entities/gmod_wire_graphics_tablet.lua
@@ -33,8 +33,12 @@ if CLIENT then
 		return x,y,w,h
 	end
 
-	function ENT:Draw()
-		self:DrawModel()
+	function ENT:Draw(flags)
+		self:DrawModel(flags)
+
+		local is_depth_pass = (bit.band(flags, STUDIO_SSAODEPTHTEXTURE) ~= 0 or bit.band(flags, STUDIO_SHADOWDEPTHTEXTURE) ~= 0)
+
+		if is_depth_pass then return end
 
 		local draw_background = self:GetDrawBackground()
 		self.GPU:RenderToWorld(nil, 512, function(x, y, w, h, monitor, pos, ang, res)
