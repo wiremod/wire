ea91c333b6cee03a0004db912f443d8819a9b70b
Evgeny Akabenko <danlimons322@gmail.com>
2026-05-20 16:16:33 +0300

    Update cl_init.lua

--- lua/entities/gmod_wire_consolescreen/cl_init.lua
+++ lua/entities/gmod_wire_consolescreen/cl_init.lua
@@ -567,8 +567,12 @@ function ENT:DrawSpecialCharacter(c,x,y,w,h,r,g,b)
 end
 
 local VECTOR_1_1_1 = Vector(1, 1, 1)
-function ENT:Draw()
-	self:DrawModel()
+function ENT:Draw(flags)
+	self:DrawModel(flags)
+
+	local is_depth_pass = (bit.band(flags, STUDIO_SSAODEPTHTEXTURE) ~= 0 or bit.band(flags, STUDIO_SHADOWDEPTHTEXTURE) ~= 0)
+
+	if is_depth_pass then return end
 
 	local tone = render.GetToneMappingScaleLinear()
 	render.SetToneMappingScaleLinear(VECTOR_1_1_1)
