4c63d9f3da1913ee1af20e43f46c871f06e57591
Evgeny Akabenko <danlimons322@gmail.com>
2026-05-20 16:19:45 +0300

    Update cl_init.lua

--- lua/entities/gmod_wire_egp_hud/cl_init.lua
+++ lua/entities/gmod_wire_egp_hud/cl_init.lua
@@ -20,7 +20,12 @@ function ENT:EGP_Update() end
 
 function ENT:DrawEntityOutline() end
 
-function ENT:Draw()
-	self:DrawModel()
+function ENT:Draw(flags)
+	self:DrawModel(flags)
+
+	local is_depth_pass = (bit.band(flags, STUDIO_SSAODEPTHTEXTURE) ~= 0 or bit.band(flags, STUDIO_SHADOWDEPTHTEXTURE) ~= 0)
+
+	if is_depth_pass then return end
+	
 	Wire_Render(self)
 end
