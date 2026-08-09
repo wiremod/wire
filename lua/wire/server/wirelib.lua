4d3ff34d2fb419e08dc75b727668b6789ddfb33c
Astralcircle <142503363+Astralcircle@users.noreply.github.com>
2026-08-09 03:15:00 +0300

    Actually fix music sounds abuse

--- lua/wire/server/wirelib.lua
+++ lua/wire/server/wirelib.lua
@@ -1582,6 +1582,7 @@ end
 
 local uniqueSoundsTbl = setmetatable({}, {__index=function(t,k) local r={[1]=0} t[k]=r return r end})
 local maxUniqueSounds = CreateConVar("wire_sounds_unique_max", "200", FCVAR_ARCHIVE, "The maximum number of sound paths a player is allowed to cache")
+local maxSoundLevel = CreateConVar("wire_max_sound_level", "180", FCVAR_ARCHIVE, "The maximum sounds volume (-1 disable)")
 
 function WireLib.SoundExists(path, ply)
 	-- Limit length and remove invalid chars
@@ -1589,19 +1590,41 @@ function WireLib.SoundExists(path, ply)
 
 	-- Extract sound flags. See https://developer.valvesoftware.com/wiki/Soundscripts#Sound_characters
 	local flags, checkpath = string.match(path, "^([^%w_/%.]*)(.*)")
+
 	if #flags > 2 or string.match(flags, "[^#@<>%^%)}]") then
 		path = checkpath
+	end
+
+	-- Disallow too loud sounds
+	local max_level = maxSoundLevel:GetInt()
+
+	if max_level ~= -1 then
+		local sound_data = sound.GetProperties(checkpath)
+
+		if sound_data then
+			local sound_level = sound_data.level
+
+			if sound_level > max_level then
+				return
+			elseif sound_level <= 0 then
+				return
+			end
+		end
 	end
 
 	if ply then
 		-- A player can only use a certain number of unique sound paths
 		local playerSounds = uniqueSoundsTbl[ply:SteamID()]
+
 		if not playerSounds[checkpath] then
-			if playerSounds[1] >= maxUniqueSounds:GetInt() then return end
+			if playerSounds[1] >= maxUniqueSounds:GetInt() then
+				return
+			end
+
 			playerSounds[checkpath] = true
 			playerSounds[1] = playerSounds[1] + 1
 		end
-	elseif not (istable(sound.GetProperties(checkpath)) or file.Exists("sound/" .. checkpath, "GAME")) then
+	elseif not sound.GetProperties(checkpath) or not file.Exists("sound/" .. checkpath, "GAME") then
 		return
 	end
 
