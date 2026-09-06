edb42eebbfd8218764e2456d4b9c842048aab745
Astralcircle <142503363+Astralcircle@users.noreply.github.com>
2026-09-06 09:10:05 +0300

    Fix possible stack overflow error
    Should fix:
    ```
    - addons/server-scripts/lua/autorun/!!!_sh_performance.lua:137: stack overflow
    1. __index - addons/server-scripts/lua/autorun/!!!_sh_performance.lua:137
     2. BackupRenderOverride - addons/wire/lua/wire/flir.lua:52
      3. BackupRenderOverride - addons/wire/lua/wire/flir.lua:53
       4. BackupRenderOverride - addons/wire/lua/wire/flir.lua:53
        5. BackupRenderOverride - addons/wire/lua/wire/flir.lua:53
         6. BackupRenderOverride - addons/wire/lua/wire/flir.lua:53
          7. BackupRenderOverride - addons/wire/lua/wire/flir.lua:53
           8. BackupRenderOverride - addons/wire/lua/wire/flir.lua:53
            9. BackupRenderOverride - addons/wire/lua/wire/flir.lua:53
             10. BackupRenderOverride - addons/wire/lua/wire/flir.lua:53
              11. BackupRenderOverride - addons/wire/lua/wire/flir.lua:53
               12. BackupRenderOverride - addons/wire/lua/wire/flir.lua:53
                13. BackupRenderOverride - addons/wire/lua/wire/flir.lua:53
                 14. BackupRenderOverride - addons/wire/lua/wire/flir.lua:53
                  15. BackupRenderOverride - addons/wire/lua...
    ```

--- lua/wire/flir.lua
+++ lua/wire/flir.lua
@@ -98,7 +98,7 @@ local function RemoveFLIR(ent)
 end
 
 local function SetFLIR(ent)
-	if not ent:IsValid() then return end
+	if not ent:IsValid() or ent.RenderOverride == FLIR.Render then return end
 	local classname = ent:GetClass()
 
 	if ent:GetColor().a > 0 and (FLIR.classWhitelist[classname] or ent:GetMoveType() == MOVETYPE_VPHYSICS or ent:IsPlayer() or ent:IsNPC() or ent:IsRagdoll()) then
